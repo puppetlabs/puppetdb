ed18cceaa419d8471a329ce38ead5066d75b29ac
Deepak Giridharagopal <deepak@puppetlabs.com>
2011-08-13 14:40:51 -0600

    Upgrade to latest sql lib
    This version should contain the ability to set fetchSize on ResultSet
    objects, as described here:
    
    http://bit.ly/pzpAgD

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [postgresql/postgresql "9.0-801.jdbc4"]
-                 [org.clojure/java.jdbc "0.0.5"]
+                 [org.clojure/java.jdbc "0.0.6"]
                  [clj-json "0.3.2"]
                  [digest "1.2.1"]
                  [log4j "1.2.16" :exclusions [javax.mail/mail
