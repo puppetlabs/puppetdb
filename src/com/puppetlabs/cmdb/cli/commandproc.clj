b6ddda252f4e819c92ba7f3e06240c4a10eef673
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-01-04 15:57:08 -0700

    Give populate-mq its own CLI
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

--- src/com/puppetlabs/cmdb/cli/commandproc.clj
+++ src/com/puppetlabs/cmdb/cli/commandproc.clj
@@ -1,15 +1,10 @@
 (ns com.puppetlabs.cmdb.cli.commandproc
-  (:require [com.puppetlabs.cmdb.catalog :as cat]
-            [com.puppetlabs.cmdb.scf.storage :as scf-store]
+  (:require [com.puppetlabs.cmdb.scf.storage :as scf-store]
             [com.puppetlabs.cmdb.command :as command]
             [com.puppetlabs.utils :as pl-utils]
             [clojure.contrib.logging :as log]
             [clojure.java.jdbc :as sql]
-            [clojure.data.json :as json]
-            [clojure.contrib.duck-streams :as ds]
-            [clamq.activemq :as activemq]
-            [clamq.protocol.producer :as mq-producer]
-            [clamq.protocol.connection :as mq-conn])
+            [clamq.activemq :as activemq])
   (:use [clojure.tools.cli :only (cli optional required group)]))
 
 (defn load-from-mq
@@ -21,17 +16,6 @@
 
    (with-open [conn (activemq/activemq-connection mq)]
      (command/process-commands! conn mq-endpoint {:db db}))))
-
-(defn populate-mq
-  [mq mq-endpoint dirname]
-  (with-open [conn (activemq/activemq-connection mq)]
-    (let [producer (mq-conn/producer conn)]
-      (doseq [file (.listFiles (ds/file-str dirname))]
-        (let [content (json/read-json (slurp file))
-              msg {:command "replace catalog" :version 1 :payload content}
-              msg (json/json-str msg)]
-          (mq-producer/publish producer mq-endpoint msg)
-          (log/info (format "Published %s" file)))))))
 
 (defn -main
   [& args]
