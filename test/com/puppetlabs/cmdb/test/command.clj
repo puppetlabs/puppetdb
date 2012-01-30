fdfc78299f14b317d072225c2b1ae2a4f31af873
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-01-30 15:18:53 -0700

    Add test cases for command-processing metrics
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

--- test/com/puppetlabs/cmdb/test/command.clj
+++ test/com/puppetlabs/cmdb/test/command.clj
@@ -53,27 +53,53 @@
      (persistent! retried)
      (persistent! failed)]))
 
+(defn global-count
+  "Returns the counter for the given global metric"
+  [metric-name]
+  (.count (global-metric metric-name)))
+
 (deftest command-mapping
   (testing "Command mapping"
+
+    (testing "should establish global metrics set"
+      (let [msg-seq [1 2 3]
+            on-msg (fn [msg])
+            [acked retried failed] (run-through-command-map msg-seq on-msg)]
+        ;; Verify that all the global metrics are present
+        (is (= (into #{} (keys (@*metrics* "global")))
+               #{:seen :processed :fatal :retried :discarded :processing-time :retry-counts}))))
 
     (testing "should ack messages without errors"
       (let [msg-seq [1 2 3]
             on-msg (fn [msg])
+            prev-seen (global-count :seen)
+            prev-processed (global-count :processed)
             [acked retried failed] (run-through-command-map msg-seq on-msg)]
         (is (= acked [1 2 3]))
         (is (= retried []))
-        (is (= failed []))))
+        (is (= failed []))
+        (is (= 3 (- (global-count :seen) prev-seen)))
+        (is (= 3 (- (global-count :processed) prev-processed)))))
 
     (testing "should retry messages that cause exceptions"
       (let [msg-seq [1 2 3]
             on-msg (fn [msg]
                      (when (even? msg)
                        (throw (RuntimeException. "even number"))))
+            prev-seen (global-count :seen)
+            prev-processed (global-count :processed)
+            prev-retried (global-count :retried)
             [acked retried failed] (run-through-command-map msg-seq on-msg)]
         ;; Despite the error thrown on '2', that message should still be acked
         (is (= acked [1 2 3]))
+        ;; We should have "seen" all 3 messages
+        (is (= 3 (- (global-count :seen) prev-seen)))
+        ;; But only processed 2 of them
+        (is (= 2 (- (global-count :processed) prev-processed)))
         ;; Here's the difference: the error on '2' causes it to get retried
         (is (= retried [2]))
+        ;; Retry count should reflect this
+        (is (= 1 (- (global-count :retried) prev-retried)))
         ;; This is a soft-failure, nothing is fatal
         (is (= failed []))))
 
@@ -82,12 +108,24 @@
             on-msg (fn [msg]
                      (when (even? msg)
                        (throw+ (fatality! (RuntimeException. "even number")))))
+            prev-seen (global-count :seen)
+            prev-processed (global-count :processed)
+            prev-retried (global-count :retried)
+            prev-fatal (global-count :fatal)
             [acked retried failed] (run-through-command-map msg-seq on-msg)]
         ;; Despite the error thrown on '2', that message should still be acked
         (is (= acked [1 2 3]))
+        ;; We should have "seen" all 3 messages
+        (is (= 3 (- (global-count :seen) prev-seen)))
+        ;; But only processed 2 of them
+        (is (= 2 (- (global-count :processed) prev-processed)))
         ;; Since the error is fatal, nothing is retried
         (is (= retried []))
-        (is (= failed [2]))))))
+        ;; Retry count should reflect this
+        (is (= 0 (- (global-count :retried) prev-retried)))
+        ;; Fatal count should reflect the error
+        (is (= failed [2]))
+        (is (= 1 (- (global-count :fatal) prev-fatal)))))))
 
 (deftest msg-handling
   (testing "Initial command processing"
@@ -101,7 +139,10 @@
       (with-redefs [parse-command (fn [msg] {:command "foobar" :version 1})
                     process-command! (fn [msg opts] :sentinel)]
         (let [f (make-msg-handler 1 nil)]
-          (is (= :sentinel (f :unused))))))
+          (is (= :sentinel (f :unused))))
+        ;; Verify that all the command-specific metrics are present
+        (is (= (into #{} (keys (get-in @*metrics* ["foobar" 1])))
+               #{:seen :processed :fatal :retried :discarded :processing-time :retry-counts}))))
 
     (testing "should do nothing if a message has exceeded the max allowable retries"
       (with-redefs [parse-command (fn [msg] {:command "foobar" :version 1 :retries 100})
