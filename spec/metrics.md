72f6f87b44bd9f06e25ef29917d46a650579fdab
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-03-12 16:09:40 -0700

    REST endpoint for querying JMX metrics
    It's not always that easy to get jconsole going in production, but everyone
    loves curl! This allows you to do things like:
    
    curl -v -H "Accept: application/json" \
      'http://localhost:8080/metrics/mbean/java.lang:type=Memory'
    
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

---
+++ spec/metrics.md
@@ -0,0 +1,41 @@
+# Metrics
+
+Querying Grayskull metrics is accomplished by making an HTTP request
+to paths under the `/metrics` REST endpoint.
+
+# Listing available metrics
+
+## Request format
+
+To get a list of all available metric names:
+
+* Request `/metrics/mbeans`.
+
+* A `GET` request is used.
+
+* There is an `Accept` header containing `application/json`.
+
+## Response format
+
+A JSON array of zero or more Strings.
+
+# Retrieving a specific metric
+
+## Request format
+
+To get the attributes of a particular metric:
+
+* Request `/metrics/mbean/<name>`, where `<name>` is something
+  returned in the list of available metrics specified above.
+
+* A `GET` request is used.
+
+* There is an `Accept` header containing `application/json`.
+
+## Response format
+
+A JSON Object mapping strings to (strings/numbers/booleans).
+
+# Examples
+
+    curl -v -H "Accept: application/json" 'http://localhost:8080/metrics/mbean/java.lang:type=Memory'
---
+++ test/com/puppetlabs/cmdb/test/http/metrics.clj
@@ -0,0 +1,57 @@
+(ns com.puppetlabs.cmdb.test.http.metrics
+  (:require [com.puppetlabs.cmdb.http.metrics :as metrics]
+            [com.puppetlabs.cmdb.http.server :as server]
+            [cheshire.core :as json]
+            [clojure.java.jdbc :as sql])
+  (:use clojure.test
+         ring.mock.request
+         [com.puppetlabs.cmdb.testutils :only [test-db]]
+         [com.puppetlabs.cmdb.scf.migrate :only [migrate!]]))
+
+(def ^:dynamic *app* nil)
+
+(use-fixtures :each (fn [f]
+                      (let [db (test-db)]
+                        (binding [*app* (server/build-app {:scf-db db})]
+                          (sql/with-connection db
+                            (migrate!)
+                            (f))))))
+
+(def c-t "application/json")
+
+(defn make-request
+  "Return a GET request against path, suitable as an argument to the
+  metrics app."
+  ([path] (make-request path {}))
+  ([path {keys [:content-type] :or {:content-type c-t} :as params}]
+     (let [request (request :get (format "/metrics/%s" path))
+           headers (:headers request)]
+       (assoc request :headers (assoc headers "accept" (:content-type params))))))
+
+(deftest metrics-set-handler
+  (testing "Remote metrics endpoint"
+    (testing "should return a 404 for an unknown metric"
+      (let [request (make-request "mbean/does_not_exist")
+            response (*app* request)]
+        (is (= (:status response) 404))))
+
+    (testing "should return a 406 for unacceptable content type"
+      (let [request (make-request "mbeans" {:content-type "text/plain"})
+            response (*app* request)]
+        (is (= (:status response) 406))))
+
+    (testing "should return a 200 for an existing metric"
+      (let [request (make-request "mbean/java.lang:type=Memory")
+            response (*app* request)]
+        (is (= (:status response) 200))
+        (is (= (get-in response [:headers "Content-Type"]) c-t))
+        (is (= (map? (json/parse-string (:body response) true))
+               true))))
+
+    (testing "should return a list of all mbeans"
+      (let [request (make-request "mbeans")
+            response (*app* request)]
+        (is (= (:status response) 200))
+        (is (= (get-in response [:headers "Content-Type"]) c-t))
+        (is (= (coll? (json/parse-string (:body response)))
+               true))))))
