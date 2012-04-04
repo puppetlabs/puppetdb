fa96e8b36597c0ce75b9d4a8b174d96a29178d83
Nick Lewis <nick@puppetlabs.com>
2012-04-04 16:45:41 -0700

    Move docstring to proper location

--- src/com/puppetlabs/cmdb/query/population.clj
+++ src/com/puppetlabs/cmdb/query/population.clj
@@ -2,9 +2,9 @@
   (:use [com.puppetlabs.jdbc :only [query-to-vec]]))
 
 (defn correlate-exported-resources
-  []
   "Fetch a map of {exported-resource [#{exporting-nodes} #{collecting-nodes}]},
   to correlate the nodes exporting and collecting resources."
+  []
   (query-to-vec (str "SELECT DISTINCT exporters.type, exporters.title, "
                      "(SELECT certname FROM certname_catalogs WHERE catalog=exporters.catalog) AS exporter, "
                      "(SELECT certname FROM certname_catalogs WHERE catalog=collectors.catalog) AS collector "
