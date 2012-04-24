5d403832e64b59cc9dd9e9bc7e54b90a44a5cfcc
Nick Lewis <nick@puppetlabs.com>
2012-04-24 12:45:06 -0700

    Add a /status/nodes/<node> route for retrieving node status
    This will return a map of the following form:
    
        {:name <node>
         :deactivated <timestamp>
         :catalog_timestamp <timestamp>
         :facts_timestamp <timestamp>}
    
    If the node is currently active, its 'deactivated' value will be null.
    If no catalog/facts are present, the corresponding timestamps will be
    null. If the node is unknown, 404 will be returned.

---
+++ src/com/puppetlabs/puppetdb/query/status.clj
@@ -0,0 +1,27 @@
+;; ## Status query
+;;
+;; ### Node status
+;;
+;; Node status information will be returned in the form:
+;;
+;;     {:name <node>
+;;      :deactivated <timestamp>
+;;      :catalog_timestamp <timestamp>
+;;      :facts_timestamp <timestamp>}
+;;
+;; If the node is active, "deactivated" will be null. If a catalog or facts are
+;; not present, the corresponding timestamps will be null.
+(ns com.puppetlabs.puppetdb.query.status
+  (:use [com.puppetlabs.jdbc :only [query-to-vec]]))
+
+(defn node-status
+  "Return the current status of a node, including whether it's active, and the
+  timestamp of its most recent catalog and facts."
+  [node]
+  (let [results (query-to-vec (str "SELECT certnames.name, certnames.deactivated, c.timestamp AS catalog_timestamp, f.timestamp AS facts_timestamp "
+                                  "FROM certnames LEFT OUTER JOIN certname_catalogs c ON certnames.name = c.certname "
+                                  "LEFT OUTER JOIN certname_facts_metadata f ON certnames.name = f.certname "
+                                  "WHERE certnames.name = ?")
+                             node)]
+    (if (seq results)
+      (first results))))
