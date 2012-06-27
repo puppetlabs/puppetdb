4eeb1e72ed177eeee4f08363d0794e2510ed1938
Chris Price <chris@puppetlabs.com>
2012-06-26 17:26:32 -0700

    Allow puppetdb acceptance tests to be run from source
    This commit contains some conditional logic that allows
    the puppetdb acceptance tests to be run via a git source
    tree, rather than having to be able to build OS packages.
    
    This will allow for quicker iteration for developers
    when writing / debugging acceptance tests.

--- acceptance/00_setup.rb
+++ acceptance/00_setup.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+LeinCommandPrefix = "cd /opt/puppet-git-repos/puppetdb; LEIN_ROOT=true"
+
 def get_tar_file(source_dir, prefix)
   # TODO: use Ruby's standard temp file mechanism
   `cd #{source_dir} && git archive --format=tar --prefix=#{prefix}/ HEAD -o /tmp/data.tar`
@@ -8,6 +10,13 @@ end
 
 test_name "Setup PuppetDB"
 
+# TODO: I'm not 100% sure whether this is the right or best way to determine
+#  whether we're running from source or running from a package; might want
+#  to change this.
+install_type = PuppetAcceptance::Options.options[:type] == 'git' ? :git : :package
+PuppetDBExtensions.test_mode = install_type
+
+
 pkg_dir = File.join(File.dirname(__FILE__), '..', 'pkg')
 
 # This is used for handling metrics
@@ -15,11 +24,34 @@ step "Install JSON on the PuppetDB server" do
   on database, "apt-get install -y libjson-ruby"
 end
 
-step "Install Puppet on all systems" do
-  on hosts, "wget http://apt.puppetlabs.com/puppetlabs-release_1.0-3_all.deb"
-  on hosts, "dpkg -i puppetlabs-release_1.0-3_all.deb"
-  on hosts, "apt-get update"
-  on hosts, "apt-get install --force-yes -y puppet"
+# TODO: I think it would be nice to get rid of all of these conditionals; maybe
+#  we could refactor this into a base class with two child classes that handle
+#  the differences... or something less ugly than this :)
+if (PuppetDBExtensions.test_mode == :git)
+  step "Install rake on the PuppetDB server" do
+    on database, "apt-get install -y rake"
+  end
+  step "Install java on the PuppetDB server" do
+    on database, "apt-get install -y openjdk-6-jre-headless"
+  end
+  step "Install lein on the PuppetDB server" do
+    which_result = on database, "which lein", :acceptable_exit_codes => [0,1]
+    needs_lein = which_result.exit_code == 1
+    if (needs_lein)
+      on database, "curl https://raw.github.com/technomancy/leiningen/1.7.1/bin/lein -o /usr/local/bin/lein"
+      on database, "chmod +x /usr/local/bin/lein"
+      on database, "LEIN_ROOT=true lein"
+    end
+  end
+end
+
+if (PuppetDBExtensions.test_mode == :package)
+  step "Install Puppet on all systems" do
+    on hosts, "wget http://apt.puppetlabs.com/puppetlabs-release_1.0-3_all.deb"
+    on hosts, "dpkg -i puppetlabs-release_1.0-3_all.deb"
+    on hosts, "apt-get update"
+    on hosts, "apt-get install --force-yes -y puppet"
+  end
 end
 
 step "Install PuppetDB on the PuppetDB server" do
@@ -29,14 +61,28 @@ step "Install PuppetDB on the PuppetDB server" do
     end
   end
 
-  step "Copy the package" do
-    on database, "mkdir -p /tmp/packages/puppetdb"
-    scp_to database, File.join(pkg_dir, "puppetdb.deb"), "/tmp/packages/puppetdb"
-  end
+  if (PuppetDBExtensions.test_mode == :package)
+    step "Copy the package" do
+      on database, "mkdir -p /tmp/packages/puppetdb"
+      scp_to database, File.join(pkg_dir, "puppetdb.deb"), "/tmp/packages/puppetdb"
+    end
 
-  step "Install the package" do
-    on database, "dpkg -i /tmp/packages/puppetdb/puppetdb.deb", :acceptable_exit_codes => (0..255)
-    on database, "apt-get -f -y install"
+    step "Install the package" do
+      on database, "dpkg -i /tmp/packages/puppetdb/puppetdb.deb", :acceptable_exit_codes => (0..255)
+      on database, "apt-get -f -y install"
+    end
+  else
+    step "Install PuppetDB via rake" do
+      on database, "rm -rf /etc/puppetdb/ssl"
+      on database, "#{LeinCommandPrefix} rake template"
+      on database, "sh /opt/puppet-git-repos/puppetdb/ext/files/debian/puppetdb.preinst install"
+      on database, "#{LeinCommandPrefix} rake install"
+      # TODO: might want to move this into teardown, but if it doesn't happen
+      #  at some point then you will end up with a bad jetty.ini file.
+      on database, "sh /opt/puppet-git-repos/puppetdb/ext/files/debian/puppetdb.postinst"
+      on database, "cat /etc/puppetdb/conf.d/jetty.ini"
+
+    end
   end
 
   step "Start PuppetDB" do
@@ -45,14 +91,20 @@ step "Install PuppetDB on the PuppetDB server" do
 end
 
 step "Install the PuppetDB terminuses on the master" do
-  step "Copy the package" do
-    on master, "mkdir -p /tmp/packages/puppetdb"
-    scp_to master, File.join(pkg_dir, "puppetdb-terminus.deb"), "/tmp/packages/puppetdb"
-  end
+  if (PuppetDBExtensions.test_mode == :package)
+    step "Copy the package" do
+      on master, "mkdir -p /tmp/packages/puppetdb"
+      scp_to master, File.join(pkg_dir, "puppetdb-terminus.deb"), "/tmp/packages/puppetdb"
+    end
 
-  step "Install the package" do
-    on master, "dpkg -i /tmp/packages/puppetdb/puppetdb-terminus.deb", :acceptable_exit_codes => (0..255)
-    on master, "apt-get -f -y install"
+    step "Install the package" do
+      on master, "dpkg -i /tmp/packages/puppetdb/puppetdb-terminus.deb", :acceptable_exit_codes => (0..255)
+      on master, "apt-get -f -y install"
+    end
+  else
+    step "Install the termini via rake" do
+      on master, "#{LeinCommandPrefix} rake sourceterminus"
+    end
   end
 
   step "Configure routes.yaml to talk to the PuppetDB server" do
