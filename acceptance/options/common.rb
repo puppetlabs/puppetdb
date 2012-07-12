41ae92bd45ff6222415d1b2dbbb7e09ac9864636
Chris Price <chris@puppetlabs.com>
2012-07-12 12:40:18 -0700

    (#15281) Add ability to toggle between database backends
    This commit allows the acceptance tests to be configured
    to use either the embedded database OR postgres.  It
    introduces some "options files" (which rely on pull req
    number 259 on the acceptance framework)--these are used
    to provide default values for various options that
    we would normally specify from the command line; they
    are also used to introduce the "puppetdb_database" option,
    which is what we will use to toggle between the different
    database backends.

---
+++ acceptance/options/common.rb
@@ -0,0 +1,8 @@
+def common_options_hash()
+  puppetdb_acceptance_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  {
+      :helper     => File.join(puppetdb_acceptance_dir, "helper.rb"),
+      :setup_dir  => File.join(puppetdb_acceptance_dir, "setup"),
+  }
+end
--- acceptance/setup/pre_suite/00_setup.rb
+++ acceptance/setup/pre_suite/00_setup.rb
@@ -8,13 +8,88 @@ extend PuppetAcceptance::DSL::InstallUtils
 LeinCommandPrefix = "cd /opt/puppet-git-repos/puppetdb; LEIN_ROOT=true"
 
 
+test_name "Setup PuppetDB"
+
+
+
 def get_tar_file(source_dir, prefix)
   # TODO: use Ruby's standard temp file mechanism
   `cd #{source_dir} && git archive --format=tar --prefix=#{prefix}/ HEAD -o /tmp/data.tar`
   '/tmp/data.tar'
 end
 
-test_name "Setup PuppetDB"
+def setup_postgres()
+  step "Install postgres on the PuppetDB server" do
+
+    step "Install the puppetdb/postgres modules and dependencies" do
+      # TODO: change this to install from a less stupid place once the
+      #  puppetdb module gets cleaned up
+      install_from_git(database, "puppetlabs-puppetdb",
+                       "git://github.com/cprice-puppet/puppetlabs-puppetdb.git",
+                       "master")
+
+      # TODO: change this to install from a less stupid place once the
+      #  postgres module gets cleaned up
+      install_from_git(database, "puppet-postgresql",
+                       "git://github.com/cprice-puppet/puppet-postgresql.git",
+                       "feature/master/align-with-puppetlabs-mysql")
+
+      # TODO: change this to install from a tag once we do a 2.3.4 stdlib release
+      install_from_git(database, "puppetlabs-stdlib",
+                       "git://github.com/puppetlabs/puppetlabs-stdlib.git",
+                       "e299ac6212b0468426f971b216447ef6bc679149")
+
+      install_from_git(database, "puppetlabs-firewall",
+                       "git://github.com/puppetlabs/puppetlabs-firewall.git",
+                       "master")
+    end
+
+    step "Apply a manifest to use the modules to set up postgres for puppetdb" do
+      module_path = database.tmpfile("puppetdb_modulepath")
+      on database, "rm #{module_path}"
+      on database, "mkdir #{module_path}"
+      on database, "ln -s #{SourcePath}/puppetlabs-puppetdb #{module_path}/puppetdb"
+      on database, "ln -s #{SourcePath}/puppet-postgresql #{module_path}/postgresql"
+      on database, "ln -s #{SourcePath}/puppetlabs-stdlib #{module_path}/stdlib"
+      on database, "ln -s #{SourcePath}/puppetlabs-firewall #{module_path}/firewall"
+
+      manifest_path = database.tmpfile("puppetdb_postgres_manifest.pp")
+      # TODO: use more stuff from the puppetdb module once it is robust enough
+      #  to handle what we need.
+      manifest_content = <<-EOS
+
+  class { '::postgresql::server':
+    config_hash => {
+        'ip_mask_allow_all_users' => '0.0.0.0/0',
+        'listen_addresses' => '*',
+        'manage_redhat_firewall' => true,
+
+        'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+        'postgres_password' => 'puppet',
+    },
+  }
+
+  postgresql::db{ 'puppetdb':
+    user          => 'puppetdb',
+    password      => 'puppetdb',
+    grant         => 'all',
+  }
+
+  $database = 'postgres'
+  $database_host = 'localhost'
+
+  file { 'puppetdb: database.ini':
+      ensure      => file,
+      path        => "/etc/puppetdb/conf.d/database.ini",
+      content     => template('puppetdb/server/database.ini.erb')
+  }
+      EOS
+
+      create_remote_file(database, manifest_path, manifest_content)
+      on database, puppet_apply("--modulepath #{module_path} #{manifest_path}")
+    end
+  end
+end
 
 # TODO: I'm not 100% sure whether this is the right or best way to determine
 #  whether we're running from source or running from a package; might want
@@ -89,79 +164,9 @@ step "Install PuppetDB on the PuppetDB server" do
     end
   end
 
-
-# TODO: potentially make this configurable?
-  step "Install postgres on the PuppetDB server" do
-
-    step "Install the puppetdb/postgres modules and dependencies" do
-      # TODO: change this to install from a less stupid place once the
-      #  puppetdb module gets cleaned up
-      install_from_git(database, "puppetlabs-puppetdb",
-                       "git://github.com/cprice-puppet/puppetlabs-puppetdb.git",
-                       "master")
-
-      # TODO: change this to install from a less stupid place once the
-      #  postgres module gets cleaned up
-      install_from_git(database, "puppet-postgresql",
-                       "git://github.com/cprice-puppet/puppet-postgresql.git",
-                       "feature/master/align-with-puppetlabs-mysql")
-
-      # TODO: change this to install from a tag once we do a 2.3.4 stdlib release
-      install_from_git(database, "puppetlabs-stdlib",
-                       "git://github.com/puppetlabs/puppetlabs-stdlib.git",
-                       "e299ac6212b0468426f971b216447ef6bc679149")
-
-      install_from_git(database, "puppetlabs-firewall",
-                       "git://github.com/puppetlabs/puppetlabs-firewall.git",
-                       "master")
-    end
-
-    step "Apply a manifest to use the modules to set up postgres for puppetdb" do
-      module_path = database.tmpfile("puppetdb_modulepath")
-      on database, "rm #{module_path}"
-      on database, "mkdir #{module_path}"
-      on database, "ln -s #{SourcePath}/puppetlabs-puppetdb #{module_path}/puppetdb"
-      on database, "ln -s #{SourcePath}/puppet-postgresql #{module_path}/postgresql"
-      on database, "ln -s #{SourcePath}/puppetlabs-stdlib #{module_path}/stdlib"
-      on database, "ln -s #{SourcePath}/puppetlabs-firewall #{module_path}/firewall"
-
-      manifest_path = database.tmpfile("puppetdb_postgres_manifest.pp")
-      # TODO: use more stuff from the puppetdb module once it is robust enough
-      #  to handle what we need.
-      manifest_content = <<-EOS
-
-class { '::postgresql::server':
-  config_hash => {
-      'ip_mask_allow_all_users' => '0.0.0.0/0',
-      'listen_addresses' => '*',
-      'manage_redhat_firewall' => true,
-
-      'ip_mask_deny_postgres_user' => '0.0.0.0/32',
-      'postgres_password' => 'puppet',
-  },
-}
-
-postgresql::db{ 'puppetdb':
-  user          => 'puppetdb',
-  password      => 'puppetdb',
-  grant         => 'all',
-}
-
-$database = 'postgres'
-$database_host = 'localhost'
-
-file { 'puppetdb: database.ini':
-    ensure      => file,
-    path        => "/etc/puppetdb/conf.d/database.ini",
-    content     => template('puppetdb/server/database.ini.erb')
-}
-      EOS
-
-      create_remote_file(database, manifest_path, manifest_content)
-      on database, puppet_apply("--modulepath #{module_path} #{manifest_path}")
-    end
+  if (options[:puppetdb_database] == 'postgres')
+    setup_postgres()
   end
-
 
   step "Start PuppetDB" do
     start_puppetdb(database)
