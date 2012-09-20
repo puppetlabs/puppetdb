877882e233789ea812d498b8a6102d83c80016aa
Chris Price <chris@puppetlabs.com>
2012-09-19 17:51:10 -0700

    Port acceptance tests to use new puppetdb module.

--- acceptance/setup/pre_suite/70_install_released_puppetdb.rb
+++ acceptance/setup/pre_suite/70_install_released_puppetdb.rb
@@ -3,10 +3,8 @@ test_config = PuppetDBExtensions.config
 # We skip this step entirely unless we are running in :upgrade mode.
 if (test_config[:install_mode] == :upgrade)
   step "Install most recent released PuppetDB on the PuppetDB server for upgrade test" do
-    install_puppetdb(database)
-    configure_puppetdb(database)
+    install_puppetdb(database, test_config[:database])
     start_puppetdb(database)
-    install_puppetdb_termini(master)
-    configure_termini(master, database)
+    install_puppetdb_termini(master, database)
   end
 end
