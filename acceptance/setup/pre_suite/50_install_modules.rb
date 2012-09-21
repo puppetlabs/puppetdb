ec39b9262bb5ed7fbb0fe52c795ee82d8484f688
Chris Price <chris@puppetlabs.com>
2012-09-21 08:59:11 -0700

    Change to cprice404 namespace for puppetdb module
    Looks like there might be one more small bug in the
    puppetdb module.  I've fixed it and released it
    to cprice404 namespace; changing the acceptance
    tests to point to that so that we can test it out
    before pushing it up to the puppetlabs namespace.

--- acceptance/setup/pre_suite/50_install_modules.rb
+++ acceptance/setup/pre_suite/50_install_modules.rb
@@ -2,6 +2,6 @@ require 'lib/puppet_acceptance/dsl/install_utils'
 
 extend PuppetAcceptance::DSL::InstallUtils
 
-step "Install the puppetdb/postgres modules and dependencies" do
-  on master, "puppet module install puppetlabs/puppetdb"
+step "Install the puppetdb module and dependencies" do
+  on master, "puppet module install cprice404/puppetdb"
 end
