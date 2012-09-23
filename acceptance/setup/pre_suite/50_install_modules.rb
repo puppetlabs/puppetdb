d9253fd7f5a0b9b9dab0a66e20619dfdd97153db
Chris Price <chris@puppetlabs.com>
2012-09-23 16:34:42 -0700

    Acceptance: use puppetdb module from puppetlabs namespace

--- acceptance/setup/pre_suite/50_install_modules.rb
+++ acceptance/setup/pre_suite/50_install_modules.rb
@@ -3,5 +3,5 @@ require 'lib/puppet_acceptance/dsl/install_utils'
 extend PuppetAcceptance::DSL::InstallUtils
 
 step "Install the puppetdb module and dependencies" do
-  on master, "puppet module install cprice404/puppetdb"
+  on master, "puppet module install puppetlabs/puppetdb"
 end
