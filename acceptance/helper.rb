b0beb79ea17950a33438b5110c1f63fdb17b7ea0
Nick Lewis <nick@puppetlabs.com>
2012-10-11 14:57:39 -0700

    Explicitly set which version of packages to install for acceptance
    This will fix an issue where, for instance, the most recent released
    version is 1.0.1, but that tag only exists in 1.0.x, so when testing
    against master, the development package is never installed because it
    isn't "newer".
    
    This also changes the version format specified for
    PUPPETDB_EXPECTED_VERSION to exactly the version returned by `rpm -q`
    and `dpkg-query`, rather than munging it. That change is because we
    already need the unmunged version string to specify which version to
    install.

--- acceptance/helper.rb
+++ acceptance/helper.rb
@@ -124,12 +124,12 @@ module PuppetDBExtensions
 
 
 
-  def install_puppetdb(host, db)
+  def install_puppetdb(host, db, version='latest')
     manifest = <<-EOS
     class { 'puppetdb':
       database               => '#{db}',
       manage_redhat_firewall => false,
-      puppetdb_version       => 'latest',
+      puppetdb_version       => '#{version}',
     }
     EOS
     apply_manifest_on(host, manifest)
@@ -148,7 +148,7 @@ module PuppetDBExtensions
             result.stdout.strip
           when :redhat
             result = on host, "rpm -q puppetdb --queryformat \"%{VERSION}-%{RELEASE}\""
-            result.stdout.strip.split('.')[0...-1].join('.')
+            result.stdout.strip
           else
             raise ArgumentError, "Unsupported OS family: '#{os}'"
         end
@@ -160,14 +160,14 @@ module PuppetDBExtensions
   end
 
 
-  def install_puppetdb_termini(host, database)
+  def install_puppetdb_termini(host, database, version='latest')
     # We pass 'restart_puppet' => false to prevent the module from trying to
     # manage the puppet master service, which isn't actually installed on the
     # acceptance nodes (they run puppet master from the CLI).
     manifest = <<-EOS
     class { 'puppetdb::master::config':
       puppetdb_server   => '#{database.node_name}',
-      puppetdb_version  => 'latest',
+      puppetdb_version  => '#{version}',
       restart_puppet    => false,
     }
     EOS
