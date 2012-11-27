3d06c23a57e31d454197b56105535004180a67bf
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-11-27 12:32:00 -0700

    Update the facts terminus to use /v2/nodes/:node/facts
    The facts endpoint has changed to no longer provide node-specific facts
    based solely on URL path components. However, we can route that request
    to the new `/v2/nodes/:node/facts` URL instead.
    
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

--- puppet/lib/puppet/indirector/facts/puppetdb.rb
+++ puppet/lib/puppet/indirector/facts/puppetdb.rb
@@ -15,7 +15,7 @@ class Puppet::Node::Facts::Puppetdb < Puppet::Indirector::REST
 
   def find(request)
     begin
-      response = http_get(request, "/v2/facts/#{request.key}", headers)
+      response = http_get(request, "/v2/#{request.key}/facts", headers)
       log_x_deprecation_header(response)
 
       if response.is_a? Net::HTTPSuccess
