9797b028b8d2dc19cbef584b28bb796afb13bed1
Chris Price <chris@puppetlabs.com>
2012-11-28 16:00:24 -0800

    Add `com.puppetlabs.time` namespace, time utility functions
    This commit adds a new namespace containing some time-related utility
    functions.
    
    In particular, it adds a function `parse-period` which,
    given a (semi-) human-readable string representation of a duration
    of time, will parse the string and return an instance of joda time's
    `Period` class.  (so, e.g., `(parse-period "2d")` returns a joda time
    `Period` representing 2 days).
    
    It also adds some helper functions that will convert `Period`
    instances to other units of time.

---
+++ src/com/puppetlabs/time.clj
@@ -0,0 +1,112 @@
+(ns com.puppetlabs.time
+  (:import (org.joda.time.format PeriodFormatterBuilder PeriodFormatter)))
+
+(defn- build-parser
+  "A utility function that builds up a joda-time `PeriodFormatter` instance that
+  can be used for parsing strings into `Period`s.  The parser returned by this
+  function is intended to parse a single unit of time from a string formatted as
+  `'%i%s'`, where `%i` is a positive integer, and `%s` is a suffix string used
+  to determine what unit of time we're returning.
+
+  `f` - A function that accepts an instance of `PeriodFormatterBuilder` and
+        calls the appropriate `append*` method for the desired unit of time.
+  `suffix` - the suffix string that will cause this parser to match."
+  [f suffix]
+  {:pre   [(fn? f)
+           (string? suffix)]
+   :post  [(instance? PeriodFormatter %)]}
+  (-> (PeriodFormatterBuilder.)
+      (f)
+      (.appendSuffix suffix)
+      (.toFormatter)))
+
+(def day-parser
+  "A parser that matches strings ending with `d` and returns a `Period` of days"
+  (build-parser #(.appendDays %) "d"))
+
+(def hour-parser
+  "A parser that matches strings ending with `h` and returns a `Period` of hours"
+  (build-parser #(.appendHours %) "h"))
+
+(def minute-parser
+  "A parser that matches strings ending with `m` and returns a `Period` of minutes"
+  (build-parser #(.appendMinutes %) "m"))
+
+(def second-parser
+  "A parser that matches strings ending with `s` and returns a `Period` of seconds"
+  (build-parser #(.appendSeconds %) "s"))
+
+(def millisecond-parser
+  "A parser that matches strings ending with `ms` and returns a `Period` of milliseconds"
+  (build-parser #(.appendMillis %) "ms"))
+
+(def period-parser
+  "A parser that matches strings ending with `d`, `h`, `m`, `s`, or `ms` and returns
+  a `Period` object representing the specified amount of time."
+  (.. (PeriodFormatterBuilder.)
+      ; it's important that "millisecond" parser comes first in this list,
+      ; because the suffix string is a superset of the suffix string for
+      ; "minutes"... so if the "minute" parser is earlier in the list,
+      ; it will recognize the "m" from a millisecond string, try to parse it,
+      ; fail, and throw an exception.
+      (append millisecond-parser)
+      (append second-parser)
+      (append minute-parser)
+      (append hour-parser)
+      (append day-parser)
+      (toFormatter)))
+
+(defn periods-equal?
+  "Given two or more instances of `Period`, returns true if they all represent
+  the same duration of time (regardless of whether or not they are specified in
+  the same units of time... in other words, the following will return `true`:
+
+      `(periods-equal? (days 2) (hours 48))`
+
+  even though this will return `false`:
+
+      `(= (days 2) (hours 48))`"
+  ([p] true)
+  ([p1 p2] (= (.toStandardDuration p1) (.toStandardDuration p2)))
+  ([p1 p2 & more]
+    (if (periods-equal? p1 p2)
+      (if (next more)
+        (recur p2 (first more) (next more))
+        (periods-equal? p2 (first more)))
+      false)))
+
+(defn parse-period
+  "Parse a String into an instance of `Period`, representing a duration of time.
+  For example, `(parse-period \"2d\")` returns a `Period` representing a duration
+  of 2 days.  Currently supported suffixes are `'d'`, `'h'`, `'m'`, `'s'`, and
+  `'ms'`."
+  [period-string]
+  (.parsePeriod period-parser period-string))
+
+(defn- to-unit
+  "Helper function for converting periods to specific units"
+  [f period]
+  (-> period
+      (.toStandardDuration)
+      (f)))
+
+(def to-days
+  "Given an instance of `Period`, return an integer representing the number of days"
+  (partial to-unit #(.getStandardDays %)))
+
+(def to-hours
+  "Given an instance of `Period`, return an integer representing the number of hours"
+  (partial to-unit #(.getStandardHours %)))
+
+(def to-minutes
+  "Given an instance of `Period`, return an integer representing the number of minutes"
+  (partial to-unit #(.getStandardMinutes %)))
+
+(def to-secs
+  "Given an instance of `Period`, return an integer representing the number of seconds"
+  (partial to-unit #(.getStandardSeconds %)))
+
+(def to-millis
+  "Given an instance of `Period`, return an integer representing the number of milliseconds"
+  (partial to-unit #(.getMillis %)))
+
---
+++ test/com/puppetlabs/test/time.clj
@@ -0,0 +1,61 @@
+(ns com.puppetlabs.test.time
+  (:use clojure.test
+        [clj-time.core :only [days hours secs minutes]]
+        com.puppetlabs.time))
+
+(deftest test-periods-equal?
+  (testing "should return true for a single period"
+    (is (periods-equal? (secs 10))))
+  (testing "should return true for periods that are equal"
+    (is (periods-equal? (days 2) (hours 48)))
+    (is (periods-equal? (hours 3) (minutes 180)))
+    (is (periods-equal? (days 1) (hours 24) (minutes (* 60 24)) (secs (* 60 60 24)))))
+  (testing "should return false for periods that are not equal"
+    (is (not (periods-equal? (days 2) (hours 2))))
+    (is (not (periods-equal? (hours 1) (minutes 59))))
+    (is (not (periods-equal? (hours 1) (minutes 60) (secs 10))))
+    (is (not (periods-equal? (hours 1) (days 1) (minutes 60) (secs (* 60 60 24)))))))
+
+(deftest test-parse-period
+  (testing "should successfully parse a value in days"
+    (is (periods-equal? (parse-period "2d") (days 2))))
+  (testing "should successfully parse a value in hours"
+    (is (periods-equal? (parse-period "10h") (hours 10))))
+  (testing "should successfully parse a value in minutes"
+    (is (periods-equal? (parse-period "120m") (hours 2))))
+  (testing "should successfully parse a value in seconds"
+    (is (periods-equal? (parse-period "14s") (secs 14))))
+  (testing "should successfully parse a value in milliseconds"
+    (is (periods-equal? (parse-period "4000ms") (secs 4))))
+  (testing "should throw an exception if the string is not valid"
+    (is (thrown-with-msg? IllegalArgumentException #"Invalid format: \"foo\""
+          (parse-period "foo")))
+    (is (thrown-with-msg? IllegalArgumentException #"Invalid format: \"2d 1s\""
+          (parse-period "2d 1s")))
+    (is (thrown-with-msg? IllegalArgumentException #"Invalid format: \"2 s\""
+          (parse-period "2 s")))))
+
+(deftest test-to-days
+  (testing "should convert periods in various units to days"
+    (is (= 1 (to-days (hours 24))))
+    (is (= 2 (to-days (minutes (* 60 24 2)))))))
+
+(deftest test-to-hours
+  (testing "should convert periods in various units to hours"
+    (is (= 1 (to-hours (minutes 60))))
+    (is (= 2 (to-hours (secs (* 60 60 2)))))))
+
+(deftest test-to-minutes
+  (testing "should convert periods in various units to minutes"
+    (is (= 60 (to-minutes (hours 1))))
+    (is (= (* 60 24) (to-minutes (days 1))))))
+
+(deftest test-to-secs
+  (testing "should convert periods in various units to seconds"
+    (is (= 60 (to-secs (minutes 1))))
+    (is (= (* 60 60) (to-secs (hours 1))))))
+
+(deftest test-to-millis
+  (testing "should convert periods in various units to millis"
+    (is (= 1000 (to-millis (secs 1))))
+    (is (= (* 1000 60) (to-millis (minutes 1))))))
