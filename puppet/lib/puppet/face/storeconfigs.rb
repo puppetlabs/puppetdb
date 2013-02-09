afb8eab73a28cb2c4522c22ffab363d88924b92b
Nick Lewis <nick@puppetlabs.com>
2013-02-08 16:31:28 -0800

    Add a `puppet storeconfigs export` command to dump the database
    This command can be used to export a copy of the ActiveRecord
    storeconfigs database (only exported resources, and only nodes with
    exported resources) which can be imported into PuppetDB.  This is useful
    when migrating from old-school storeconfigs to PuppetDB, as it allows
    agents to run without losing any of their exported resources for a time.
    Once every agent has run once, the database should be up-to-date in any
    case.

---
+++ puppet/lib/puppet/face/storeconfigs.rb
@@ -0,0 +1,86 @@
+require 'puppet/face'
+
+Puppet::Face.define(:storeconfigs, '0.0.1') do
+  copyright "Puppet Labs", 2011
+  license   "Apache 2 license"
+
+  summary "Interact with the storeconfigs database"
+  description <<-DESC
+    This subcommand interacts with the ActiveRecord storeconfigs database, and
+    can be used to export a dump of that data which is suitable for import by
+    PuppetDB.
+  DESC
+
+  action :export do
+    summary "Export the storeconfigs database"
+    arguments "<filename>"
+    description <<-DESC
+      Generate a complete dump of all catalogs from the storeconfigs database,
+      in a format which can be consumed by PuppetDB. Returns the set of nodes
+      which were exported.
+    DESC
+
+    when_invoked do |filename, options|
+      require 'puppet/rails'
+
+      Puppet::Rails.connect
+
+      # Fetch all nodes, including exported resources and their params
+      nodes = Puppet::Rails::Host.all(:include => {:resources => [:param_values, :puppet_tags]},
+                                      :conditions => {:resources => {:exported => true}})
+
+      catalogs = nodes.map {|node| node_to_catalog_hash(node)}
+
+      File.open(filename, 'w') do |file|
+        # The content of this file is a series of individual catalogs as JSON
+        # objects, not an array of them. This makes it easier to, for instance,
+        # process them in a stream.
+        catalogs.each do |catalog|
+          file.puts catalog.to_pson
+        end
+      end
+
+      nodes.map(&:name).sort
+    end
+
+    when_rendering :console do |nodes|
+      "Exported #{nodes.length} nodes"
+    end
+  end
+
+  def node_to_catalog_hash(node)
+    resources = node.resources.map {|resource| resource_to_hash(resource)}
+
+    {
+      :metadata => {
+        :api_version => 1,
+      },
+      :data => {
+        :name => node.name,
+        :version => node.last_compile || Time.now,
+        :edges => [],
+        :resources => resources,
+        :environment => node.environment || 'production'
+      },
+    }
+  end
+
+  def resource_to_hash(resource)
+    parameters = resource.param_values.inject({}) do |params,param_value|
+      params.merge(param_value.param_name.name => param_value.value)
+    end
+
+    tags = resource.puppet_tags.map(&:name).uniq.sort
+
+    {
+      :type       => resource.restype,
+      :title      => resource.title,
+      :exported   => true,
+      :parameters => parameters,
+      :tags       => tags,
+      :aliases    => [],
+      :file       => resource.file,
+      :line       => resource.line,
+    }
+  end
+end
---
+++ puppet/spec/unit/face/storeconfigs_spec.rb
@@ -0,0 +1,129 @@
+#!/usr/bin/env ruby
+
+require 'spec_helper'
+require 'puppet/rails'
+require 'puppet/indirector/catalog/active_record'
+require 'puppet/face/storeconfigs'
+
+describe Puppet::Face[:storeconfigs, '0.0.1'], :if => (Puppet.features.sqlite? and Puppet.features.rails?) do
+  def setup_scratch_database
+    Puppet::Rails.stubs(:database_arguments).returns(
+      :adapter => 'sqlite3',
+      :log_level => Puppet[:rails_loglevel],
+      :database => ':memory:'
+    )
+    Puppet[:railslog]     = '/dev/null'
+    Puppet::Rails.init
+  end
+
+  before :each do
+    setup_scratch_database
+    Puppet[:storeconfigs] = true
+    Puppet[:storeconfigs_backend] = :active_record
+  end
+
+  describe "export action" do
+    after :each do
+      FileUtils.rm_f(@path)
+    end
+
+    before :each do
+      file = Tempfile.new('export')
+      @path = file.path
+      file.close!
+    end
+
+
+    describe "with nodes present" do
+      def notify(title, exported=false)
+        Puppet::Resource.new(:notify, title, :parameters => {:message => title}, :exported => exported)
+      end
+
+      def save_catalog(catalog)
+        request = Puppet::Resource::Catalog.indirection.request(:save, catalog.name, catalog)
+        Puppet::Resource::Catalog::ActiveRecord.new.save(request)
+      end
+
+      before :each do
+        catalog = Puppet::Resource::Catalog.new('foo')
+
+        catalog.add_resource notify('not exported')
+        catalog.add_resource notify('exported', true)
+
+        save_catalog(catalog)
+      end
+
+      it "should have the right structure" do
+        result = subject.export(@path)
+
+        result.should == ['foo']
+
+        catalog = PSON.load(File.read(@path))
+
+        catalog.keys.should =~ ['metadata', 'data']
+
+        catalog['metadata'].should == {'api_version' => 1}
+
+        data = catalog['data']
+
+        data.keys.should =~ ['name', 'version', 'edges', 'resources', 'environment']
+
+        data['name'].should == 'foo'
+        data['edges'].should == []
+        data['environment'].should == 'production'
+
+        data['resources'].first.should == {
+          'type'       => 'Notify',
+          'title'      => 'exported',
+          'exported'   => true,
+          'tags'       => ['exported', 'notify'],
+          'aliases'    => [],
+          'file'       => nil,
+          'line'       => nil,
+          'parameters' => {
+            'message' => 'exported',
+          },
+        }
+      end
+
+      it "should only include exported resources" do
+        result = subject.export(@path)
+
+        result.should == ['foo']
+
+        catalogs = File.readlines(@path).map {|line| PSON.load(line)}
+
+        catalogs.length.should == 1
+        data = catalogs.first['data']
+        data['name'].should == 'foo'
+
+        data['resources'].map {|resource| [resource['type'], resource['title']]}.should == [['Notify', 'exported']]
+        data['resources'].should be_all {|resource| resource['exported'] == true}
+      end
+
+      it "should exclude nodes with no exported resources" do
+        catalog = Puppet::Resource::Catalog.new('bar')
+
+        catalog.add_resource notify('also not exported')
+
+        save_catalog(catalog)
+
+        result = subject.export(@path)
+
+        result.should == ['foo']
+
+        catalogs = File.readlines(@path).map {|line| PSON.load(line)}
+
+        catalogs.map {|catalog| catalog['data']['name']}.should == ['foo']
+      end
+    end
+
+    it "should do nothing if there are no nodes" do
+      result = subject.export(@path)
+
+      result.should == []
+
+      File.read(@path).should be_empty
+    end
+  end
+end
