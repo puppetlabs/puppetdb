57a7297f0f607bc22fe0dad7ed47f466c6f70d86
Chris Price <chris@puppetlabs.com>
2013-03-04 17:01:50 -0800

    Refactor import to use new producer/consumer code

--- src/com/puppetlabs/puppetdb/cli/import.clj
+++ src/com/puppetlabs/puppetdb/cli/import.clj
@@ -13,8 +13,9 @@
             [clojure.java.io :as io])
   (:import  [com.puppetlabs.archive TarGzReader]
             [org.apache.commons.compress.archivers.tar TarArchiveEntry])
-  (:use [com.puppetlabs.utils :only (cli!)]
-        [com.puppetlabs.puppetdb.cli.export :only [export-root-dir export-metadata-file-name]]))
+  (:use [com.puppetlabs.utils :only (cli! num-cpus)]
+        [com.puppetlabs.puppetdb.cli.export :only [export-root-dir export-metadata-file-name]]
+        [com.puppetlabs.concurrent :only [producer consumer work-queue->seq]]))
 
 (def cli-description "Import PuppetDB catalog data from a backup file")
 
@@ -49,10 +50,20 @@
     (when-not (= pl-http/status-ok (:status result))
       (log/error result))))
 
+
+(defn get-tar-entry-with-content
+  ;; TODO: docs, preconds
+  [tar-reader]
+  (let [tar-entry (archive/next-entry tar-reader)]
+    (if (nil? tar-entry)
+      nil
+      {:tar-entry tar-entry
+       :content   (archive/read-entry-content tar-reader)})))
+
 (defn process-tar-entry
   "Determine the type of an entry from the exported archive, and process it
   accordingly."
-  [_ content ^TarArchiveEntry tar-entry host port metadata]
+  [host port metadata {:keys [content tar-entry]}]
   {:pre  [(string? content)
           (instance? TarArchiveEntry tar-entry)
           (string? host)
@@ -60,16 +71,20 @@
           (map? metadata)]}
   (let [path    (.getName tar-entry)
         pattern (str "^" (.getPath (io/file export-root-dir "catalogs" ".*\\.json")) "$")]
-    (when (re-find (re-pattern pattern) path)
-      (println (format "Importing catalog from archive entry '%s'" path))
-      ;; NOTE: these submissions are async and we have no guarantee that they
-      ;;   will succeed.  We might want to add something at the end of the import
-      ;;   that polls puppetdb until the command queue is empty, then does a
-      ;;   query to the /nodes endpoint and shows the set difference between
-      ;;   the list of nodes that we submitted and the output of that query
-      (submit-catalog host port
-        (get-in metadata [:command-versions :replace-catalog])
-        content))))
+    (if (re-find (re-pattern pattern) path)
+      (do
+        ;; NOTE: these submissions are async and we have no guarantee that they
+        ;;   will succeed.  We might want to add something at the end of the import
+        ;;   that polls puppetdb until the command queue is empty, then does a
+        ;;   query to the /nodes endpoint and shows the set difference between
+        ;;   the list of nodes that we submitted and the output of that query
+        (submit-catalog host port
+          (get-in metadata [:command-versions :replace-catalog])
+          content)
+        {:type    :catalog
+         :message (format "Submitted catalog from archive entry '%s'" path)})
+      {:type    :unknown
+       :message "Unrecognized tar entry; skipping"})))
 
 (defn -main
   [& args]
@@ -80,13 +95,11 @@
         [{:keys [infile host port]} _] (cli! args specs required)
         metadata    (parse-metadata infile)]
 ;; TODO: do we need to deal with SSL or can we assume this only works over a plaintext port?
-    (let [agents (transient [])]
-      (with-open [tar-reader (archive/tarball-reader infile)]
-        (doseq [tar-entry (archive/all-entries tar-reader)]
-          (let [content   (archive/read-entry-content tar-reader)
-                agent     (agent true)]
-            (conj! agents agent)
-            (send agent process-tar-entry content tar-entry host port metadata))))
-      (doseq [agent (persistent! agents)]
-        (println "Waiting for agent")
-        (await agent)))))
+    (with-open [tar-reader (archive/tarball-reader infile)]
+      (let [p       (producer #(get-tar-entry-with-content tar-reader) 1 5)
+            c       (consumer p #(process-tar-entry host port metadata %) (num-cpus))
+            results (work-queue->seq (:result-queue c))]
+        (doseq [{:keys [type message]} results]
+          (condp = type
+            :catalog (log/info message)
+            nil))))))
