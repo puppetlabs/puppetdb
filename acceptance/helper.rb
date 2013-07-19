5f29e12175975d5cd26cdf7601aae8f51d379ed2
Nate Wolfe <nwolfe@puppetlabs.com>
2013-07-19 11:42:42 -0700

    Skip test if sqlite3 isn't installed

--- acceptance/helper.rb
+++ acceptance/helper.rb
@@ -242,6 +242,14 @@ module PuppetDBExtensions
     end
   end
 
+
+  def is_gem_installed_on?(host, gem)
+    # Include a trailing space when grep'ing to force an exact match of the gem name,
+    # so, for example, when checking for 'rspec' we don't match with 'rspec-core'.
+    result = on host, "gem list #{gem} | grep \"#{gem} \"", :acceptable_exit_codes => [0,1]
+    result.exit_code == 0
+  end
+
   ############################################################################
   # NOTE: the following methods should only be called during run-from-source
   #  acceptance test runs.
