d8582ae839fdc89954a6b7d965ed392eca294600
Deepak Giridharagopal <deepak@puppetlabs.com>
2013-07-26 18:18:01 -0600

    Upgrade at-at, ring-mock, postgresql, log4j

--- project.clj
+++ project.clj
@@ -38,20 +38,20 @@
                  ;; Version information
                  [trptcolin/versioneer "0.1.0"]
                  ;; Job scheduling
-                 [overtone/at-at "1.1.1"]
+                 [overtone/at-at "1.2.0"]
                  ;; Nicer exception handling with try+/throw+
                  [slingshot "0.10.3"]
-                 [digest "1.3.0"]
-                 [log4j "1.2.16" :exclusions [javax.mail/mail
+                 [digest "1.4.3"]
+                 [log4j "1.2.17" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
                  ;; Database connectivity
                  [com.jolbox/bonecp "0.7.1.RELEASE" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.hsqldb/hsqldb "2.2.8"]
-                 [postgresql/postgresql "9.0-801.jdbc4"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]
                  [clojureql "1.0.3"]
                  ;; MQ connectivity
                  [clamq/clamq-activemq "0.4" :exclusions [org.slf4j/slf4j-api]]
@@ -64,7 +64,7 @@
                  [org.apache.commons/commons-compress "1.4.1"]]
 
   :profiles {:dev {:resource-paths ["test-resources"],
-                   :dependencies [[ring-mock "0.1.1"]]}}
+                   :dependencies [[ring-mock "0.1.5"]]}}
 
   :jar-exclusions [#"leiningen/"]
 
