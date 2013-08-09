2e3036bed4689c970b2964c26b3101e33c105e30
Chris Price <chris@puppetlabs.com>
2013-08-09 12:35:53 -0700

    Remove unused settings relating to queued commands
    Some time ago, we did some investigatory work around the possibility
    of queueing failed commands to disk on the master and trying to
    re-send them to puppetdb later.  This work ended up being scrapped
    due to complications relating to the unpredictable threading/process
    model for the environments that the puppet master might be
    deployed in.  However, it appears that we failed to remove one of
    the configuration settings that had been added in support of that.

--- puppet/spec/unit/util/puppetdb/config_spec.rb
+++ puppet/spec/unit/util/puppetdb/config_spec.rb
@@ -31,7 +31,6 @@ describe Puppet::Util::Puppetdb::Config do
         config = described_class.load
         config.server.should == 'puppetdb'
         config.port.should == 8081
-        config.max_queued_commands.should == 1000
       end
 
     end
@@ -60,12 +59,10 @@ CONF
 [main]
 server = main_server
 port = 1234
-max_queued_commands = 0
 CONF
         config = described_class.load
         config.server.should == 'main_server'
         config.port.should == 1234
-        config.max_queued_commands.should == 0
       end
 
       it "should use the default if no value is specified" do
