6df63da1005732eb790657f7491f3effae698603
Nate Wolfe <nwolfe@puppetlabs.com>
2013-08-14 17:23:59 -0700

    Fix test to generate events and handle time
    (cherry picked from commit 21c898799b2f470e10eb37b8781189dee19032d3)
    
    Conflicts:
    	acceptance/helper.rb
    	acceptance/tests/reports/report_query_by_timestamp.rb

--- acceptance/tests/reports/report_query_by_timestamp.rb
+++ acceptance/tests/reports/report_query_by_timestamp.rb
@@ -4,23 +4,43 @@ require 'cgi'
 
 test_name "basic validation of puppet report query by timestamp" do
 
-  query_start_time = CGI.escape(Time.now.iso8601)
+  Log.notify "Setting up manifest file"
+  manifest = <<MANIFEST
+notify { "hi":
+  message => "Hi ${::clientcert}"
+}
+MANIFEST
 
-  # Query for all of the events after the start time
-  result = on database, %Q|curl -G -H 'Accept: application/json' http://localhost:8080/experimental/events --data 'query=[">",%20"timestamp",%20"#{query_start_time}"]'|
+  tmpdir = master.tmpdir('report_storage')
 
-  # We expect no results (assuming all of the machines' timestamps are relatively
-  #  sane), because we haven't done any agent runs after the specified time.
-  events = JSON.parse(result.stdout)
-  assert_equal(0, events.length, "Expected no results from event query with timestamp more recent than last batch of agent runs")
+  manifest_file = File.join(tmpdir, 'site.pp')
 
-  # TODO: the module should be setting up the report processors so that we don't
-  # have to add it on the CLI here
+  create_remote_file(master, manifest_file, manifest)
+
+  on master, "chmod -R +rX #{tmpdir}"
+
+  start_times = agents.inject({}) do |hash, agent|
+    hash[agent.node_name] = current_time_on agent
+    hash
+  end
+
+  agents.each do |agent|
+    # Query for all of the events after the start time
+    result = on database, %Q|curl -G http://localhost:8080/experimental/events --data 'query=["and",%20["=",%20"certname",%20"#{agent.node_name}"],%20[">",%20"timestamp",%20"#{start_times[agent.node_name]}"]]'|
+
+    # We expect no results (assuming all of the machines' timestamps are relatively sane),
+    # because we haven't done any agent runs after the specified time.
+    events = JSON.parse(result.stdout)
+    assert_equal(0, events.length, "Expected no results from event query with timestamp more recent than last batch of agent runs")
+  end
+
+  # TODO: the module should be setting up the report processors so that we don't have to add it on the CLI here
   with_puppet_running_on master, {
     'master' => {
       'storeconfigs' => 'true',
       'store_configs_backend' => 'puppetdb',
-      'autosign' => 'true'
+      'autosign' => 'true',
+      'manifest' => manifest_file
     }} do
 
       step "Run agents once to submit reports" do
@@ -31,21 +51,22 @@ test_name "basic validation of puppet report query by timestamp" do
   # Wait until all the commands have been processed
   sleep_until_queue_empty database
 
-  # Query for all of the events after the start time
-  result = on database, %Q|curl -G -H 'Accept: application/json' http://localhost:8080/experimental/events --data 'query=[">",%20"timestamp",%20"#{query_start_time}"]'|
+  agents.each do |agent|
+    start_time = start_times[agent.node_name]
 
-  # This time, we do expect results because we've done agent runs more recently
-  # than the timestamp['
-  events = JSON.parse(result.stdout)
-  assert(events.length > 0, "Expected results from event query with timestamp more recent than last batch of agent runs")
+    # Query for all of the events after the start time
+    result = on database, %Q|curl -G http://localhost:8080/experimental/events --data 'query=["and",%20["=",%20"certname",%20"#{agent.node_name}"],%20[">",%20"timestamp",%20"#{start_time}"]]'|
 
-  # Now, just for good measure, we'll try a compound query with an end time greater
-  # than the timestamp of the agent runs, and expect the results to match the
-  # previous query.
+    # This time, we do expect results because we've done agent runs more recently than the timestamp
+    events = JSON.parse(result.stdout)
+    assert(events.length > 0, "Expected results from event query with timestamp more recent than last batch of agent runs")
 
-  end_time = CGI.escape(Time.now.iso8601)
-  result = on database, %Q|curl -G -H 'Accept: application/json' http://localhost:8080/experimental/events --data 'query=["and",%20[">",%20"timestamp",%20"#{query_start_time}"],%20["<",%20"timestamp",%20"#{end_time}"]]'|
-  events2 = JSON.parse(result.stdout)
-  assert(events.length == events2.length, "Expected compound event time query to return the same number of results as the previous query")
-
+    # Now, just for good measure, we'll try a compound query with an end time greater
+    # than the timestamp of the agent runs, and expect the results to match the
+    # previous query.
+    end_time = current_time_on agent
+    result = on database, %Q|curl -G http://localhost:8080/experimental/events --data 'query=["and",%20["=",%20"certname",%20"#{agent.node_name}"],%20["and",%20[">",%20"timestamp",%20"#{start_time}"],%20["<",%20"timestamp",%20"#{end_time}"]]]'|
+    events2 = JSON.parse(result.stdout)
+    assert(events.length == events2.length, "Expected compound event time query to return the same number of results as the previous query")
+  end
 end
