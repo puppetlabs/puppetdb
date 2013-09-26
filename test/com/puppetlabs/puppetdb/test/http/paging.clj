5caa25b244ce30d3859720c93e1a581437b794a0
Nate Wolfe <nwolfe@puppetlabs.com>
2013-09-26 15:42:02 -0700

    (PE-1673) Properly handle malformed order-by input
    Prior to this commit, an invalid JSON input for the
    'order-by' query paging parameter was causing an
    exception to be thrown and an incorrect response to
    be returned. This commit adds proper error handling.

---
+++ test/com/puppetlabs/puppetdb/test/http/paging.clj
@@ -0,0 +1,22 @@
+(ns com.puppetlabs.puppetdb.test.http.paging
+  (:require [com.puppetlabs.http :as pl-http])
+  (:use clojure.test
+        com.puppetlabs.puppetdb.fixtures
+        [com.puppetlabs.puppetdb.testutils :only [get-request]]))
+
+(use-fixtures :each with-http-app)
+
+(deftest paging-options
+  (doseq [endpoint ["/v3/events"
+                    "/v3/event-counts"
+                    "/v3/fact-names"
+                    "/v3/facts"
+                    "/v3/nodes"
+                    "/v3/reports"
+                    "/v3/resources"]]
+    (testing (str endpoint " order-by should properly handle malformed JSON input")
+      (let [malformed-JSON  "[{\"field\":\"status\" \"order\":\"DESC\"}]"
+            response        (*app* (get-request endpoint ["these" "are" "unused"] {:order-by malformed-JSON}))
+            body            (get response :body "null")]
+        (is (= (:status response) pl-http/status-bad-request))
+        (is (re-find #"Illegal value '.*' for :order-by" body))))))
