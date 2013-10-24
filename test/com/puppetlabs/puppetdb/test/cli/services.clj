5acd4e0a22fe39463a52fa4162a435783c1920e8
Ryan Senior <ryan.senior@puppetlabs.com>
2013-10-24 16:40:06 -0500

    Moved the launch-puppetdb functions into a new testutils.repl namespace

--- test/com/puppetlabs/puppetdb/test/cli/services.clj
+++ test/com/puppetlabs/puppetdb/test/cli/services.clj
@@ -239,33 +239,3 @@
         (with-log-output logz
           (is (false? (f {:ssl-client-cn "badguy"})))
           (is (= 1 (count (logs-matching #"^badguy rejected by certificate whitelist " @logz)))))))))
-
-(defn launch-puppetdb
-  "Starts a puppetdb instance with defaults from config.sample.ini.  This is useful
-   for starting a puppetdb from the repl, allowing code changes without a restart.
-   Specify a file path for the :config parameter to use a different config file.
-   Override entries in the config with a :config-overrides map.  `config-overrides`
-   is a map similar to the ones created by ini-to-map.  Keys are sections, values are
-   a map with config keypairs."
-  [& {:keys [config config-overrides]
-      :or {config "config.sample.ini"}}]
-  (let [new-config-file (testutils/temp-file "config" ".ini")
-        config-path (absolute-path new-config-file)]
-    (println "Writing current config to" config-path)
-    (utils/spit-ini new-config-file (merge-with merge (utils/ini-to-map config) config-overrides))
-    (-main "--config" config-path)))
-
-(defn launch-mem-puppetdb
-  "Launches a puppetdb instance using an in-memory HSQL database and creates a
-   temporary dirctory for MQ storage (vardir in the config). See launch-puppetdb
-   for more info on parameters."
-  [& {:keys [config config-overrides]
-      :or {config "config.sample.ini"}}]
-  (launch-puppetdb :config-overrides (merge-with merge
-                                                 {:global {:vardir (testutils/temp-dir)}
-                                                  :database (fixt/create-db-map)}
-                                                 config-overrides)))
-
-#_(def mem-puppetdb
-     (future
-       (launch-mem-puppetdb)))
