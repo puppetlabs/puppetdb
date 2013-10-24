6e86672707433d8ac3cc273cc38b1c9513b732da
Ken Barber <ken@bob.sh>
2013-10-24 12:27:56 +0100

    Extend travis testing to test against postgres
    This expands the test.sh script to accept a third element in T_TEST that
    represents the database to test against.
    
    We now launch a database (from travis services) and create a postgres db
    when required.
    
    Signed-off-by: Ken Barber <ken@bob.sh>

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-T_LANG="${T_TEST%%:*}"
-T_VERSION="${T_TEST#*:}"
+T_TEST_ARRAY=(${T_TEST//:/ })
+
+T_LANG=${T_TEST_ARRAY[0]}
+T_VERSION=${T_TEST_ARRAY[1]}
+T_DB=${T_TEST_ARRAY[2]}
 
 echo "Running tests in language: ${T_LANG} version: ${T_VERSION}"
 
@@ -16,7 +19,22 @@ else
   if [ $T_LANG == "java" ]; then
     jdk_switcher use $T_VERSION
     java -version
-    lein2 test
+    if [ $T_DB == "postgres" ]; then
+      psql -c 'create database puppetdbtest;' -U postgres
+      PUPPETDB_DBTYPE=$T_DB \
+      PUPPETDB_DBUSER=postgres \
+      PUPPETDB_DBSUBNAME=//127.0.0.1:5432/puppetdbtest \
+      PUPPETDB_DBPASSWORD= \
+      lein2 test
+    else
+      if [ $T_DB == "embedded" ]; then
+        PUPPETDB_DBTYPE=$T_DB \
+        lein2 test
+      else
+        echo "Invalid database ${T_DB}"
+        exit 1
+      fi
+    fi
   else
     echo "Invalid language ${T_LANG}"
     exit 1
