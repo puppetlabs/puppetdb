7c2afa4b98966c05dc2b7c5e1279dc95fb7ec550
Ryan Senior <ryan.senior@puppetlabs.com>
2013-10-28 11:32:02 -0500

    Split config related fns out of services.clj and into it's own namespace

--- src/com/puppetlabs/puppetdb/cli/services.clj
+++ src/com/puppetlabs/puppetdb/cli/services.clj
@@ -43,7 +43,6 @@
 ;;   maintain acceptable performance.
 ;;
 (ns com.puppetlabs.puppetdb.cli.services
-  (:import [java.security KeyStore])
   (:require [com.puppetlabs.puppetdb.scf.storage :as scf-store]
             [com.puppetlabs.puppetdb.command :as command]
             [com.puppetlabs.puppetdb.command.dlo :as dlo]
@@ -57,13 +56,13 @@
             [clojure.tools.logging :as log]
             [com.puppetlabs.cheshire :as json]
             [com.puppetlabs.puppetdb.http.server :as server]
-            [com.puppetlabs.ssl :as ssl])
+            [com.puppetlabs.puppetdb.config :as conf])
   (:use [clojure.java.io :only [file]]
         [clj-time.core :only [ago secs minutes days]]
         [overtone.at-at :only (mk-pool interspaced)]
         [com.puppetlabs.time :only [to-secs to-millis parse-period format-period period?]]
         [com.puppetlabs.jdbc :only (with-transacted-connection)]
-        [com.puppetlabs.utils :only (cli! configure-logging! inis-to-map with-error-delivery missing?)]
+        [com.puppetlabs.utils :only (cli! with-error-delivery)]
         [com.puppetlabs.repl :only (start-repl)]
         [com.puppetlabs.puppetdb.scf.migrate :only [migrate!]]
         [com.puppetlabs.puppetdb.version :only [version update-info]]
@@ -198,80 +197,6 @@
     (check-for-updates update-server db)
     (log/debug "Skipping update check on Puppet Enterprise")))
 
-(defn configure-commandproc-threads
-  "Update the supplied config map with the number of
-  command-processing threads to use. If no value exists in the config
-  map, default to half the number of CPUs. If only one CPU exists, we
-  will use one command-processing thread."
-  [config]
-  {:pre  [(map? config)]
-   :post [(map? %)
-          (pos? (get-in % [:command-processing :threads]))]}
-  (let [default-nthreads (-> (pl-utils/num-cpus)
-                             (/ 2)
-                             (int)
-                             (max 1))]
-    (update-in config [:command-processing :threads] #(or % default-nthreads))))
-
-(defn configure-web-server-ssl-from-pems
-  "Configures the web server's SSL settings based on Puppet PEM files, rather than
-  via a java keystore (jks) file.  The configuration map returned by this function
-  will have overwritten any existing keystore-related settings to use in-memory
-  KeyStore objects, which are constructed based on the values of
-  `:ssl-key`, `:ssl-cert`, and `:ssl-ca-cert` from
-  the input map.  The output map does not include the `:puppet-*` keys, as they
-  are not meaningful to the web server implementation."
-  [{:keys [ssl-key ssl-cert ssl-ca-cert] :as jetty}]
-  {:pre  [ssl-key
-          ssl-cert
-          ssl-ca-cert]
-   :post [(map? %)
-          (instance? KeyStore (:keystore %))
-          (string? (:key-password %))
-          (instance? KeyStore (:truststore %))
-          (missing? % :trust-password :ssl-key :ssl-cert :ssl-ca-cert)]}
-  (let [old-ssl-config-keys [:keystore :truststore :key-password :trust-password]
-        old-ssl-config      (select-keys jetty old-ssl-config-keys)]
-    (when (pos? (count old-ssl-config))
-      (log/warn (format "Found settings for both keystore-based and Puppet PEM-based SSL; using PEM-based settings, ignoring %s"
-                  (keys old-ssl-config)))))
-  (let [truststore  (-> (ssl/keystore)
-                        (ssl/assoc-cert-file! "PuppetDB CA" ssl-ca-cert))
-        keystore-pw (pl-utils/uuid)
-        keystore    (-> (ssl/keystore)
-                        (ssl/assoc-private-key-file! "PuppetDB Agent Private Key" ssl-key keystore-pw ssl-cert))]
-    (-> jetty
-        (dissoc :ssl-key :ssl-ca-cert :ssl-cert :trust-password)
-        (assoc :keystore keystore)
-        (assoc :key-password keystore-pw)
-        (assoc :truststore truststore))))
-
-(defn jetty7-minimum-threads
-  "Given a thread count, make sure it meets the minimum count for Jetty 7 to
-  operate. It will return a warning if it does not, and return the minimum
-  instead of the original value.
-
-  This is to work-around a bug/feature in Jetty 7 that blocks the web server
-  when max-threads is less than the number of cpus on a system.
-
-  See: http://projects.puppetlabs.com/issues/22168 for more details.
-
-  This bug is solved in Jetty 9, so this check can probably be removed if we
-  upgrade."
-  ([threads]
-  (jetty7-minimum-threads threads (inc (pl-utils/num-cpus))))
-
-  ([threads min-threads]
-  {:pre [(pos? threads)
-         (pos? min-threads)]
-   :post [(pos? %)]}
-  (if (< threads min-threads)
-    (do
-      (log/warn (format "max-threads = %s is less than the minium allowed on this system for Jetty 7 to operate. This will be automatically increased to the safe minimum: %s"
-                  threads min-threads))
-      min-threads)
-    threads)))
-
 (defn build-whitelist-authorizer
   "Build a function that will authorize requests based on the supplied
   certificate whitelist (see `cn-whitelist->authorizer` for more
@@ -287,77 +212,6 @@
           (log/warnf "%s rejected by certificate whitelist %s" ssl-client-cn whitelist)
           false)))))
 
-(defn configure-web-server
-  "Update the supplied config map with information about the HTTP webserver to
-  start. This will specify client auth, and add a default host/port
-  http://puppetdb:8080 if none are supplied (and SSL is not specified)."
-  [{:keys [jetty] :as config}]
-  {:pre  [(map? config)]
-   :post [(map? %)
-          (missing? (:jetty %) :ssl-key :ssl-cert :ssl-ca-cert)]}
-  (let [initial-config  {:max-threads 50}
-        merged-jetty    (merge initial-config jetty)
-        pem-required-keys [:ssl-key :ssl-cert :ssl-ca-cert]
-        pem-config        (select-keys jetty pem-required-keys)]
-    (assoc config :jetty
-      (-> (condp = (count pem-config)
-            3 (configure-web-server-ssl-from-pems jetty)
-            0 jetty
-            (throw (IllegalArgumentException.
-                     (format "Found SSL config options: %s; If configuring SSL from Puppet PEM files, you must provide all of the following options: %s"
-                        (keys pem-config) pem-required-keys))))
-          (assoc :client-auth :need)
-          (assoc :max-threads (jetty7-minimum-threads (:max-threads merged-jetty)))))))
-
-(defn configure-gc-params
-  "Helper function that munges the supported permutations of our GC-related
-  `ttl` and interval settings (if present) from their config file
-  representation to our internal representation as Period objects."
-  [{:keys [database command-processing] :as config :or {database {}}}]
-  {:pre  [(map? config)]
-   :post [(map? %)
-          (= (dissoc database :gc-interval :report-ttl :node-purge-ttl :node-ttl :node-ttl-days)
-             (dissoc (:database %) :gc-interval :report-ttl :node-purge-ttl :node-ttl))
-          (period? (get-in % [:command-processing :dlo-compression-threshold]))
-          (every? period? (map (:database %) [:node-ttl :node-purge-ttl :report-ttl :gc-interval]))]}
-  (let [maybe-parse-period #(some-> % parse-period)
-        maybe-days #(some-> % days)
-        maybe-minutes #(some-> % minutes)
-        gc-interval-default (minutes 60)
-        dlo-compression-default (days 1)
-        ;; These defaults have to be actual periods rather than nil, because
-        ;; the user could explicitly specify 0, and we want to treat that the
-        ;; same
-        node-ttl-default (secs 0)
-        node-purge-ttl-default (secs 0)
-        report-ttl-default (days 14)
-        parsed-commandproc (update-in command-processing [:dlo-compression-threshold] #(or (maybe-parse-period %) dlo-compression-default))
-        parsed-database (-> database
-                            (update-in [:gc-interval] #(or (maybe-minutes %) gc-interval-default))
-                            (update-in [:report-ttl] #(or (maybe-parse-period %) report-ttl-default))
-                            (update-in [:node-purge-ttl] #(or (maybe-parse-period %) (secs 0)))
-                            (update-in [:node-ttl] #(or (maybe-parse-period %) (maybe-days (:node-ttl-days database)) node-ttl-default))
-                            (dissoc :node-ttl-days))]
-    (assoc config :database parsed-database :command-processing parsed-commandproc)))
-
-(defn default-db-config [global]
-  {:classname   "org.hsqldb.jdbcDriver"
-   :subprotocol "hsqldb"
-   :subname     (format "file:%s;hsqldb.tx=mvcc;sql.syntax_pgs=true" (file (:vardir global) "db"))})
-
-(defn configure-database
-  "Update the supplied config map with information about the database. Adds a
-  default hsqldb. If a single part of the database information is specified
-  (such as classname but not subprotocol), no defaults will be filled in."
-  [{:keys [database read-database global] :as config}]
-  {:pre  [(map? config)]
-   :post [(map? config)]}
-  (let [write-db (or database (default-db-config global))
-        read-db (or read-database write-db)]
-    (assoc config
-      :database write-db
-      :read-database (assoc read-db :read-only? true))))
-
 (defn normalize-product-name
   "Checks that `product-name` is specified as a legal value, throwing an
   exception if not. Returns `product-name` if it's okay."
@@ -369,65 +223,6 @@
       (throw (IllegalArgumentException.
                (format "product-name %s is illegal; either puppetdb or pe-puppetdb are allowed" product-name))))
     lower-product-name))
-
-(defn validate-vardir
-  "Checks that `vardir` is specified, exists, and is writeable, throwing
-  appropriate exceptions if any condition is unmet."
-  [vardir]
-  (if-let [vardir (file vardir)]
-    (cond
-     (not (.isAbsolute vardir))
-     (throw (IllegalArgumentException.
-             (format "Vardir %s must be an absolute path." vardir)))
-
-     (not (.exists vardir))
-     (throw (java.io.FileNotFoundException.
-             (format "Vardir %s does not exist. Please create it and ensure it is writable." vardir)))
-
-     (not (.isDirectory vardir))
-     (throw (java.io.FileNotFoundException.
-             (format "Vardir %s is not a directory." vardir)))
-
-     (not (.canWrite vardir))
-     (throw (java.io.FileNotFoundException.
-             (format "Vardir %s is not writable." vardir))))
-
-    (throw (IllegalArgumentException.
-            "Required setting 'vardir' is not specified. Please set it to a writable directory.")))
-  vardir)
-
-(defn set-global-configuration!
-  "Store away global configuration"
-  [config]
-  {:pre  [(map? config)]
-   :post [(map? %)]}
-  (def configuration config)
-  config)
-
-(defn parse-config!
-  "Parses the given config file/directory and configures its various
-  subcomponents.
-
-  Also accepts an optional map argument 'initial-config'; if
-  provided, any initial values in this map will be included
-  in the resulting config map."
-  ([path]
-    (parse-config! path {}))
-  ([path initial-config]
-    {:pre [(string? path)
-           (map? initial-config)]}
-    (let [file (file path)]
-      (when-not (.canRead file)
-        (throw (IllegalArgumentException.
-                (format "Configuration path '%s' must exist and must be readable." path)))))
-
-    (-> (merge initial-config (inis-to-map path))
-        (configure-logging!)
-        (configure-commandproc-threads)
-        (configure-web-server)
-        (configure-database)
-        (configure-gc-params)
-        (set-global-configuration!))))
 
 (defn on-shutdown
   "General cleanup when a shutdown request is received."
@@ -450,9 +245,9 @@
                                                       required-cli-options)
         initial-config                             {:debug (:debug options)}
         {:keys [jetty database read-database global command-processing]
-            :as config}                            (parse-config! (:config options) initial-config)
+            :as config}                            (conf/parse-config (:config options) initial-config)
         product-name                               (normalize-product-name (get global :product-name "puppetdb"))
-        vardir                                     (validate-vardir (:vardir global))
+        vardir                                     (conf/validate-vardir (:vardir global))
         update-server                              (:update-server global "http://updates.puppetlabs.com/check-for-updates")
         ;; TODO: revisit the choice of 20000 as a default value for event queries
         event-query-limit                          (get global :event-query-limit 20000)
---
+++ test/com/puppetlabs/puppetdb/test/config.clj
@@ -0,0 +1,199 @@
+(ns com.puppetlabs.puppetdb.test.config
+  (:import [java.security KeyStore])
+  (require [clojure.test :refer :all]
+           [com.puppetlabs.puppetdb.config :refer :all]
+           [com.puppetlabs.utils :as utils]
+           [com.puppetlabs.time :as pl-time]
+           [clj-time.core :as time]
+           [com.puppetlabs.testutils.logging :as tu-log]
+           [clojure.java.io :as io]))
+
+(deftest commandproc-configuration
+  (testing "should use the thread value specified"
+    (let [config (configure-commandproc-threads {:command-processing {:threads 37}})]
+      (is (= (get-in config [:command-processing :threads]) 37))))
+
+  (let [with-ncores (fn [cores]
+                      (with-redefs [utils/num-cpus (constantly cores)]
+                        (-> (configure-commandproc-threads {})
+                            (get-in [:command-processing :threads]))))]
+    (testing "should default to half the available CPUs"
+      (is (= (with-ncores 4) 2)))
+    (testing "should default to half the available CPUs, rounding down"
+      (is (= (with-ncores 5) 2)))
+    (testing "should default to half the available CPUs, even on single core boxes"
+      (is (= (with-ncores 1) 1)))))
+
+(deftest database-configuration
+  (testing "database"
+    (testing "should use the value specified"
+      (let [config (configure-database {:database {:classname "something"}})]
+        (is (= (get-in config [:database :classname]) "something"))
+        (is (nil? (get-in config [:database :subprotocol])))
+        (is (nil? (get-in config [:database :subname])))))
+
+    (testing "should default to hsqldb"
+      (let [config (configure-database {:global {:vardir "/var/lib/puppetdb"}})
+            expected {:classname "org.hsqldb.jdbcDriver"
+                      :subprotocol "hsqldb"
+                      :subname "file:/var/lib/puppetdb/db;hsqldb.tx=mvcc;sql.syntax_pgs=true"}]
+        (is (= (select-keys (:database config) #{:classname :subprotocol :subname})
+               expected))))
+
+    (testing "the read-db defaulted to the specified write-db"
+      (let [config (configure-database {:database {:classname "something"}})]
+        (is (= (get-in config [:read-database :classname]) "something"))
+        (is (nil? (get-in config [:read-database :subprotocol])))
+        (is (nil? (get-in config [:read-database :subname])))))
+
+    (testing "the read-db defaulted to the hsql write-db-default"
+      (let [config (configure-database {:global {:vardir "/var/lib/puppetdb"}})
+            expected {:classname "org.hsqldb.jdbcDriver"
+                      :subprotocol "hsqldb"
+                      :subname "file:/var/lib/puppetdb/db;hsqldb.tx=mvcc;sql.syntax_pgs=true"}]
+        (is (= (select-keys (:read-database config) #{:classname :subprotocol :subname})
+               expected))))
+
+    (testing "the read-db should be specified by a read-database property"
+      (let [config (configure-database {:read-database {:classname "something"}})]
+        (is (= (get-in config [:read-database :classname]) "something"))
+        (is (nil? (get-in config [:read-database :subprotocol])))
+        (is (nil? (get-in config [:read-database :subname])))))))
+
+(deftest garbage-collection
+  (testing "gc-interval"
+    (testing "should use the value specified in minutes"
+      (let [{:keys [gc-interval]} (:database (configure-gc-params {:database {:gc-interval 900}}))]
+        (is (pl-time/period? gc-interval))
+        (is (= (time/minutes 900) gc-interval))))
+
+    (testing "should default to 60 minutes"
+      (let [{:keys [gc-interval]} (:database (configure-gc-params {}))]
+        (is (pl-time/period? gc-interval))
+        (is (= (time/minutes 60) gc-interval)))))
+
+  (testing "node-ttl"
+    (testing "should parse node-ttl and return a Pl-Time/Period object"
+      (let [{:keys [node-ttl]} (:database (configure-gc-params { :database { :node-ttl "10d" }}))]
+        (is (pl-time/period? node-ttl))
+        (is (= (time/days 10) (time/days (pl-time/to-days node-ttl))))))
+    (testing "should support node-ttl-days for backward compatibility"
+      (let [{:keys [node-ttl] :as dbconfig} (:database (configure-gc-params { :database { :node-ttl-days 10 }}))]
+        (is (pl-time/period? node-ttl))
+        (is (= (time/days 10) node-ttl))
+        (is (not (contains? dbconfig :node-ttl-days)))))
+    (testing "should prefer node-ttl over node-ttl-days"
+      (let [{:keys [node-ttl] :as dbconfig} (:database (configure-gc-params { :database {:node-ttl "5d"
+                                                                                        :node-ttl-days 10 }}))]
+        (is (pl-time/period? node-ttl))
+        (is (= (time/days 5) (time/days (pl-time/to-days node-ttl))))
+        (is (not (contains? dbconfig :node-ttl-days)))))
+    (testing "should default to zero (no expiration)"
+      (let [{:keys [node-ttl] :as dbconfig} (:database (configure-gc-params {}))]
+        (is (pl-time/period? node-ttl))
+        (is (= (time/secs 0) node-ttl)))))
+
+  (testing "report-ttl"
+    (testing "should parse report-ttl and produce report-ttl"
+      (let [{:keys [report-ttl]} (:database (configure-gc-params { :database { :report-ttl "10d" }}))]
+        (is (pl-time/period? report-ttl))
+        (is (= (time/days 10) (time/days (pl-time/to-days report-ttl))))))
+    (testing "should default to 14 days"
+      (let [{:keys [report-ttl]} (:database (configure-gc-params {}))]
+        (is (pl-time/period? report-ttl))
+        (is (= (time/days 14) (time/days (pl-time/to-days report-ttl))))))))
+
+(deftest jetty7-minimum-threads-test
+  (testing "should return the same number when higher than num-cpus"
+    (is (= 500 (jetty7-minimum-threads 500 1))))
+  (testing "should set the number to min threads when it is higher and return a warning"
+    (tu-log/with-log-output logs
+      (is (= 4 (jetty7-minimum-threads 1 4)))
+      (is (= 1 (count (tu-log/logs-matching #"max-threads = 1 is less than the minium allowed on this system for Jetty 7 to operate." @logs)))))))
+
+(deftest http-configuration
+  (testing "should enable need-client-auth"
+    (let [config (configure-web-server {:jetty {:client-auth false}})]
+      (is (= (get-in config [:jetty :client-auth]) :need))))
+  (let [old-config {:keystore       "/some/path"
+                    :key-password   "pw"
+                    :truststore     "/some/other/path"
+                    :trust-password "otherpw"}]
+    (testing "should not muck with keystore/truststore settings if PEM-based SSL
+              settings are not provided"
+      (let [processed-config (:jetty (configure-web-server {:jetty old-config}))]
+        (is (= old-config
+               (select-keys processed-config
+                 [:keystore :key-password :truststore :trust-password])))))
+    (testing "should fail if some but not all of the PEM-based SSL settings are found"
+      (let [partial-pem-config (merge old-config {:ssl-ca-cert "/some/path"})]
+        (is (thrown-with-msg? java.lang.IllegalArgumentException
+              #"If configuring SSL from Puppet PEM files, you must provide all of the following options"
+              (configure-web-server {:jetty partial-pem-config})))))
+
+    (let [pem-config (merge old-config
+                        {:ssl-key     (io/resource "com/puppetlabs/test/ssl/private_keys/localhost.pem")
+                         :ssl-cert    (io/resource "com/puppetlabs/test/ssl/certs/localhost.pem")
+                         :ssl-ca-cert (io/resource "com/puppetlabs/test/ssl/certs/ca.pem")})]
+      (testing "should warn if both keystore-based and PEM-based SSL settings are found"
+        (tu-log/with-log-output logs
+          (configure-web-server {:jetty pem-config})
+          (is (= 1 (count (tu-log/logs-matching #"Found settings for both keystore-based and Puppet PEM-based SSL" @logs))))))
+      (testing "should prefer PEM-based SSL settings, override old keystore settings
+                  with instances of java.security.KeyStore, and remove PEM settings
+                  from final jetty config hash"
+        (let [processed-config (:jetty (configure-web-server {:jetty pem-config}))]
+          (is (instance? KeyStore (:keystore processed-config)))
+          (is (instance? KeyStore (:truststore processed-config)))
+          (is (string? (:key-password processed-config)))
+          (is (not (contains? processed-config :trust-password)))
+          (is (not (contains? processed-config :ssl-key)))
+          (is (not (contains? processed-config :ssl-cert)))
+          (is (not (contains? processed-config :ssl-ca-cert)))))))
+  (testing "should set max-threads"
+    (let [config (configure-web-server {:jetty {}})]
+      (is (contains? (:jetty config) :max-threads))))
+  (testing "should merge configuration with initial-configs correctly"
+    (let [user-config {:jetty {:truststore "foo"}}
+          config      (configure-web-server user-config)]
+      (is (= config {:jetty {:truststore "foo" :max-threads 50 :client-auth :need}})))
+    (let [user-config {:jetty {:max-threads 500 :truststore "foo"}}
+          config      (configure-web-server user-config)]
+      (is (= config {:jetty {:truststore "foo" :max-threads 500 :client-auth :need}})))))
+
+(deftest vardir-validation
+  (testing "should fail if it's not specified"
+    (is (thrown-with-msg? IllegalArgumentException #"is not specified"
+          (validate-vardir nil))))
+
+  (testing "should fail if it's not an absolute path"
+    (is (thrown-with-msg? IllegalArgumentException #"must be an absolute path"
+          (validate-vardir "foo/bar/baz"))))
+
+  (testing "should fail if it doesn't exist"
+    (is (thrown-with-msg? java.io.FileNotFoundException #"does not exist"
+          (validate-vardir "/abc/def/ghi"))))
+
+  (testing "should fail if it's not a directory"
+    (let [filename (doto (java.io.File/createTempFile "not_a" "directory")
+                     (.deleteOnExit))]
+      (is (thrown-with-msg? java.io.FileNotFoundException #"is not a directory"
+            (validate-vardir filename)))))
+
+  (testing "should fail if it's not writable"
+    (let [filename (doto (java.io.File/createTempFile "not" "writable")
+                     (.deleteOnExit)
+                     (.delete)
+                     (.mkdir)
+                     (.setReadOnly))]
+      (is (thrown-with-msg? java.io.FileNotFoundException #"is not writable"
+            (validate-vardir filename)))))
+
+  (testing "should return the value if everything is okay"
+    (let [filename (doto (java.io.File/createTempFile "totally" "okay")
+                     (.deleteOnExit)
+                     (.delete)
+                     (.mkdir)
+                     (.setWritable true))]
+      (is (= (validate-vardir filename) filename)))))
+
