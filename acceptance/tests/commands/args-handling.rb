1d635e37b089e823eabd72864cebca64c3871670
Ken Barber <ken@bob.sh>
2013-11-08 12:47:12 +0000

    Fix cli with no argument handling
    Seems the cli.tools 0.2.2 library decided to change its API and is now
    not returning nil valued entries when there is no default. This has played
    havoc with our required-args detection, meaning that when you attempt to
    call a command without any arguments you get a nasty exception.
    
    This patch fixes this, and adds acceptance tests to ensure we don't regress.
    
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ acceptance/tests/commands/args-handling.rb
@@ -0,0 +1,15 @@
+test_name "argument handling" do
+  ["ssl-setup", "import", "export", "anonymize"].each do |k|
+    step "running puppetdb-#{k} -h should not error out" do
+      on database, "/usr/sbin/puppetdb-#{k} -h"
+    end
+
+    # We don't test ssl-setup here, because its actions without any arguments
+    # are tested elsewhere.
+    next if k == 'ssl-setup'
+    step "running puppetdb-#{k} with no arguments should not error out" do
+      on database, "/usr/sbin/puppetdb-#{k}", :acceptable_exit_codes => [1]
+      assert_match(/Missing required argument/, stdout)
+    end
+  end
+end
