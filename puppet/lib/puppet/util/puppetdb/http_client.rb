25982e4390b1c2a2a0754e85b9617c4965e234ea
Chris Price <chris@puppetlabs.com>
2014-03-25 17:36:59 -0700

    Use a different HTTP client for JRuby

--- puppet/lib/puppet/util/puppetdb/http_client.rb
+++ puppet/lib/puppet/util/puppetdb/http_client.rb
@@ -3,6 +3,11 @@ require 'puppet/network/http_pool'
 
 class Puppet::Util::Puppetdb::HttpClient
   def self.instance(server, port)
-    Puppet::Network::HttpPool.http_instance(server, port)
+    if RUBY_PLATFORM == "java"
+      JRubyHttpClient.instance(server, port)
+    else
+      Puppet::Network::HttpPool.http_instance(server, port)
+    end
+
   end
 end
