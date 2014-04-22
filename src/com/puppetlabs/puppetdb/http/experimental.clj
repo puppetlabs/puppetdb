e9cd65a9be9387ae62524cbcf2ed51e7cd5753f9
Ryan Senior <ryan.senior@puppetlabs.com>
2014-04-22 06:25:26 -0500

    (PDB-570) Remove planetarium endpoint
    The project that was using this endpoint is no longer being used, so
    this endpoint (and related code) could be removed.

--- src/com/puppetlabs/puppetdb/http/experimental.clj
+++ src/com/puppetlabs/puppetdb/http/experimental.clj
@@ -1,12 +1,8 @@
 (ns com.puppetlabs.puppetdb.http.experimental
-  (:use [com.puppetlabs.puppetdb.http.experimental.planetarium-catalog :only (planetarium-catalog-app)]
-        [com.puppetlabs.puppetdb.http.experimental.population :only (population-app)]
+  (:use [com.puppetlabs.puppetdb.http.experimental.population :only (population-app)]
         [net.cgrand.moustache :only (app)]))
 
 (def experimental-app
   (app
-    ["planetarium-catalog" &]
-    {:any planetarium-catalog-app}
-
     ["population" &]
     {:any population-app}))
