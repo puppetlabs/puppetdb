6c9ad0de60e086fe98ab06fa27e01854a5407f91
Ken Barber <ken@bob.sh>
2014-04-23 00:36:43 +0100

    Use rubygem-sqlite3 for fedora 19/20
    Signed-off-by: Ken Barber <ken@bob.sh>

--- acceptance/setup/pre_suite/40_install_deps.rb
+++ acceptance/setup/pre_suite/40_install_deps.rb
@@ -61,7 +61,7 @@ step "Install rubygems and sqlite3 on master" do
       on master, "yum install -y rubygems ruby-sqlite3 rubygem-activerecord"
     end
   when :fedora
-    on master, "yum install -y rubygems ruby-sqlite3"
+    on master, "yum install -y rubygems rubygem-sqlite3"
     on master, "gem install activerecord -v 3.2.17 --no-ri --no-rdoc -V --backtrace"
   when :debian
     on master, "apt-get install -y rubygems libsqlite3-ruby"
