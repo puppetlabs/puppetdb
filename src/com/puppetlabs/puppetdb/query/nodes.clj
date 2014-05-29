5e2449bfca0c6caa232b24988a3bc1a96a08c703
Ken Barber <ken@bob.sh>
2014-05-29 15:13:23 +0100

    (PDB-660) Turn on streaming for nodes end-point
    This turns on streaming for the nodes end-point.
    
    Signed-off-by: Ken Barber <ken@bob.sh>

--- src/com/puppetlabs/puppetdb/query/nodes.clj
+++ src/com/puppetlabs/puppetdb/query/nodes.clj
@@ -4,14 +4,10 @@
 ;; spec](../spec/node.md).
 ;;
 (ns com.puppetlabs.puppetdb.query.nodes
-  (:refer-clojure :exclude [compile conj! distinct disj! drop sort take])
-  (:require [clojure.string :as string]
-            [puppetlabs.kitchensink.core :as kitchensink]
-            [com.puppetlabs.jdbc :as jdbc])
-  (:use [com.puppetlabs.puppetdb.scf.storage-utils :only [db-serialize sql-array-query-string sql-as-numeric]]
-        [clojure.core.match :only [match]]
-        [com.puppetlabs.puppetdb.query :only [node-query->sql node-operators execute-query]]
-        [com.puppetlabs.puppetdb.query.paging :only [validate-order-by!]]))
+  (:require [puppetlabs.kitchensink.core :as kitchensink]
+            [com.puppetlabs.jdbc :as jdbc]
+            [com.puppetlabs.puppetdb.query :as query]
+            [com.puppetlabs.puppetdb.query.paging :refer [validate-order-by!]]))
 
 (defn node-columns
   "Return node columns based on version"
@@ -25,59 +21,76 @@
 (defn query->sql
   "Converts a vector-structured `query` to a corresponding SQL query which will
   return nodes matching the `query`."
-  [version query]
-  {:pre  [((some-fn nil? sequential?) query)]
-   :post [(jdbc/valid-jdbc-query? %)]}
-  (let [operators (node-operators version)
-        [subselect & params] (node-query->sql version operators query)
-        sql (case version
-              (:v1 :v2 :v3)
-              (format "SELECT subquery1.name,
-                       subquery1.deactivated,
-                       catalogs.timestamp AS catalog_timestamp,
-                       certname_facts_metadata.timestamp AS facts_timestamp,
-                       reports.end_time AS report_timestamp
-                       FROM (%s) subquery1
-                         LEFT OUTER JOIN catalogs
-                           ON subquery1.name = catalogs.certname
-                         LEFT OUTER JOIN certname_facts_metadata
-                           ON subquery1.name = certname_facts_metadata.certname
-                         LEFT OUTER JOIN reports
-                           ON subquery1.name = reports.certname
-                             AND reports.hash
-                               IN (SELECT report FROM latest_reports)
-                       ORDER BY subquery1.name ASC" subselect)
+  ([version query]
+     (query->sql version query {}))
+  ([version query paging-options]
+     {:pre  [((some-fn nil? sequential?) query)]
+      :post [(map? %)
+             (jdbc/valid-jdbc-query? (:results-query %))
+             (or
+              (not (:count? paging-options))
+              (jdbc/valid-jdbc-query? (:count-query %)))]}
+     (validate-order-by! (node-columns version) paging-options)
+     (let [operators (query/node-operators version)
+           [subselect & params] (query/node-query->sql version operators query)
+           sql (case version
+                 (:v1 :v2 :v3)
+                 (format "SELECT subquery1.name,
+                          subquery1.deactivated,
+                          catalogs.timestamp AS catalog_timestamp,
+                          certname_facts_metadata.timestamp AS facts_timestamp,
+                          reports.end_time AS report_timestamp
+                          FROM (%s) subquery1
+                            LEFT OUTER JOIN catalogs
+                              ON subquery1.name = catalogs.certname
+                            LEFT OUTER JOIN certname_facts_metadata
+                              ON subquery1.name = certname_facts_metadata.certname
+                            LEFT OUTER JOIN reports
+                              ON subquery1.name = reports.certname
+                                AND reports.hash
+                                  IN (SELECT report FROM latest_reports)
+                          ORDER BY subquery1.name ASC" subselect)
 
-              ;; For :v4 the query now all lives in node-query->sql
-              subselect)]
-    (apply vector sql params)))
+                 ;; For :v4 the query now all lives in node-query->sql
+                 subselect)
+           paged-select (jdbc/paged-sql sql paging-options)
+           result {:results-query (apply vector paged-select params)}]
+       (if (:count? paging-options)
+         (assoc result :count-query (apply vector (jdbc/count-sql subselect) params))
+         result))))
+
+(defn munge-result-rows
+  [version]
+  (case version
+    (:v1 :v2 :v3) identity
+
+    (fn [rows]
+      (map
+       #(kitchensink/mapkeys jdbc/underscores->dashes %)
+       rows))))
 
 (defn query-nodes
   "Search for nodes satisfying the given SQL filter."
-  ([version filter-expr] (query-nodes version filter-expr nil))
-  ([version filter-expr paging-options]
-     {:pre  [(jdbc/valid-jdbc-query? filter-expr)]
-      :post [(map? %)
-             (sequential? (:result %))]}
-     (validate-order-by! (node-columns version) paging-options)
-     (let [results (execute-query filter-expr paging-options)]
-       (case version
-         (:v1 :v2 :v3)
-         results
-
-         (assoc results :result
-                (map
-                 #(kitchensink/mapkeys jdbc/underscores->dashes %)
-                 (:result results)))))))
+  [version query-sql]
+  {:pre  [(map? query-sql)
+          (jdbc/valid-jdbc-query? (:results-query query-sql))]
+   :post [(map? %)
+          (sequential? (:result %))]}
+  (let [{[sql & params] :results-query
+         count-query    :count-query} query-sql
+         result {:result (query/streamed-query-result
+                          version sql params
+                          (comp doall (munge-result-rows version)))}]
+    (if count-query
+      (assoc result :count (jdbc/get-result-count count-query))
+      result)))
 
 (defn status
   "Given a node's name, return the current status of the node.  Results
   include whether it's active and the timestamp of its most recent catalog, facts,
   and report."
   [version node]
-  {:pre  [string? node]
-   :post [(or (nil? %)
-              (= (set (node-columns version)) (kitchensink/keyset %)))]}
+  {:pre  [string? node]}
   (let [sql     (query->sql version ["=" "name" node])
-        results (jdbc/query-to-vec sql)]
+        results (:result (query-nodes version sql))]
     (first results)))
