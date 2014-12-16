c115e444053f0eebdca8b318e3dfc8e2e7a99470
Mathieu Parent <math.parent@gmail.com>
2014-12-16 17:10:18 +0100

    Ensure ruby 2.1.8 is not matched as ruby 1.8
    Just in case

--- puppet/lib/puppet/util/puppetdb/char_encoding.rb
+++ puppet/lib/puppet/util/puppetdb/char_encoding.rb
@@ -33,7 +33,7 @@ module CharEncoding
 
 
   def self.utf8_string(str)
-    if RUBY_VERSION =~ /1.8/
+    if RUBY_VERSION =~ /^1.8/
       # Ruby 1.8 doesn't have String#encode and related methods, and there
       #  appears to be a bug in iconv that will interpret some byte sequences
       #  as 6-byte characters.  Thus, we are forced to resort to some unfortunate
