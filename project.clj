31f0f223f2df81020601bcc799750af9bca8681b
Ken Barber <ken@bob.sh>
2015-02-18 01:28:43 +0000

    Bump to latest ezbake plugin
    This bumps us to use ezbake 0.2.2, which at the very least solves logrotate
    pathing mistakes in 0.2.1.
    
    Signed-off-by: Ken Barber <ken@bob.sh>

--- project.clj
+++ project.clj
@@ -120,7 +120,7 @@
              :ezbake {:dependencies ^:replace [[puppetlabs/puppetdb ~pdb-version]
                                                [org.clojure/tools.nrepl "0.2.3"]]
                       :name "puppetdb"
-                      :plugins [[puppetlabs/lein-ezbake "0.2.1"
+                      :plugins [[puppetlabs/lein-ezbake "0.2.2"
                                  :exclusions [org.clojure/clojure]]]}
              :ci {:plugins [[lein-pprint "1.1.1"]]}}
 
