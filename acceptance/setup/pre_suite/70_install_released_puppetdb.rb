3877436865a08078f8be0270e44e1e4cee0bba23
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-05-22 13:36:45 -0700

    (maint) Pass test_url to install_puppetdb helper function

--- acceptance/setup/pre_suite/70_install_released_puppetdb.rb
+++ acceptance/setup/pre_suite/70_install_released_puppetdb.rb
@@ -2,9 +2,10 @@
 if (test_config[:install_mode] == :upgrade and not test_config[:skip_presuite_provisioning])
   step "Install most recent released PuppetDB on the PuppetDB server for upgrade test" do
     databases.each do |database|
-      install_puppetdb(database, test_config[:database], 'latest')
-      start_puppetdb(database, "/v3/version")
-      install_puppetdb_termini(master, database, 'latest', 'puppetdb-terminus', "/v3/version")
+      test_url='/v3/version'
+      install_puppetdb(database, test_config[:database], 'latest', test_url)
+      start_puppetdb(database, test_url)
+      install_puppetdb_termini(master, database, 'latest', 'puppetdb-terminus', test_url)
     end
   end
 end
