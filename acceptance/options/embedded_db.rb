1c5859ab337bdb1c18710791df7acc3d68f5d1ae
John Duarte <john.duarte@puppetlabs.com>
2015-05-28 12:45:50 -0700

    (PDB-1300) Set conditional beaker options for AIO
    This commit adds conditional logic to the embedded_db.rb and
    postgres.rb beaker options files to include puppetserver options
    when the beaker `type` option is set to `aio`.

--- acceptance/options/embedded_db.rb
+++ acceptance/options/embedded_db.rb
@@ -3,6 +3,10 @@
 #  a variable 'options_file_path', though.
 require File.expand_path(File.join(File.dirname(options_file_path), 'common.rb'))
 
-common_options_hash.merge({
-  :puppetdb_database => 'embedded',
-})
+common_options_hash.tap do |my_hash|
+    my_hash[:puppetdb_database]      = 'embedded'
+    my_hash[:is_puppetserver]        = 'true'                                 if ENV['BEAKER_TYPE'] == 'aio'
+    my_hash[:'use-service']          = 'true'                                 if ENV['BEAKER_TYPE'] == 'aio'
+    my_hash[:'puppetserver-confdir'] = '/etc/puppetlabs/puppetserver/conf.d'  if ENV['BEAKER_TYPE'] == 'aio'
+    my_hash[:puppetservice]          = 'puppetserver'                         if ENV['BEAKER_TYPE'] == 'aio'
+end
