7ff77e0c92889cb08116da708d793b2fc0b33416
Russell Mull <russell.mull@puppetlabs.com>
2015-06-08 16:54:20 -0700

    (maint) Disable sync when interval is zero
    This is a very natural thing to write in a config file.
    
    Also throw :puppetlabs.puppetdb.config/configuration exceptions on
    invalid config, instead of InvalidArgumentException.

---
+++ test/puppetlabs/pe_puppetdb_extensions/sync/services_test.clj
@@ -0,0 +1,29 @@
+(ns puppetlabs.pe-puppetdb-extensions.sync.services-test
+  (:require [puppetlabs.pe-puppetdb-extensions.sync.services :refer :all]
+            [clojure.test :refer :all]
+            [slingshot.test :refer :all]))
+
+(deftest enable-periodic-sync?-test
+  (testing "Happy case"
+    (is (= true (enable-periodic-sync?
+                 [{:endpoint "http://foo.bar:8080", :interval 12}]))))
+
+  (testing "Disable sync cases"
+    (are [remote-config] (= false (enable-periodic-sync? remote-config))
+      [{:endpoint "http://foo.bar:8080", :interval 0}]
+      [{:endpoint "http://foo.bar:8080"}]
+      []
+      nil))
+
+  (testing "Invalid sync configs"
+    (are [remote-config] (thrown+? [:type :puppetlabs.puppetdb.config/configuration-error]
+                                   (enable-periodic-sync? remote-config))
+
+      [{:endpoint "http://foo.bar:8080", :interval "NOT A NUMBER!"}]
+
+      [{:endpoint "http://foo.bar:8080", :interval nil}]
+
+      [{:endpoint "http://foo.bar:8080", :interval -12}]
+
+      [{:endpoint "http://foo.bar:8080", :interval 42}
+       {:endpoint "http://baz.zap:8080", :interval 42}])))
