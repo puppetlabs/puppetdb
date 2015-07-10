807b4bb0cfbe1662e3e1e1677edc3294904563a5
Wyatt Alt <wyaltster@gmail.com>
2015-07-10 02:44:06 -0700

    Revert "(maint) make install_puppetdb and install_puppetdb_terminus versions nil"

--- acceptance/setup/pre_suite/70_install_released_puppetdb.rb
+++ acceptance/setup/pre_suite/70_install_released_puppetdb.rb
@@ -1,8 +1,8 @@
 # We skip this step entirely unless we are running in :upgrade mode.
 if (test_config[:install_mode] == :upgrade)
   step "Install most recent released PuppetDB on the PuppetDB server for upgrade test" do
-    install_puppetdb(database, test_config[:database])
+    install_puppetdb(database, test_config[:database], 'latest')
     start_puppetdb(database)
-    install_puppetdb_termini(master, database)
+    install_puppetdb_termini(master, database, 'latest')
   end
 end
