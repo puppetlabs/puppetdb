d406d456c0474b59dfcfead567d730797f3aea58
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-07-09 17:23:14 -0700

    (maint) Fix travis install for non-master
    This commit also fixes the command to check to see if the dependency
    tag of PuppetDB exists when install the FOSS puppetdb repo.
    This commit also upgrades travis to use the new container based CI, by
    setting sudo: false to avoid annoying messages.

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -8,8 +8,8 @@ LEIN="${1:-lein2}"
 # get_dep_version
 # :arg1 is the dependency project whose version we want to grab from project.clj
 get_dep_version() {
-	local REGEX="puppetlabs\/${1:?} "
-	$LEIN pprint :dependencies | grep "$REGEX" | cut -d\" -f2
+  local REGEX="puppetlabs\/puppetdb\s*\"([[:alnum:]]|\-|\.)+\"[[:space:]]*\]"
+  echo "$($LEIN pprint :dependencies | egrep $REGEX | cut -d\" -f2)"
 }
 
 rm -rf checkouts && mkdir checkouts
@@ -24,7 +24,7 @@ git clone https://github.com/puppetlabs/puppetdb
 # the dependency version. If we can't find it, just use the master branch.
 depversion="$(get_dep_version 'puppetdb')"
 pushd 'puppetdb'
-if [ -n "$(git tag -l | grep $depversion | awk '{print $2;}')" ]
+if git tag -l $depversion
 then
     git checkout $depversion
 fi
