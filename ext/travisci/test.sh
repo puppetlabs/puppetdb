3b088248158b266c3a1578dbeb6bc45cb47c037c
Ryan Senior <ryan.senior@puppetlabs.com>
2015-07-10 17:21:23 -0500

    (maint) Fix travis script to checkout similar branch when SNAPSHOT
    Currently the test.sh file that Travis uses attempts to find the version
    of PuppetDB from the project.clj. If found and version exists as a tag,
    it builds that version and uses it. Previously if a tag was not found it
    always used master. This commit changes that to use the branch on PDB
    that is the same name as the current branch. So if we're on stable, and
    no tag is found for "3.0.1-SNAPSHOT" it will checkout stable from PDB
    and use that for the test run.

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -4,6 +4,7 @@ set -x
 set -e
 
 LEIN="${1:-lein2}"
+pdb_ext_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 # get_dep_version
 # :arg1 is the dependency project whose version we want to grab from project.clj
@@ -24,9 +25,13 @@ git clone https://github.com/puppetlabs/puppetdb
 # the dependency version. If we can't find it, just use the master branch.
 depversion="$(get_dep_version 'puppetdb')"
 pushd 'puppetdb'
-if git tag -l $depversion
+
+tag="$(git tag -l "${depversion?}")"
+if test -n "${tag?}"
 then
-    git checkout $depversion
+    git checkout "$depversion"
+else
+    git checkout "$pdb_ext_branch"
 fi
 $LEIN install
 popd
