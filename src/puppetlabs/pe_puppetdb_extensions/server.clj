f387094fbd6903d445994add94cca49467d670e3
Ryan Senior <ryan.senior@puppetlabs.com>
2015-08-21 09:38:31 -0700

    (maint) Remove wrapping of puppetdb-middleware
    This change follows a similar one on the PDB core side. This middleware
    is now being added to the whole application and it's not needed on each
    invidual route anymore.

--- src/puppetlabs/pe_puppetdb_extensions/server.clj
+++ src/puppetlabs/pe_puppetdb_extensions/server.clj
@@ -20,7 +20,6 @@
   * `authorizer` - a function that takes a request and returns a
     :authorized if the request is authorized, or a user-visible reason if not.
     If not supplied, we default to authorizing all requests."
-  [query-fn get-authorizer]
-  (-> (routes (compojure/context "/v1" [] (v1-app query-fn))
-              (route/not-found "Not Found"))
-      (wrap-with-puppetdb-middleware get-authorizer)))
+  [query-fn]
+  (routes (compojure/context "/v1" [] (v1-app query-fn))
+          (route/not-found "Not Found")))
