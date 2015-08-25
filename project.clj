4aa8674705eae7e01b2bb7fc43693d353dc1e1da
Ryan Senior <ryan.senior@puppetlabs.com>
2015-08-25 10:25:38 -0500

    (MAINT) Bump to 3.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "3.1.0")
-(def pe-pdb-version "3.1.0")
+(def pdb-version "3.1.1-SNAPSHOT")
+(def pe-pdb-version "3.1.1-SNAPSHOT")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
