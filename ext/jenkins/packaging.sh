481781260e74c0274ab527fbe1a1fc883fb1dd2f
Wayne <wayne@puppetlabs.com>
2015-09-02 12:16:26 -0700

    (QENG-2198) Move PuppetDB acceptance over to vmpooler.
    * Add sqa-utils gem.
    * Rewrite `acceptance/setup/pre_suite/80_add_dev_repo.rb` to use
      `install_puppetlabs_dev_repo` instead of the special stuff it was doing.
    * Add logic to `ext/jenkins/beaker-tests.sh` that will allow PR and merge tests
      to run without making changes to ci-job-configs for vmpooler (those changes
      are coming though!)
    * In `ext/jenkins/packaging.sh` provide `PUPPETDB_PACKAGE_BUILD_VERSION` instead
      of `PACKAGE_BUILD_VERSION` to avoid having to convert to that variable in
      `ext/jenkins/beaker-tests.sh`.
    * Remove S3 packaging/cleanup stuff.
    * Set default beaker type to `aio`. This isn't really necessary, but the test
      suite doesn't work with `foss`, at least not on stable branch.
    * BEAKER_CONFIG should take a filename. Same applies to BEAKER_OPTIONS. This
      change matches up with the way we treat variables of the same name in CI.
    
    (cherry picked from commit 1698d86ae521d769d67f6e179a0371b3388b3e2c)

--- ext/jenkins/packaging.sh
+++ ext/jenkins/packaging.sh
@@ -11,63 +11,25 @@ echo "REPO_URL: ${REPO_URL}"
 echo "**********************************************"
 
 set -x
+set -e
 
 export COW="base-jessie-amd64.cow base-precise-amd64.cow base-trusty-amd64.cow base-wheezy-amd64.cow base-wily-amd64.cow base-xenial-amd64.cow"
 export MOCK="pl-el-6-x86_64 pl-el-7-x86_64"
 tmp_m2=$(pwd)/$(mktemp -d m2-local.XXXX)
 
-set -e
 lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- install
 lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- deps
 # The ci-voom profile has nexus credentials
 lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile user,ci-voom deploy
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile ezbake ezbake build
-set +e
+lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile ezbake ezbake stage
 
 pushd "target/staging"
-PACKAGE_BUILD_VERSION=$(rake pl:print_build_param[ref] | tail -n 1)
+rake package:bootstrap
+rake pl:jenkins:uber_build[5]
 
 cat > "${WORKSPACE}/puppetdb.packaging.props" <<PROPS
-PACKAGE_BUILD_VERSION=${PACKAGE_BUILD_VERSION}
+PUPPETDB_PACKAGE_BUILD_VERSION=$(rake pl:print_build_param[ref] | tail -n 1)
 PROPS
 popd
 
-echo "**********************************************"
-echo "NOW DO S3 MAGIC"
-echo "**********************************************"
-
-REPO_DIR=/opt/jenkins-builds/puppetdb/$PACKAGE_BUILD_VERSION/repos
-REPO_CONFIGS=/opt/jenkins-builds/puppetdb/$PACKAGE_BUILD_VERSION/repo_configs
-NAME=puppetdb
-BUCKET_NAME=${NAME}-prerelease
-
-S3_BRANCH_PATH=s3://${BUCKET_NAME}/${NAME}/${PACKAGE_BUILD_VERSION}
-
 set -x
-
-# Set ship targets and build team for packaging repo
-REPO_HOST=builds.delivery.puppetlabs.net
-export REPO_DIR REPO_HOST
-
-
-# Now rebuild the metadata
-# We add the `--follow-symlinks` flag to the apt metadata in preparation for
-# the switch from reprepro to aptly. Aptly employs symlinks to maintain the
-# current expected directory structure. As we cannot ship symlinks to s3, this
-# flag will send over the resolved files.
-ssh $REPO_HOST <<PUBLISH_PACKAGES
-
-set -e
-set -x
-
-echo "BUCKET_NAME IS: ${BUCKET_NAME}"
-
-time s3cmd --verbose --acl-public --delete-removed  sync ${REPO_CONFIGS}/*  ${S3_BRANCH_PATH}/repo_configs/
-
-time s3cmd --verbose --acl-public --delete-removed  --follow-symlinks sync ${REPO_DIR}/apt/{jessie,lucid,precise,trusty,wheezy,wily,xenial}  ${S3_BRANCH_PATH}/repos/apt/
-
-time s3cmd --verbose --acl-public --delete-removed  sync ${REPO_DIR}/el/{5,6,7}  ${S3_BRANCH_PATH}/repos/el/
-
-time s3cmd --verbose --acl-public --delete-removed  sync ${REPO_DIR}/fedora/f*  ${S3_BRANCH_PATH}/repos/fedora/
-
-PUBLISH_PACKAGES
