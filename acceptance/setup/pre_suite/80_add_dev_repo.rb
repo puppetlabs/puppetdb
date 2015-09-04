fa96f9302cb51beb47b8dd58a7db036e155e4e01
Wayne <wayne@puppetlabs.com>
2015-09-04 10:01:41 -0700

    (QENG-2198) Fix reference to package_build_version in dev repo setup.

--- acceptance/setup/pre_suite/80_add_dev_repo.rb
+++ acceptance/setup/pre_suite/80_add_dev_repo.rb
@@ -2,8 +2,7 @@ install_opts = options.merge( { :dev_builds_repos => ["PC1"] })
 repo_config_dir = 'tmp/repo_configs'
 if (test_config[:install_type] == :package and not test_config[:skip_presuite_provisioning])
   databases.each do |database|
-    package_build_version = ENV['PACKAGE_BUILD_VERSION']
-    install_puppetlabs_dev_repo database, 'puppetdb', package_build_version,
+    install_puppetlabs_dev_repo database, 'puppetdb', test_config[:package_build_version],
                                 repo_config_dir, install_opts
   end
 end
