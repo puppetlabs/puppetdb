58dcf019fec2426fca68b335be4ed70c145273a5
Wayne <wayne@puppetlabs.com>
2015-09-03 21:12:28 -0700

    (QENG-2198) More packaging script cleanup.

--- ext/jenkins/packaging.sh
+++ ext/jenkins/packaging.sh
@@ -11,16 +11,15 @@ echo "REPO_URL: ${REPO_URL}"
 echo "**********************************************"
 
 set -x
+set -e
 
 export COW="base-precise-amd64.cow base-trusty-amd64.cow base-wheezy-amd64.cow"
 export MOCK="pl-el-6-x86_64 pl-el-7-x86_64"
 tmp_m2=$(pwd)/$(mktemp -d m2-local.XXXX)
 
-set -e
 lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- install
 lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- deps
 lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile ezbake ezbake stage
-set +e
 
 pushd "target/staging"
 rake package:bootstrap
@@ -30,15 +29,3 @@ cat > "${WORKSPACE}/puppetdb.packaging.props" <<PROPS
 PUPPETDB_PACKAGE_BUILD_VERSION=$(rake pl:print_build_param[ref] | tail -n 1)
 PROPS
 popd
-
-set -x
-
-# Set ship targets and build team for packaging repo
-REPO_HOST=neptune.puppetlabs.lan
-export REPO_DIR REPO_HOST
-
-# Now rebuild the metadata
-ssh $REPO_HOST <<PUBLISH_PACKAGES
-
-set -e
-set -x
