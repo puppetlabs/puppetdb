51b5eac0f53d5b744452882ac1dd93558aeaf563
Rob Browning <rlb@puppetlabs.com>
2015-09-24 15:14:10 -0500

    (PDB-1996) Don't test HSQLDB in Travis CI
    Use lowercase for non-exported vars, switch to case, and add quotation.

--- ext/travisci/test.sh
+++ ext/travisci/test.sh
@@ -8,27 +8,15 @@ ulimit -u 4096
 case "$PDB_TEST_LANG" in
   clojure)
     java -version
-    case "$PDB_TEST_DB" in
-      postgres)
-        psql -c 'create database puppetdb_test;' -U postgres
-        psql -c 'create database puppetdb2_test;' -U postgres
-        PUPPETDB_DBTYPE=postgres \
-          PUPPETDB_DBUSER=postgres \
-          PUPPETDB_DBSUBNAME=//127.0.0.1:5432/puppetdb_test \
-          PUPPETDB_DBPASSWORD= \
-          PUPPETDB2_DBUSER=postgres \
-          PUPPETDB2_DBSUBNAME=//127.0.0.1:5432/puppetdb2_test \
-          PUPPETDB2_DBPASSWORD= \
-            lein2 test
-        ;;
-      hsqldb)
-        PUPPETDB_DBTYPE=hsqldb lein2 test
-        ;;
-      *)
-        echo "Invalid database: $PDB_TEST_DB" 1>&2
-        exit 1
-        ;;
-    esac
+    psql -c 'create database puppetdb_test;' -U postgres
+    psql -c 'create database puppetdb2_test;' -U postgres
+    PUPPETDB_DBUSER=postgres \
+      PUPPETDB_DBSUBNAME=//127.0.0.1:5432/puppetdb_test \
+      PUPPETDB_DBPASSWORD= \
+      PUPPETDB2_DBUSER=postgres \
+      PUPPETDB2_DBSUBNAME=//127.0.0.1:5432/puppetdb2_test \
+      PUPPETDB2_DBPASSWORD= \
+        lein2 test
     ;;
   ruby)
     ruby -v
