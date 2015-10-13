edb483210aca94e181abd34800621a6412cca4b2
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-10-13 11:02:59 -0700

    (PDB-2006) Update jenkins lein-test config to setup second testing db
    This commit aligns our lein-test.sh from FOSS with the script by the
    same name in PE now that we use 2 postgresql's instance in clojure
    testing in FOSS.

--- ext/jenkins/lein-test.sh
+++ ext/jenkins/lein-test.sh
@@ -9,16 +9,23 @@ DBHOST="fixture-pg94.delivery.puppetlabs.net"
 DBPORT="5432"
 
 # Set up project specific database variables
-export PUPPETDB_DBUSER="$DBUSER"
-export PUPPETDB_DBPASSWORD="puppetdb137"
 export PGPASSWORD="puppetdb137"
+export PUPPETDB_DBUSER="$DBUSER"
+export PUPPETDB_DBPASSWORD="$PGPASSWORD"
 export PUPPETDB_DBSUBNAME="//${DBHOST}:${DBPORT}/${DBNAME}"
+
+# Set up the second project database variables
+DBNAME2=$(echo "p-p-e_${PUPPETDB_BRANCH}_${BUILD_ID}_${BUILD_NUMBER}_${PUPPETDB_DBTYPE}2_${JDK}" | tr - _)
+export PUPPETDB2_DBUSER="$DBUSER"
+export PUPPETDB2_DBPASSWORD="$PGPASSWORD"
+export PUPPETDB2_DBSUBNAME="//${DBHOST}:${DBPORT}/${DBNAME2}"
 
 rm -f testreports.xml *.war *.jar
 
 export HTTP_CLIENT="wget --no-check-certificate -O"
 
 psql -h "${DBHOST}" -U "${DBUSER}" -d postgres -c "create database ${DBNAME}"
+psql -h "${DBHOST}" -U "${DBUSER}" -d postgres -c "create database ${DBNAME2}"
 
 lein --version
 lein clean
@@ -30,3 +37,4 @@ lein test
 
 # Clean up our database
 psql -h "${DBHOST}" -U "${DBUSER}" -d postgres -c "drop database ${DBNAME}"
+psql -h "${DBHOST}" -U "${DBUSER}" -d postgres -c "drop database ${DBNAME2}"
