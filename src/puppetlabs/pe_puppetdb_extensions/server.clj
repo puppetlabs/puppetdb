8286c8d539a51cd1711926cca82f52515f80ea1e
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-10-21 14:11:21 -0700

    (PDB-2096) Add not-found route to top-level `/pdb` route
    This commit adds the `compojure.route/not-found` route at the top-level
    of the PuppetDB routes so that new PuppetDB routes won't need to add the
    not-found route. This also fixes an error where if a user hit any
    non-existent route under `/pdb`, PDB would throw an exception related to
    our middleware not playing nicely with not-founds which produced a
    confusing `nil...status` error.

--- src/puppetlabs/pe_puppetdb_extensions/server.clj
+++ src/puppetlabs/pe_puppetdb_extensions/server.clj
@@ -1,25 +1,15 @@
 (ns puppetlabs.pe-puppetdb-extensions.server
   (:require [puppetlabs.pe-puppetdb-extensions.state-overview :as state-overview]
-            [puppetlabs.puppetdb.middleware :refer
-             [wrap-with-puppetdb-middleware wrap-with-globals]]
-            [clojure.tools.logging :as log]
-            [puppetlabs.trapperkeeper.core :refer [defservice]]
-            [compojure.core :refer [GET ANY routes] :as compojure]
-            [compojure.route :as route]))
+            [compojure.core :as compojure]))
 
 (defn v1-app
   [query-fn]
-  (routes (GET "/state-overview" [] (state-overview/state-overview-app query-fn))))
+  (compojure/routes
+   (compojure/GET "/state-overview" []
+     (state-overview/state-overview-app query-fn))))
 
 (defn build-app
-  "Generate a Ring application that handles PuppetDB requests
-
-  `globals` is a map containing global state useful
-   to request handlers which may contain the following:
-
-  * `authorizer` - a function that takes a request and returns a
-    :authorized if the request is authorized, or a user-visible reason if not.
-    If not supplied, we default to authorizing all requests."
   [query-fn]
-  (routes (compojure/context "/v1" [] (v1-app query-fn))
-          (route/not-found "Not Found")))
+  (compojure/routes
+   (compojure/context "/v1" [] (v1-app query-fn))))
+
