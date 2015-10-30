f475b0bc93e985d9c81ca2a2c06d05b19b23e711
Ken Barber <ken@bob.sh>
2015-10-30 13:46:14 +0000

    (PDB-2113) Release 3.2.0
    Signed-off-by: Ken Barber <ken@bob.sh>

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "3.2.0-SNAPSHOT")
-(def pe-pdb-version "3.2.0-SNAPSHOT")
+(def pdb-version "3.2.0")
+(def pe-pdb-version "3.2.0")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
