21bb7a15f0f8104b2ed0844a9b8bed50c110835d
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-11-05 12:05:17 -0800

    (PDB-2097) Disable Puppet Server startup checkins to Dujour
    Prior to this commit Puppet Server would checkin to Dujour when
    puppetserver was started up because we munged `/etc/hosts` after
    puppetserver had already been installed. This commit separates the
    installation of puppet and puppetserver so that we can put the munging
    of `/etc/hosts` in the middle.
    
    This commit also removes a lot of dead code that was getting in the way
    of the refactor.

--- acceptance/setup/post_suite/01_validate_database.rb
+++ acceptance/setup/post_suite/01_validate_database.rb
@@ -8,49 +8,33 @@ step "Verify we've been talking to the correct database" do
   #  all pass even if they were running against a different
   #  database than we'd intended.)
 
-  if (test_config[:database] == :postgres)
+  # If we're running w/postgres, we're going to use some hacky raw SQL
+  #  and shell magic to validate that the database schema version is the
+  #  same in the database as in the source code, and that the certnames
+  #  table contains exactly the list of hostnames as the hosts array.
+  # If so, we'll assume that puppetdb was communicating with postgres
+  #  successfully during the test run. (These conditions were chosen
+  #  somewhat arbitrarily.)
 
-    # If we're running w/postgres, we're going to use some hacky raw SQL
-    #  and shell magic to validate that the database schema version is the
-    #  same in the database as in the source code, and that the certnames
-    #  table contains exactly the list of hostnames as the hosts array.
-    # If so, we'll assume that puppetdb was communicating with postgres
-    #  successfully during the test run. (These conditions were chosen
-    #  somewhat arbitrarily.)
+  step "Validate database schema version" do
+    db_migration_version = on(database, "PGPASSWORD=puppetdb psql -h localhost -U puppetdb -d puppetdb --tuples-only -c \"SELECT max(version) from schema_migrations\" |tr -d \" \" |grep -v \"^\s*$\"").stdout.strip
 
-    step "Validate database schema version" do
-      db_migration_version = on(database, "PGPASSWORD=puppetdb psql -h localhost -U puppetdb -d puppetdb --tuples-only -c \"SELECT max(version) from schema_migrations\" |tr -d \" \" |grep -v \"^\s*$\"").stdout.strip
+    # This is terrible; it's dependent on the order of some function definitions
+    # in the code, and it's just generally moronic.  We should provide a lein task
+    # or some way of interrogating the latest expected schema version from
+    # the command line so that we can get rid of this crap.
+    source_migration_version = on(database, "java -cp /usr/share/puppetdb/puppetdb.jar clojure.main -m puppetlabs.puppetdb.core version |grep target_schema_version|cut -f2 -d'='").stdout.strip
 
-      # This is terrible; it's dependent on the order of some function definitions
-      # in the code, and it's just generally moronic.  We should provide a lein task
-      # or some way of interrogating the latest expected schema version from
-      # the command line so that we can get rid of this crap.
-      source_migration_version = on(database, "java -cp /usr/share/puppetdb/puppetdb.jar clojure.main -m puppetlabs.puppetdb.core version |grep target_schema_version|cut -f2 -d'='").stdout.strip
-
-      assert_equal(db_migration_version, source_migration_version,
-                   "Expected migration version from source code '#{source_migration_version}' " +
-                       "to match migration version found in postgres database '#{db_migration_version}'")
-    end
+    assert_equal(db_migration_version, source_migration_version,
+                 "Expected migration version from source code '#{source_migration_version}' " +
+                 "to match migration version found in postgres database '#{db_migration_version}'")
+  end
 
 
-    step "Validate the contents of the certname table" do
-      db_hostnames_str = on(database, "PGPASSWORD=puppetdb psql -h localhost -U puppetdb -d puppetdb --tuples-only -c \"SELECT name from certnames\" |grep -v \"^\s*$\"").stdout.strip
-      db_hostnames = Set.new(db_hostnames_str.split.map { |x| x.strip })
-      acceptance_hostnames = Set.new(hosts.map { |host| host.node_name })
-      assert_equal(acceptance_hostnames, db_hostnames, "Expected hostnames from certnames table to match the ones known to the acceptance harness")
-    end
-
-
-  else
-    # If we're running w/the embedded db, we're just going to check to make
-    #  sure that postgres is not installed.  Assuming it's not, then if the
-    #  tests passed, by deductive reasoning the embedded DB *MUST* be working
-    #  OK.  I mean... right? ;)
-
-    ["postgresql", "postgresql-server"].each do |pkg|
-      result = on(database, "puppet resource package #{pkg} |grep \"ensure\" |egrep \"(absent)|(purged)\" |wc -l").stdout.strip.to_i
-      assert_equal(1, result, "Expected 'ensure => purged' for package '#{pkg}'")
-    end
-
+  step "Validate the contents of the certname table" do
+    db_hostnames_str = on(database, "PGPASSWORD=puppetdb psql -h localhost -U puppetdb -d puppetdb --tuples-only -c \"SELECT name from certnames\" |grep -v \"^\s*$\"").stdout.strip
+    db_hostnames = Set.new(db_hostnames_str.split.map { |x| x.strip })
+    acceptance_hostnames = Set.new(hosts.map { |host| host.node_name })
+    assert_equal(acceptance_hostnames, db_hostnames, "Expected hostnames from certnames table to match the ones known to the acceptance harness")
   end
 end
--- acceptance/setup/pre_suite/30_generate_ssl_certs.rb
+++ acceptance/setup/pre_suite/30_generate_ssl_certs.rb
@@ -1,10 +1,6 @@
 unless (test_config[:skip_presuite_provisioning])
   step "Run an agent to create the SSL certs" do
     on( master, "chown -R puppet:puppet /opt/puppetlabs/puppet/cache")
-    databases.each do |database|
-      with_puppet_running_on(master, {'master' => {'autosign' => 'true', 'trace' => 'true'}}) do
-        run_agent_on(database, "--test")
-      end
-    end
+    puppetserver_initialize_ssl
   end
 end
