9ef9ca5f6ac996be31b36c7c2161e0d01a161a5d
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-11-13 16:00:06 -0800

    (maint) Fix one-handed equality zipper test

--- test/puppetlabs/puppetdb/zip_test.clj
+++ test/puppetlabs/puppetdb/zip_test.clj
@@ -129,8 +129,8 @@
     (is (= node tree))))
 
 (deftest test-map-traversal
-  (is (= {2 {2 {2 "foo"}}})
-      (post-order-transform (tree-zipper {1 {1 {1 "foo"}}})
-                            [(fn [node]
-                               (when (instance? clojure.lang.MapEntry node)
-                                 (update node 0 inc)))])))
+  (is (= {2 {2 {2 "foo"}}}
+         (:node (post-order-transform (tree-zipper {1 {1 {1 "foo"}}})
+                                      [(fn [node]
+                                         (when (instance? clojure.lang.MapEntry node)
+                                           (update node 0 inc)))])))))
