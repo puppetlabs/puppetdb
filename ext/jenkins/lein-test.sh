d55e0a27d2fb88c3d9ace16256184ac32be17ec2
Ryan Senior <ryan.senior@puppetlabs.com>
2015-11-16 11:32:36 -0600

    (MAINT) Copy over lein-test.sh from puppetdb core
    This file is assumed present in the codebase now, which is correct on
    Travis but will fail on jenkins. Copying this over for now until we get
    a better solution.

--- ext/jenkins/lein-test.sh
+++ ext/jenkins/lein-test.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-exec checkouts/puppetdb/ext/jenkins/lein-test.sh
+set -ueo pipefail
+set -x
+
+ulimit -u 4096
+
+pghost=fixture-pg94.delivery.puppetlabs.net
+pgport=5432
+
+export HTTP_CLIENT="wget --no-check-certificate -O"
+
+rm -f testreports.xml *.war *.jar
+
+export PDB_TEST_DB_HOST="$pghost"
+export PDB_TEST_DB_PORT="$pgport"
+
+# For now, just conflate the normal test and admin users.
+export PDB_TEST_DB_USER=puppetdb
+export PDB_TEST_DB_ADMIN=puppetdb
+export PDB_TEST_DB_USER_PASSWORD=puppetdb137
+export PDB_TEST_DB_ADMIN_PASSWORD=puppetdb137
+
+PDB_TEST_ID="$(ruby -e "require 'securerandom';  print SecureRandom.uuid")"
+PDB_TEST_ID="$(echo -n "$PDB_TEST_ID" | perl -pe 's/[^a-zA-Z0-9_]/_/gmo')"
+declare -rx PDB_TEST_ID
+
+lein --version
+lein clean
+exec lein test
