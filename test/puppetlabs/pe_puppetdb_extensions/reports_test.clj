25de890d026344926151be9ce2ab4257b2599fbc
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-11-17 08:50:11 -0800

    (PDB-2183) Fix reports-resoruces produce-streaming-body call for query
    This commit fixes a bug where the produce-streaming-body wasn't calling
    query correctly for the the report-resoruces endpoint.
    This commit adds a test for the endpoint as well.

--- test/puppetlabs/pe_puppetdb_extensions/reports_test.clj
+++ test/puppetlabs/pe_puppetdb_extensions/reports_test.clj
@@ -1,8 +1,8 @@
 (ns puppetlabs.pe-puppetdb-extensions.reports-test
   (:require [clojure.test :refer :all]
             [clj-time.core :refer [now]]
-            [puppetlabs.puppetdb.testutils.services :as svcs]
-            [puppetlabs.puppetdb.scf.storage-utils :as sutils]
+            [clojure.string :as str]
+            [puppetlabs.puppetdb.testutils.services :refer [get-json]]
             [puppetlabs.pe-puppetdb-extensions.testutils :as utils
              :refer [with-puppetdb-instance blocking-command-post]]
             [puppetlabs.puppetdb.examples.reports :refer [reports]]
@@ -18,6 +18,21 @@
         (->> report
              reports/report-query->wire-v6
              (blocking-command-post (utils/pdb-cmd-url) "store report" 6))
-        (let [expected (->> report reports/report-query->wire-v6 :resources keywordize-keys)
-              actual (->> (svcs/get-json (utils/pdb-query-url) "/reports") first :resources :data)]
-          (is (= expected actual)))))))
+        (let [expected (->> report
+                            reports/report-query->wire-v6
+                            :resources
+                            keywordize-keys)
+              reports-response (first (get-json (utils/pdb-query-url) "/reports"))]
+
+          (testing "resources are the same as from the reports endpoint"
+            (is (= (set expected)
+                   (set (->> reports-response :resources :data)))))
+
+          (testing "resources are the same as from the child endpoint"
+            (is (= (set expected)
+                   (set (get-json (utils/pdb-query-url)
+                                  (format "/reports/%s/resources" (-> reports-response
+                                                                      :resources
+                                                                      :href
+                                                                      (str/split #"/")
+                                                                      (nth 5)))))))))))))
