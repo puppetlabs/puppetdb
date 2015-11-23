c3010d5226dddcecc839de56cae8898459f089f3
Ken Barber <ken@bob.sh>
2015-11-23 14:02:39 +0000

    (PDB-2122) Convert to new single query API
    This converts the pe-puppetdb code to use the new query API that requires a
    from operator to express the entity, instead of an entity field.
    
    It also adapts to use the new options hash for produce-streaming-body.
    
    Signed-off-by: Ken Barber <ken@bob.sh>

--- src/puppetlabs/pe_puppetdb_extensions/state_overview.clj
+++ src/puppetlabs/pe_puppetdb_extensions/state_overview.clj
@@ -1,47 +1,48 @@
 (ns puppetlabs.pe-puppetdb-extensions.state-overview
   (:require [clj-time.core :refer [hours seconds ago]]
             [puppetlabs.kitchensink.core :refer [parse-number]]
-            [puppetlabs.puppetdb.http :as http]
-            [puppetlabs.puppetdb.time :refer [to-timestamp]]
-            [puppetlabs.puppetdb.query-eng :refer [produce-streaming-body]]))
+            [puppetlabs.puppetdb.http :as http]))
 
 (defn fn-produce-body
-  [query-fn entity]
+  [query-fn]
   (fn [query]
-    (query-fn entity :v4 query nil doall)))
+    (query-fn :v4 query nil doall)))
 
 (defn state-overview-app
   [query-fn]
   (fn [{:keys [params] :as request}]
-   (let [produce-reports (fn-produce-body query-fn :reports)
-         produce-nodes (fn-produce-body query-fn :nodes)
-         unresponsive-threshold (let [urt (get params "unresponsive_threshold")]
-                                  (if (seq urt)
-                                    (-> urt parse-number seconds ago)
-                                    (-> 1 hours ago)))
-         [{noops :count}] (produce-reports ["extract" [["function" "count"]]
-                                            ["and"
-                                             ["=" ["node" "active"] true]
-                                             ["=" "noop" true]
-                                             ["=" "latest_report?" true]
-                                             [">" "end_time" unresponsive-threshold]]])
-         basics (produce-reports ["extract" ["status" ["function" "count"]]
-                                    ["and"
-                                     ["=" ["node" "active"] true]
-                                     ["=" "noop" false]
-                                     ["=" "latest_report?" true]
-                                     [">" "end_time" unresponsive-threshold]]
-                                    ["group_by" "status"]])
-         [{unresponsives :count}] (produce-nodes ["extract" [["function" "count"]]
-                                                  ["and"
-                                                   ["=" ["node" "active"] true]
-                                                   ["<" "report_timestamp" unresponsive-threshold]]])
-         [{unreporteds :count}] (produce-nodes ["extract" [["function" "count"]]
-                                                ["and"
-                                                 ["=" ["node" "active"] true]
-                                                 ["null?" "report_timestamp" true]
-                                                 ["null?" "report_environment" true]]])]
-     (->> (for [{:keys [status count]} basics] [(keyword status) count])
-          (into {:unresponsive unresponsives :unreported unreporteds :noop noops})
-          (merge {:failed 0 :unchanged 0 :unresponsive 0 :noop 0 :changed 0 :unreported 0})
-          http/json-response))))
+    (let [produce-body (fn-produce-body query-fn)
+          unresponsive-threshold (let [urt (get params "unresponsive_threshold")]
+                                   (if (seq urt)
+                                     (-> urt parse-number seconds ago)
+                                     (-> 1 hours ago)))
+          [{noops :count}] (produce-body ["from" "reports"
+                                          ["extract" [["function" "count"]]
+                                           ["and"
+                                            ["=" ["node" "active"] true]
+                                            ["=" "noop" true]
+                                            ["=" "latest_report?" true]
+                                            [">" "end_time" unresponsive-threshold]]]])
+          basics (produce-body ["from" "reports"
+                                ["extract" ["status" ["function" "count"]]
+                                 ["and"
+                                  ["=" ["node" "active"] true]
+                                  ["=" "noop" false]
+                                  ["=" "latest_report?" true]
+                                  [">" "end_time" unresponsive-threshold]]
+                                 ["group_by" "status"]]])
+          [{unresponsives :count}] (produce-body ["from" "nodes"
+                                                  ["extract" [["function" "count"]]
+                                                   ["and"
+                                                    ["=" ["node" "active"] true]
+                                                    ["<" "report_timestamp" unresponsive-threshold]]]])
+          [{unreporteds :count}] (produce-body ["from" "nodes"
+                                                ["extract" [["function" "count"]]
+                                                 ["and"
+                                                  ["=" ["node" "active"] true]
+                                                  ["null?" "report_timestamp" true]
+                                                  ["null?" "report_environment" true]]]])]
+      (->> (for [{:keys [status count]} basics] [(keyword status) count])
+           (into {:unresponsive unresponsives :unreported unreporteds :noop noops})
+           (merge {:failed 0 :unchanged 0 :unresponsive 0 :noop 0 :changed 0 :unreported 0})
+           http/json-response))))
