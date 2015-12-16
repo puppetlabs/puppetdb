586f91601aea482ced74dd7836c4232da2550257
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-12-16 15:59:36 -0800

    (PDB-2132) Add historical-catalogs and resource-graphs endpoints
    This commit adds endpoints and query-recs for historical catalogs. The
    `historical-catalogs` endpoint is for `sync` and for `export`s. The
    `resource-graphs` endpoint will be the CM UI's entrypoint into the
    historical data.

---
+++ src/puppetlabs/pe_puppetdb_extensions/catalogs.clj
@@ -0,0 +1,137 @@
+(ns puppetlabs.pe-puppetdb-extensions.catalogs
+  (:require [compojure.core :as compojure]
+            [puppetlabs.puppetdb.query.paging :as paging]
+            [puppetlabs.puppetdb.http.query :as http-q]
+            [puppetlabs.puppetdb.query-eng :as query-eng]
+            [puppetlabs.puppetdb.scf.storage-utils :as sutils]
+            [puppetlabs.puppetdb.query-eng.engine :as engine]
+            [puppetlabs.puppetdb.scf.storage :as scf-storage]))
+
+(def historical-catalog-query
+  "Query for the top level catalogs entity"
+  (engine/map->Query
+   (-> engine/catalog-query
+       (update :projections merge {"resources" {:type :json
+                                                :queryable? false
+                                                :field :c.resources}
+                                   "edges" {:type :json
+                                            :queryable? false
+                                            :field :c.edges}})
+       (assoc :selection {:from [[:catalogs :c]]
+                          :left-join [[:environments :e]
+                                      [:= :c.environment_id :e.id]]}
+              :alias "historical_catalogs"))))
+
+(def resource-graph-query
+  (engine/map->Query
+    {:projections
+     {"certname" {:type :string
+                  :queryable? true
+                  :field :r.certname}
+      "catalog_uuid" {:type :uuid
+                      :queryable? true
+                      :field (engine/hsql-uuid-as-str :r.catalog_uuid)}
+      "transaction_uuid" {:type :string
+                          :queryable? true
+                          :field (engine/hsql-uuid-as-str :r.transaction_uuid)}
+      "producer_timestamp" {:type :timestamp
+                            :queryable? true
+                            :field :r.producer_timestamp}
+      "noop" {:type :boolean
+              :queryable? true
+              :field :r.noop}
+      "status" {:type :string
+                :queryable? true
+                :field :report_statuses.status}
+      "code_id" {:type :string
+                 :queryable? true
+                 :field :c.code_id}
+      "environment" {:type :string
+                     :queryable? true
+                     :field :e.environment}
+      "catalog_resources" {:type :json
+                           :queryable? false
+                           :field :c.resources}
+      "report_resources" {:type :json
+                          :queryable? false
+                          :field :r.resources}
+      "edges" {:type :json
+               :queryable? false
+               :field :c.edges}}
+
+     :selection {:from [[:reports :r]]
+                 :left-join [[:catalogs :c] [:= :c.transaction_uuid :r.transaction_uuid]
+                             [:environments :e] [:= :r.environment_id :e.id]
+                             :report_statuses [:= :r.status_id :report_statuses.id]]}
+
+     :relationships {;; Parents - direct
+                     "node" {:columns ["certname"]}
+                     "environments" {:local-columns ["environment"]
+                                     :foreign-columns ["name"]}
+
+                     ;; Children - direct
+                     "edges" {:columns ["certname"]}
+                     "resources" {:columns ["certname"]}}
+     :alias "resource_graphs"
+     :subquery? false
+     :entity :reports
+     :source-table "reports"}))
+
+(def historical-catalogs-app
+  (let [param-spec {:optional paging/query-params}]
+    (http-q/query-route-from "historical_catalogs" :v1 param-spec [])))
+
+(def resource-graphs-app
+  (let [param-spec {:optional paging/query-params}]
+    (http-q/query-route-from "resource_graphs" :v1 param-spec [])))
+
+(defn merge-resources [report-resources catalog-resources]
+  (->> (clojure.set/join (sutils/parse-db-json report-resources)
+                         (sutils/parse-db-json catalog-resources)
+                         {:resource_type :type
+                          :resource_title :title})
+       (map (comp #(merge % {:file nil :line nil})
+                  #(dissoc % :resource_type :resource_title)))))
+
+(defn munge-jsonb-edges
+  [edges]
+  (->> edges
+       sutils/parse-db-json
+       (map (fn [{:keys [source target relationship]}]
+              {:source_type (:type source)
+               :source_title (:title source)
+               :target_type (:type target)
+               :target_title (:title target)
+               :relationship relationship}))))
+
+(defn munge-jsonb-resources
+  [edges]
+  (->> edges
+       sutils/parse-db-json
+       (map (partial merge {:file nil :line nil}))))
+
+(defn munge-historical-catalogs-rows
+  [_ _]
+  (fn [rows]
+    (->> rows
+         (map (comp #(update % :resources munge-jsonb-resources)
+                    #(update % :edges munge-jsonb-edges))))))
+
+(defn munge-resource-graph-rows
+  [_ _]
+  (fn [rows]
+    (->> rows
+         (map (comp #(update % :edges munge-jsonb-edges)
+                    #(assoc % :resources (merge-resources (:report_resources %)
+                                                          (:catalog_resources %)))
+                    #(dissoc % :catalog_resources :report_resources))))))
+
+(defn turn-on-historical-catalogs!
+  [store-historical-catalogs?]
+  (when store-historical-catalogs?
+    (reset! scf-storage/store-catalogs-historically? true))
+  (swap! query-eng/entity-fn-idx merge
+         {:historical-catalogs {:munge munge-historical-catalogs-rows
+                                :rec historical-catalog-query}}
+         {:resource-graphs {:munge munge-resource-graph-rows
+                            :rec resource-graph-query}}))
