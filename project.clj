48a5ed4b20ee06aa072d548753df88678010bf2d
Wyatt Alt <wyaltster@gmail.com>
2016-01-11 12:35:54 -0800

    remove -SNAPSHOT for 3.2.3 release

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "3.2.3-SNAPSHOT")
-(def pe-pdb-version "3.2.3-SNAPSHOT")
+(def pdb-version "3.2.3")
+(def pe-pdb-version "3.2.3")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
