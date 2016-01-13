8e4f40b9bff9cf86591cf17fce2816b8f5e7bb58
Wyatt Alt <wyaltster@gmail.com>
2016-01-13 08:38:06 -0800

    update version to 3.2.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "3.2.3")
-(def pe-pdb-version "3.2.3")
+(def pdb-version "3.2.4-SNAPSHOT")
+(def pe-pdb-version "3.2.4-SNAPSHOT")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
