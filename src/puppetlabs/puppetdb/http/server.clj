2982e63e49969b12441eca162f66945eb19eea80
Ryan Senior <ryan.senior@puppetlabs.com>
2016-01-14 12:29:28 -0600

    (PDB-2242) Move v4 routes into server.clj
    With the v4 namespace being so small (only containing the routing data
    structure) it's easier to maintain this in-line with server. That's now
    the one stop shop for all query routes.

--- src/puppetlabs/puppetdb/http/server.clj
+++ src/puppetlabs/puppetdb/http/server.clj
@@ -5,14 +5,14 @@
    application."
   (:require [clojure.tools.logging :as log]
             [puppetlabs.puppetdb.http :as http]
-            [puppetlabs.puppetdb.http.v4 :refer [v4-app]]
             [puppetlabs.puppetdb.middleware :refer [wrap-with-globals
                                                     wrap-with-metrics
                                                     wrap-with-illegal-argument-catch
                                                     verify-accepts-json
                                                     make-pdb-handler]]
             [ring.util.response :as rr]
-            [puppetlabs.comidi :as cmdi]))
+            [puppetlabs.comidi :as cmdi]
+            [puppetlabs.puppetdb.http.handlers :as handlers]))
 
 (defn- refuse-retired-api
   [version]
@@ -32,8 +32,24 @@
    (retired-api-route "v1")
    (retired-api-route "v2")
    (retired-api-route "v3")
-   (cmdi/context "/v4"
-                 v4-app)))
+   (apply cmdi/context "/v4"
+          (map (fn [[route-str handler]]
+                 (cmdi/context route-str (handler :v4)))
+               {"" handlers/experimental-index-routes
+                "/facts" handlers/facts-routes
+                "/edges" handlers/edge-routes
+                "/factsets" handlers/factset-routes
+                "/fact-names" handlers/fact-names-routes
+                "/fact-contents" handlers/fact-contents-routes
+                "/fact-paths" handlers/fact-path-routes
+                "/nodes" handlers/node-routes
+                "/environments" handlers/environments-routes
+                "/resources" handlers/resources-routes
+                "/catalogs" handlers/catalog-routes
+                "/events" handlers/events-routes
+                "/event-counts" handlers/event-counts-routes
+                "/aggregate-event-counts" handlers/agg-event-counts-routes 
+                "/reports" handlers/reports-routes}))))
 
 (defn build-app
   "Generates a Ring application that handles PuppetDB requests.
