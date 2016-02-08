4eca5a8981d4841844423ee4d2097795c72aa36a
Ken Barber <ken@bob.sh>
2016-02-08 15:08:17 +0000

    (maint) Update library dependencies
    Signed-off-by: Ken Barber <ken@bob.sh>

--- project.clj
+++ project.clj
@@ -9,10 +9,10 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def tk-version "1.1.1")
-(def tk-jetty9-version "1.3.1")
-(def ks-version "1.1.0")
-(def tk-status-version "0.2.1")
+(def tk-version "1.2.0")
+(def tk-jetty9-version "1.5.0")
+(def ks-version "1.2.0")
+(def tk-status-version "0.3.1")
 
 (def pdb-jvm-opts
   (case (System/getProperty "java.specification.version")
@@ -33,16 +33,16 @@
   :pedantic? :abort
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.4.0"]
+                 [cheshire "5.5.0"]
                  [org.clojure/core.match "0.2.2"]
-                 [org.clojure/math.combinatorics "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/core.memoize "0.5.8"]
                  [puppetlabs/tools.namespace "0.2.4.1"]
                  [clj-stacktrace "0.2.8"]
-                 [metrics-clojure "2.6.0" :exclusions [org.clojure/clojure org.slf4j/slf4j-api]]
-                 [clj-time "0.10.0"]
+                 [metrics-clojure "2.6.1" :exclusions [org.clojure/clojure org.slf4j/slf4j-api]]
+                 [clj-time "0.11.0"]
                  [org.clojure/java.jmx "0.3.1"]
                  ;; Filesystem utilities
                  [me.raynes/fs "1.4.5"]
@@ -54,25 +54,25 @@
                  [slingshot "0.12.2"]
 
                  ;; Database connectivity
-                 [com.zaxxer/HikariCP "2.4.1"]
-                 [org.clojure/java.jdbc "0.3.7"]
+                 [com.zaxxer/HikariCP "2.4.3"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [org.postgresql/postgresql "9.4-1206-jdbc4"]
 
                  ;; MQ connectivity
-                 [org.apache.activemq/activemq-broker "5.13.0" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.activemq/activemq-kahadb-store "5.13.0" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.activemq/activemq-pool "5.13.0" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.activemq/activemq-broker "5.13.1" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.activemq/activemq-kahadb-store "5.13.1" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.activemq/activemq-pool "5.13.1" :exclusions [org.slf4j/slf4j-api]]
 
                  ;; Parsing library required by PQL
                  [instaparse "1.4.1"]
 
                  ;; bridge to allow some spring/activemq stuff to log over slf4j
-                 [org.slf4j/jcl-over-slf4j "1.7.10"]
+                 [org.slf4j/jcl-over-slf4j "1.7.14" :exclusions [org.slf4j/slf4j-api]]
                  ;; WebAPI support libraries.
                  [compojure "1.4.0"]
-                 [clj-http "2.0.0"]
+                 [clj-http "2.0.1"]
                  [ring/ring-core "1.4.0" :exclusions [javax.servlet/servlet-api]]
-                 [org.apache.commons/commons-compress "1.8"]
+                 [org.apache.commons/commons-compress "1.10"]
                  [puppetlabs/kitchensink ~ks-version]
                  [puppetlabs/trapperkeeper ~tk-version]
                  [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty9-version]
@@ -82,12 +82,14 @@
                  [com.novemberain/pantomime "2.1.0"]
                  [fast-zip-visit "1.0.2"]
                  [robert/hooke "1.3.0"]
-                 [honeysql "0.6.2"]
+                 [honeysql "0.6.3"]
                  [com.rpl/specter "0.5.7"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [puppetlabs/http-client "0.4.4"]
+                 [puppetlabs/http-client "0.5.0" :exclusions [org.apache.httpcomponents/httpclient
+                                                              org.apache.httpcomponents/httpcore
+                                                              org.slf4j/slf4j-api]]
                  [com.taoensso/nippy "2.10.0" :exclusions [org.clojure/tools.reader]]
-                 [bidi "1.23.1" :exclusions [org.clojure/clojurescript]]
+                 [bidi "1.25.1" :exclusions [org.clojure/clojurescript]]
                  [puppetlabs/comidi "0.3.1"]]
 
   :jvm-opts ~pdb-jvm-opts
@@ -124,11 +126,11 @@
                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test"]
                                   [puppetlabs/kitchensink ~ks-version :classifier "test"]
                                   [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty9-version :classifier "test"]
-                                  [org.flatland/ordered "1.5.2"]
+                                  [org.flatland/ordered "1.5.3"]
                                   [org.clojure/test.check "0.5.9"]
-                                  [environ "1.0.0"]
-                                  [org.clojure/tools.cli "0.3.0"] ; prevents dependency clash caused by lein-cloverage
-                                  [riddley "0.1.10"]]}
+                                  [environ "1.0.2"]
+                                  [org.clojure/tools.cli "0.3.3"] ; prevents dependency clash caused by lein-cloverage
+                                  [riddley "0.1.12"]]}
              :ezbake {:dependencies ^:replace [[puppetlabs/puppetdb ~pdb-version]
                                                [org.clojure/tools.nrepl "0.2.3"]]
                       :name "puppetdb"
