28a24462c34f17652bb38e8739177cb79d91d942
Russell Mull <russell.mull@puppetlabs.com>
2016-02-24 14:37:04 -0800

    Remove -SNAPSHOT for 3.2.4 release

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "3.2.4-SNAPSHOT")
-(def pe-pdb-version "3.2.4-SNAPSHOT")
+(def pdb-version "3.2.4")
+(def pe-pdb-version "3.2.4")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
