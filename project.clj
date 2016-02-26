4c058cb9cb8e3ec7dfb2ce8e831f16bdd3c22d0c
Russell Mull <russell.mull@puppetlabs.com>
2016-02-26 09:30:44 -0800

    Bump version to 3.2.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "3.2.4")
-(def pe-pdb-version "3.2.4")
+(def pdb-version "3.2.5-SNAPSHOT")
+(def pe-pdb-version "3.2.5-SNAPSHOT")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
