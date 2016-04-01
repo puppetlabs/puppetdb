a5a4586fc0941aa5b95ec23faa53d06a3d82aa0d
Wyatt Alt <wyaltster@gmail.com>
2016-04-01 13:28:41 -0700

    bump version to 4.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "4.0.1")
-(def pe-pdb-version "4.0.1")
+(def pdb-version "4.0.2-SNAPSHOT")
+(def pe-pdb-version "4.0.2-SNAPSHOT")
 
 (def tk-version "1.1.1")
 (def ks-version "1.0.0")
