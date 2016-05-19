80b5a57e956728c543055b175ca8f56eb319f612
Ryan Senior <ryan.senior@puppetlabs.com>
2016-05-19 12:16:40 -0500

    (MAINT) Bump to version 4.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "4.1.0")
-(def pe-pdb-version "4.1.0")
+(def pdb-version "4.1.1-SNAPSHOT")
+(def pe-pdb-version "4.1.1-SNAPSHOT")
 
 (def rbac-client-version "0.1.4")
 (def rbac-version "1.2.19")
