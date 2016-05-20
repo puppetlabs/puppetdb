35a44a8c6723b38e304b428b783cda8b8eba0141
Andrew Roetker <andrew.roetker@puppetlabs.com>
2016-05-20 10:59:00 -0700

    (PE-15817) Update rbac-client dependency
    This commit updates the rbac-client so that we don't choke on startup if
    the rbac-consumer api-url is not set.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 (def pdb-version "4.1.1-SNAPSHOT")
 (def pe-pdb-version "4.1.1-SNAPSHOT")
 
-(def rbac-client-version "0.1.4")
+(def rbac-client-version "0.1.5")
 (def rbac-version "1.2.19")
 
 (def activity-version"0.5.1")
