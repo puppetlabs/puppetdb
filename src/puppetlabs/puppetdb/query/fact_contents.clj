95a46fdf12ec3d790dafe85cbfdb0a2e56eeb142
Wyatt Alt <wyaltster@gmail.com>
2016-07-22 16:04:56 -0700

    (PDB-2843) add function support to fact-contents
    This corrects a bug where the fact-contents and edges endpoints would not allow
    usage of functions.

--- src/puppetlabs/puppetdb/query/fact_contents.clj
+++ src/puppetlabs/puppetdb/query/fact_contents.clj
@@ -1,23 +1,26 @@
 (ns puppetlabs.puppetdb.query.fact-contents
   (:require [puppetlabs.puppetdb.facts :as f]
             [puppetlabs.puppetdb.cheshire :as json]
+            [puppetlabs.puppetdb.query :as query]
             [puppetlabs.puppetdb.schema :as pls]
             [puppetlabs.puppetdb.utils :as utils]
             [schema.core :as s]))
 
 (def row-schema
-  {(s/optional-key :certname) s/Str
-   (s/optional-key :environment) (s/maybe s/Str)
-   (s/optional-key :path) s/Str
-   (s/optional-key :name) s/Str
-   (s/optional-key :value) (s/maybe s/Str)})
+  (query/wrap-with-supported-fns
+    {(s/optional-key :certname) s/Str
+     (s/optional-key :environment) (s/maybe s/Str)
+     (s/optional-key :path) s/Str
+     (s/optional-key :name) s/Str
+     (s/optional-key :value) (s/maybe s/Str)}))
 
 (def converted-row-schema
-  {(s/optional-key :certname) s/Str
-   (s/optional-key :environment) (s/maybe s/Str)
-   (s/optional-key :path) f/fact-path
-   (s/optional-key :name) s/Str
-   (s/optional-key :value) s/Any})
+  (query/wrap-with-supported-fns
+    {(s/optional-key :certname) s/Str
+     (s/optional-key :environment) (s/maybe s/Str)
+     (s/optional-key :path) f/fact-path
+     (s/optional-key :name) s/Str
+     (s/optional-key :value) s/Any}))
 
 (pls/defn-validated munge-result-row :- converted-row-schema
   "Coerce the value of a row to the proper type, and convert the path back to
