178d7821d129e0f229c4c2294c7b0b7567511132
Russell Mull <russell.mull@puppetlabs.com>
2017-03-01 13:24:31 -0800

    (PDB-3302) Use fs/temp-file in puppetdb_query integration test

--- test/puppetlabs/puppetdb/integration/storeconfigs/puppetdb_query.clj
+++ test/puppetlabs/puppetdb/integration/storeconfigs/puppetdb_query.clj
@@ -10,21 +10,19 @@
     (testing "Initial agent run, to populate puppetdb with data to query"
       (int/run-puppet ps pdb "notify { 'hello, world!': }"))
 
-    (let [test-out-file "/tmp/test_puppetdb_query.txt"]
+    (let [test-out-file (fs/temp-file "test_puppetdb_query" ".txt")]
       (testing "Agent run with puppedb_query in the manifest"
-        (when (fs/exists? test-out-file)
-          (fs/delete test-out-file))
 
         (int/run-puppet ps pdb
                         (str "node default {"
                              "  $counts = puppetdb_query(['from', 'catalogs',"
                              "                            ['extract', [['function', 'count']]]])"
                              "  $count = $counts[0]['count']"
-                             "  file { '" test-out-file "':"
+                             "  file { '" (.getCanonicalPath test-out-file) "':"
                              "        ensure  => present,"
                              "        content => \"${count}\""
                              "  }"
                              "}"))
 
         (testing "should write the correct catalog count to a temp file"
-          (is (= "1" (slurp "/tmp/test_puppetdb_query.txt"))))))))
+          (is (= "1" (slurp test-out-file))))))))
