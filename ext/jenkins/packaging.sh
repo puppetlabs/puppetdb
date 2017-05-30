4a5092da95d609971fa5730a1bb9d2689e8b5f96
Russell Mull <russell.mull@puppetlabs.com>
2017-05-30 15:41:39 -0700

    Use vanilla ezbake commands in packaging.sh

--- ext/jenkins/packaging.sh
+++ ext/jenkins/packaging.sh
@@ -15,17 +15,9 @@ set -e
 
 export COW="base-jessie-amd64.cow base-precise-amd64.cow base-trusty-amd64.cow base-wheezy-amd64.cow base-wily-amd64.cow base-xenial-amd64.cow"
 export MOCK="pl-el-6-x86_64 pl-el-7-x86_64"
-tmp_m2=$(pwd)/$(mktemp -d m2-local.XXXX)
 
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- install
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- deps
-# The ci-voom profile has nexus credentials
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile user,ci-voom deploy
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile ezbake ezbake stage
-
-pushd "target/staging"
-rake package:bootstrap
-rake pl:jenkins:uber_build[5]
+lein deploy
+lein with-profile ezbake ezbake build
 
 cat > "${WORKSPACE}/puppetdb.packaging.props" <<PROPS
 PUPPETDB_PACKAGE_BUILD_VERSION=$(rake pl:print_build_param[ref] | tail -n 1)
