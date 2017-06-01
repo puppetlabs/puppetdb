f4ad4678763b1880894499bcb7ba04e686dd678d
Russell Mull <russell.mull@puppetlabs.com>
2017-06-01 15:26:53 -0700

    Improve packaging.sh
    - Remove unnecessary build steps
    - Don't ship things to nexus that don't need to be
    - Explicitly ship release builds to nexus when they're not already there

--- ext/jenkins/packaging.sh
+++ ext/jenkins/packaging.sh
@@ -3,6 +3,7 @@
 echo "**********************************************"
 echo "PARAMS FROM UPSTREAM:"
 echo ""
+echo "WORKSPACE: ${WORKSPACE}"
 echo "PUPPETDB_BRANCH: ${PUPPETDB_BRANCH}"
 echo "LEIN_PROJECT_VERSION: ${LEIN_PROJECT_VERSION}"
 echo "GEM_SOURCE: ${GEM_SOURCE}"
@@ -13,23 +14,33 @@ echo "**********************************************"
 set -x
 set -e
 
+# Upload release versions to nexus
+version="$(grep 'def pdb-version' project.clj | cut -d\" -f 2)"
+test "$version"
+
+if ! [[ "$version" =~ SNAPSHOT ]]; then
+    echo "${version} appears to be a release version"
+    nexus_base="http://nexus.delivery.puppetlabs.net/service/local/repositories"
+    probe_url="${nexus_base}/releases/content/puppetlabs/puppetdb/${version}/puppetdb-${version}.jar"
+    status="$(curl --head --silent -o /dev/null -w "%{http_code}" "${probe_url}")"
+    if [ "$status" -eq 200 ]; then
+        echo "puppetdb-${version} already exists on Nexus; skipping deploy"
+    else
+        echo "Deploying puppetdb ${version} to releases"
+        lein deploy releases
+    fi
+else
+    echo "${version} appears to be a snapshot version"
+fi
+
+# Build packages using a locally created jar
 export COW="base-jessie-amd64.cow base-precise-amd64.cow base-trusty-amd64.cow base-wheezy-amd64.cow base-wily-amd64.cow base-xenial-amd64.cow"
 export MOCK="pl-el-6-x86_64 pl-el-7-x86_64"
-tmp_m2=$(pwd)/$(mktemp -d m2-local.XXXX)
 
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- install
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- deps
-# The ci-voom profile has nexus credentials
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile user,ci-voom deploy
-lein update-in : assoc :local-repo "\"${tmp_m2}\"" -- with-profile ezbake ezbake stage
+lein with-profile ezbake ezbake build
 
 pushd "target/staging"
-rake package:bootstrap
-rake pl:jenkins:uber_build[5]
-
 cat > "${WORKSPACE}/puppetdb.packaging.props" <<PROPS
 PUPPETDB_PACKAGE_BUILD_VERSION=$(rake pl:print_build_param[ref] | tail -n 1)
 PROPS
 popd
-
-set -x
