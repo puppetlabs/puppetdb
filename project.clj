b9ff9ac37f12088f450a67351ce97b262e3697b5
Russell Mull <russell.mull@puppetlabs.com>
2017-06-01 18:19:58 -0700

    Revert "Remove circular lein deps from ezbake profile"
    This reverts commit 7aa41a5d8d51fc874164e1a5ce88ab517b80b193.

--- project.clj
+++ project.clj
@@ -98,7 +98,9 @@
                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test"]
                                   [puppetlabs/kitchensink ~ks-version :classifier "test"]
                                   [environ "1.0.0"]]}
-             :ezbake {:dependencies [[org.clojure/tools.nrepl "0.2.3"]]
+             :ezbake {:dependencies ^:replace [[puppetlabs/puppetdb ~pdb-version]
+                                               [org.clojure/tools.nrepl "0.2.3"]
+                                               [puppetlabs/pe-puppetdb-extensions ~pe-pdb-version]]
                       :plugins [[puppetlabs/lein-ezbake "1.1.3"
                                 :exclusions [org.clojure/clojure]]]
                       :version ~pe-pdb-version
