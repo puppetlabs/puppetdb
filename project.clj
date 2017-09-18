d8dac5ec7fd9c724a2274055cad5f4e3d787f25d
Rob Browning <rlb@puppet.com>
2017-09-18 16:07:19 -0500

    Update project.clj for 4.2.3.6 release

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(def pdb-version "4.2.3.6-SNAPSHOT")
-(def pe-pdb-version "4.2.3.6-SNAPSHOT")
+(def pdb-version "4.2.3.6")
+(def pe-pdb-version "4.2.3.6")
 
 (def rbac-client-version "0.1.5")
 (def rbac-version "1.2.19")
