a0e3d319ba0c1b80fa62da9bb2ae0f90df263448
Wyatt Alt <wyatt.alt@gmail.com>
2017-10-11 14:38:20 -0700

    (maint) change install_mode to install_type

--- acceptance/setup/pre_suite/70_install_released_puppetdb.rb
+++ acceptance/setup/pre_suite/70_install_released_puppetdb.rb
@@ -1,8 +1,8 @@
 # We skip this step entirely unless we are running in :upgrade mode.
-OLDEST_SUPPORTED_UPGRADE="2.3.8"
+OLDEST_SUPPORTED_UPGRADE="4.0.0"
 
-if ([:upgrade_oldest, :upgrade_latest].include? test_config[:install_mode] and not test_config[:skip_presuite_provisioning])
-  install_target = test_config[:install_mode] == :upgrade_latest ? 'latest' : OLDEST_SUPPORTED_UPGRADE
+if ([:upgrade_oldest, :upgrade_latest].include? test_config[:install_type] and not test_config[:skip_presuite_provisioning])
+  install_target = test_config[:install_type] == :upgrade_latest ? 'latest' : OLDEST_SUPPORTED_UPGRADE
   step "Install most recent released PuppetDB on the PuppetDB server for upgrade test" do
     databases.each do |database|
       enable_https_apt_sources(database)
