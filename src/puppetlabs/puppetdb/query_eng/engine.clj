ad41aa0a559241a29fdc6b62d2fca2683a31c1e8
Wyatt Alt <wyatt.alt@gmail.com>
2017-11-02 16:06:29 -0700

    (PDB-3723) Exclude structured values from fact-contents
    [rlb@puppet.com: adjust message; move removal of path gc and unused db
     variable to other commits]

--- src/puppetlabs/puppetdb/query_eng/engine.clj
+++ src/puppetlabs/puppetdb/query_eng/engine.clj
@@ -515,7 +515,10 @@
                                            "                   from fact_paths) distinct_paths"
                                            "   where jsonb_extract_path(stable||volatile,"
                                            "                            variadic path_array)"
-                                           "           is not null)"))
+                                           "           is not null"
+                                           "         and jsonb_typeof(jsonb_extract_path(stable||volatile,
+                                                                                         variadic path_array))"
+                                           "               <> 'object')"))
                                      :fs]]
                              :left-join [[:environments :env]
                                          [:= :fs.environment_id :env.id]]}
