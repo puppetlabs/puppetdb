ac47c39399fe38e0eb6b1a1b94fa2d3b2bacf4e7
Wyatt Alt <wyatt.alt@gmail.com>
2017-12-12 17:27:51 -0800

    (PDB-3417) support != in PQL

--- test/puppetlabs/puppetdb/pql/transform_test.clj
+++ test/puppetlabs/puppetdb/pql/transform_test.clj
@@ -59,6 +59,9 @@
       ["from" "nodes"
        ["=" "a" 1]]
 
+      [:condexpression [:field "certname"] "!=" [:integer "4"]]
+      ["not" ["=" "certname" 4]]
+
       [:from "nodes"
        [:extract "a" "b" "c"]]
       ["from" "nodes"
