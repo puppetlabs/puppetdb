bf9666f5ee9cc82846e0599a97fb71163c394f7f
Morgan Rhodes <morgan@puppet.com>
2018-06-12 09:58:18 -0700

    (maint) Quote some vars

--- docker/puppetdb/docker-entrypoint.sh
+++ docker/puppetdb/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -d "/etc/puppetlabs/puppetdb/ssl" ]; then
     sleep 1
   done
   set -e
-  /opt/puppetlabs/bin/puppet config set certname ${HOSTNAME}
+  /opt/puppetlabs/bin/puppet config set certname "$HOSTNAME"
   /opt/puppetlabs/bin/puppet agent --verbose --onetime --no-daemonize --waitforcert 120
   /opt/puppetlabs/server/bin/puppetdb ssl-setup -f
 fi
