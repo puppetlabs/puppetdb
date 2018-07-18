d27a8cfadf097cd07ce9c78dc58292087b41d1d8
Austin Blatt <austinblatt@gmail.com>
2018-07-18 09:31:02 -0700

    (PDB-3963) Use proper gem-list from puppetserver
    Prior to this commit (due to a file name change in puppetserver), the
    integration tests were broken between master branches.
    
    This commit makes it possible for the puppetdb master branch to run
    against master as well as the tagged releases of puppetserver by
    choosing the proper file name depending on puppetserver's version.

--- src-gems/puppetlabs/puppetdb/integration/install_gems.clj
+++ src-gems/puppetlabs/puppetdb/integration/install_gems.clj
@@ -10,16 +10,17 @@
   (let [jruby-config (jruby-puppet-core/initialize-and-create-jruby-config config)]
     (jruby-core/cli-run! jruby-config "gem" args)))
 
-(defn install-gems [config _]
+(defn install-gems [gem-list-name config _]
   (gem-run! config "install" "facter")
   (gem-run! config "install" "hiera")
 
   ;; Install the puppetserver vendored gems listed inside its jar; this is where
   ;; ezbake gets them
-  (let [gem-list (string/split (slurp (io/resource "ext/build-scripts/gem-list.txt")) #"\n")]
+  (let [gem-list (string/split (slurp (io/resource (str "ext/build-scripts/" gem-list-name))) #"\n")]
     (doseq [[gem version] (map #(string/split % #"\s") gem-list)]
       (gem-run! config "install" gem "--version" version))))
 
 (defn -main
   [& args]
-  (cli/run install-gems args))
+  (let [[gem-list-name & others] args]
+    (cli/run (partial install-gems gem-list-name) others)))
