793aa1d98e8cce4490d571f4ad85d3bdce0849ec
Iristyle <Iristyle@github>
2019-04-02 15:56:06 -0700

    (maint) Move Docker wait from SSL to wait script
     - Centralize waiting operations in 10-wait-for-hosts.sh by moving
       any waiting out of 30-configure-ssl.sh

--- docker/puppetdb/docker-entrypoint.d/20-wait-for-hosts.sh
+++ docker/puppetdb/docker-entrypoint.d/20-wait-for-hosts.sh
@@ -5,6 +5,8 @@
 #
 # Optional environment variables:
 #   PUPPETDB_WAITFORHOST_SECONDS    Number of seconds to wait for host, defaults to 30
+#   PUPPETDB_WAITFORHEALTH_SECONDS  Number of seconds to wait for health
+#                                   checks of Consul / Puppetserver to succeed, defaults to 600
 
 msg() {
     echo "($0) $1"
@@ -23,13 +25,27 @@ wait_for_host() {
 }
 
 PUPPETDB_WAITFORHOST_SECONDS=${PUPPETDB_WAITFORHOST_SECONDS:-30}
+PUPPETDB_WAITFORHEALTH_SECONDS=${PUPPETDB_WAITFORHEALTH_SECONDS:-600}
+PUPPETSERVER_HOSTNAME="${PUPPETSERVER_HOSTNAME:-puppet}"
+CONSUL_HOSTNAME="${CONSUL_HOSTNAME:-consul}"
+CONSUL_PORT="${CONSUL_PORT:-8500}"
 
 wait_for_host "postgres"
 
 if [ "$USE_PUPPETSERVER" = true ]; then
-  wait_for_host "${PUPPETSERVER_HOSTNAME:-puppet}"
+  wait_for_host $PUPPETSERVER_HOSTNAME
+  HEALTH_COMMAND="curl --silent --fail --insecure "\""https://${PUPPETSERVER_HOSTNAME}:8140/status/v1/simple"\"" | grep -q '^running$'"
 fi
 
 if [ "$CONSUL_ENABLED" = "true" ]; then
-  wait_for_host "${CONSUL_HOSTNAME:-consul}"
+  wait_for_host $CONSUL_HOSTNAME
+  # with Consul enabled, wait on Consul instead of Puppetserver
+  HEALTH_COMMAND="curl --silent --fail "\""http://${CONSUL_HOSTNAME}:${CONSUL_PORT}/v1/health/checks/puppet"\"" | grep -q '"\""Status"\"": "\""passing"\""'"
+fi
+
+if [ -n "$HEALTH_COMMAND" ]; then
+  /wtfc.sh --timeout=$PUPPETDB_WAITFORHEALTH_SECONDS --interval=1 --progress $HEALTH_COMMAND
+  if [ $? -ne 0 ]; then
+    error "Required health check failed"
+  fi
 fi
