da39c1c4d460fc008b4b83078c74fe59ae5c0169
Iristyle <Iristyle@github>
2019-04-05 14:22:29 -0700

    (maint) Refactor PDB specs to use docker-compose
     - Much easier to describe the cluster in a compose file and start it
       that way than intersperse `docker run` commands throughout the test
       suite
    
     - `docker exec` must be converted to `docker-compose exec`
    
     - PDB data can be ephemeral in the container
    
     - Add some additional docker-compose helpers to spec_helper, namely:
    
       * get_service_base_uri - gets host mapped port for a service
       * get_service_container - gets the container id for a service
       * teardown_cluster - enumerates all containers in a cluster, and
                            kills them, taking into account Windows
       * get_containers - gets all containers in the cluster
       * emit_logs - emits all the logs for every container in cluster

--- docker/puppetdb/spec/spec_helper.rb
+++ docker/puppetdb/spec/spec_helper.rb
@@ -49,15 +49,46 @@ module Helpers
     run_command("icacls \"#{root}\" /grant Users:\"(OI)(CI)F\" /T")
   end
 
-  def get_container_port(container, port)
-    @mapped_ports["#{container}:#{port}"] ||= begin
-      service_ip_port = run_command("docker port #{container} #{port}/tcp")[:stdout].chomp
+  def get_service_base_uri(service, port)
+    @mapped_ports["#{service}:#{port}"] ||= begin
+      result = run_command("docker-compose --no-ansi port #{service} #{port}")
+      service_ip_port = result[:stdout].chomp
+      raise "Could not retrieve service endpoint for #{service}:#{port}" if service_ip_port == ''
       uri = URI("http://#{service_ip_port}")
       uri.host = 'localhost' if uri.host == '0.0.0.0'
-      STDOUT.puts "determined #{container} endpoint for port #{port}: #{uri}"
+      STDOUT.puts "determined #{service} endpoint for port #{port}: #{uri}"
       uri
     end
-    @mapped_ports["#{container}:#{port}"]
+    @mapped_ports["#{service}:#{port}"]
+  end
+
+  def get_service_container(service, timeout = 120)
+    return retry_block_up_to_timeout(timeout) do
+      container = run_command("docker-compose --no-ansi ps --quiet #{service}")[:stdout].chomp
+      if container.empty?
+        raise "docker-compose never started a service named '#{service}' in #{timeout} seconds"
+      end
+
+      STDOUT.puts("service named '#{service}' is hosted in container: '#{container}'")
+      container
+    end
+  end
+
+  def teardown_cluster
+    STDOUT.puts("Tearing down test cluster")
+    get_containers.each do |id|
+      STDOUT.puts("Killing container #{id}")
+      run_command("docker container kill #{id}")
+    end
+    # still needed to remove network / provide failsafe
+    run_command('docker-compose --no-ansi down --volumes')
+  end
+
+  def get_containers
+    result = run_command('docker-compose --no-ansi --log-level INFO ps -q')
+    ids = result[:stdout].chomp
+    STDOUT.puts("Retrieved running container ids:\n#{ids}")
+    ids.lines.map(&:chomp)
   end
 
   def inspect_container(container, query)
@@ -78,4 +109,8 @@ module Helpers
     STDOUT.puts(logs)
   end
 
+  def emit_logs
+    STDOUT.puts("Emitting container logs")
+    get_containers.each { |id| emit_log(id) }
+  end
 end
