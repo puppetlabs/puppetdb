a0ffc0cbbb285c1efff024ff4b9cf284a811244f
Robert Roland <robert.roland@puppet.com>
2019-06-06 11:26:38 -0700

    (PDB-4385) update lein-ezbake version for FIPS
    Update lein-ezbake to 2.0.3 to support FIPS

--- project.clj
+++ project.clj
@@ -267,7 +267,7 @@
                                                ;; in the final package.
                                                [puppetlabs/puppetdb ~pdb-version]]
                       :name "puppetdb"
-                      :plugins [[puppetlabs/lein-ezbake "1.9.7"]]}
+                      :plugins [[puppetlabs/lein-ezbake "2.0.3"]]}
              :testutils {:source-paths ^:replace ["test"]
                          :resource-paths ^:replace []
                          ;; Something else may need adjustment, but
