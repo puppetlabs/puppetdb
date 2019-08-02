323363c399adb6b5ffad901d299a20a5b9ee7764
Austin Blatt <austinblatt@gmail.com>
2019-08-02 16:29:43 -0700

    (PDB-4388) Support acceptance tests on redhat7-fips

--- acceptance/setup/pre_suite/15_setup_repos.rb
+++ acceptance/setup/pre_suite/15_setup_repos.rb
@@ -1,11 +1,8 @@
 unless (test_config[:skip_presuite_provisioning])
   step "Install Puppet Labs repositories" do
 
-    # when running upgrade_oldest init puppet5 repos
-    puppet_collection = test_config[:install_mode] == :upgrade_oldest ? :puppet5 : :puppet6
-
     hosts.each do |host|
-      initialize_repo_on_host(host, test_config[:os_families][host.name], test_config[:nightly], puppet_collection)
+      initialize_repo_on_host(host, test_config[:os_families][host.name], test_config[:nightly], puppet_repo_version)
     end
   end
 end
--- acceptance/setup/pre_suite/20_install_puppet.rb
+++ acceptance/setup/pre_suite/20_install_puppet.rb
@@ -1,12 +1,7 @@
 test_name "Install Puppet" do
   unless (test_config[:skip_presuite_provisioning])
     step "Install Puppet" do
-      # when running upgrade_oldest install puppet5, except on rhel8 oldest supported is puppet6
-      puppet_collection = test_config[:install_mode] == :upgrade_oldest ? "puppet5" : "puppet6"
-      if is_rhel8
-        puppet_collection = "puppet6"
-      end
-      install_puppet(puppet_collection)
+      install_puppet(puppet_repo_version)
     end
   end
 
