15a6919425b23324cb813d99816c53b078191df0
Nate Wolfe <nwolfe@puppet.com>
2019-08-07 17:44:44 -0700

    (MAINT) Fix postgres ENV variable
    Using an ENV variable in the definition of another ENV variable in the
    Dockerfile doesn't work as expected, as these values are interpreted at
    build time, not run time.
    
    Also bump to the latest ssl.sh which supports DNS_ALT_NAMES, and plumb
    that through with an empty default.

--- docker/puppetdb/docker-entrypoint.d/30-configure-ssl.sh
+++ docker/puppetdb/docker-entrypoint.d/30-configure-ssl.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/etc/puppetlabs/puppet/ssl/certs/${HOSTNAME}.pem" ] && [ "$USE_PUPPETSERVER" = true ]; then
   set -e
-  /ssl.sh
+  DNS_ALT_NAMES="${DNS_ALT_NAMES}" /ssl.sh "$HOSTNAME"
 fi
 if [ ! -d "/etc/puppetlabs/puppetdb/ssl" ] && [ "$USE_PUPPETSERVER" = true ]; then
   /ssl-setup.sh -f
