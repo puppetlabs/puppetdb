581ed0e02636e44c27a6602cbe368d65abbce026
Austin Blatt <austinblatt@gmail.com>
2019-09-10 10:34:25 -0700

    (maint) test: enable tlsv1 on rhel8
    Our acceptance tests were not working properly on redhat8 due to assuming
    tlsv1.

--- acceptance/setup/pre_suite/15_setup_repos.rb
+++ acceptance/setup/pre_suite/15_setup_repos.rb
@@ -1,6 +1,10 @@
 unless (test_config[:skip_presuite_provisioning])
   step "Install Puppet Labs repositories" do
 
+    if is_rhel8
+      on(hosts, 'update-crypto-policies --set LEGACY')
+    end
+
     hosts.each do |host|
       initialize_repo_on_host(host, test_config[:os_families][host.name], test_config[:nightly], puppet_repo_version)
     end
