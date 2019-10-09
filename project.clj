9993c7926926825836259fd7b806dffbed87b466
Austin Blatt <austinblatt@gmail.com>
2019-10-09 11:34:59 -0700

    (maint) Bump ezbake to 2.1.5
    This stops us from building fips packages for FOSS

--- project.clj
+++ project.clj
@@ -298,7 +298,7 @@
                                                ;; in the final package.
                                                [puppetlabs/puppetdb ~pdb-version]]
                       :name "puppetdb"
-                      :plugins [[puppetlabs/lein-ezbake "2.1.4"]]}
+                      :plugins [[puppetlabs/lein-ezbake "2.1.5"]]}
              :testutils {:source-paths ^:replace ["test"]
                          :resource-paths ^:replace []
                          ;; Something else may need adjustment, but
