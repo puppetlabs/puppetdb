f9cd419ffc2c151935819a6455cf92b8bf0bb8de
Iristyle <Iristyle@github>
2020-04-15 17:37:16 -0700

    (maint) Remove LCOW support / convert to WSL2
     - Remove having to add .test to the end of container names in compose
       due to DNS resolution bugs in LCOW
     - Remove any LCOW file system permission workarounds given WSL2 does
       not have these problems

--- docker/puppetdb/docker-entrypoint.d/00-lcow-volume-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-# Creating log path as a workaround for LCOW VOLUME bug
-# https://github.com/moby/moby/issues/39892
-
-# when the log directory doesn't exist, there can be errors from apps logging there
-mkdir -p /opt/puppetlabs/server/data/puppetdb/logs
