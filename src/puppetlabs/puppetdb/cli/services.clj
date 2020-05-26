puppetlabs/puppetdb

commit 7a52d73801a38be79b725a25aa74bcfe1d847bcb

(PDB-4732) Forbid migration when there's more than one write db

src/puppetlabs/puppetdb/cli/services.clj
@@ -597,6 +597,12 @@
 
     (when-let [v (version/version)]
       (log/info (trs "PuppetDB version {0}" v)))
+
+    (when (> (count (conf/section-subsections database)) 1)
+      (throw
+       (ex-info "Currently unable to migrate multiple databases (see config)"
+                {:kind ::must-migrate-multiple-databases})))
+
     (init-with-db database config)
 
     (if upgrade-and-exit?
@@ -737,8 +743,10 @@
           ::invalid-database-configuration
           (stop (invalid-conf-msg (:failed-validation data)) 1)
 
-          ::migration-required
-          (stop (.getMessage ex) (int \m))
+          ::migration-required (stop (.getMessage ex) (int \m))
+
+          ::must-migrate-multiple-databases
+          (stop (str "puppetdb: " (.getMessage ex) "\n") 2)
 
           ;; Unrecognized -- pass it on.
           (throw ex))))))

commit c5060f1188b5738933fc034a4e753ed78d962890

(PDB-4743) Fix migration check when there's more than one db
Comparison was backward, and also didn't consult "migrate=?" settings.

fixes: 7a52d73801a38be79b725a25aa74bcfe1d847bcb

src/puppetlabs/puppetdb/cli/services.clj
@@ -656,10 +656,12 @@
     (when-let [v (version/version)]
       (log/info (trs "PuppetDB version {0}" v)))
 
-    (when (> (count (conf/section-subsections database)) 1)
-      (throw
-       (ex-info "Currently unable to migrate multiple databases (see config)"
-                {:kind ::must-migrate-multiple-databases})))
+    (let [db-cfgs (conf/section-subsections database)]
+      (when (and (> (count db-cfgs) 1)
+                 (some :migrate (vals db-cfgs)))
+        (throw
+         (ex-info "Currently unable to migrate multiple databases (see config)"
+                  {:kind ::must-migrate-multiple-databases}))))
 
     (init-with-db database config)
 