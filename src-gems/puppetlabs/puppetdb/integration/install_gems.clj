92e59358a3f26fe0d8ebc4e305ab6dc9d90e6f18
Austin Blatt <austin.blatt@puppet.com>
2020-06-23 10:55:43 -0700

    (maint) Add --no-document option to puppetserver gem install
    This prevents an issue with rdoc now that puppetserver's jruby uses
    Rubygems 3

--- src-gems/puppetlabs/puppetdb/integration/install_gems.clj
+++ src-gems/puppetlabs/puppetdb/integration/install_gems.clj
@@ -11,14 +11,14 @@
     (jruby-core/cli-run! jruby-config "gem" args)))
 
 (defn install-gems [gem-list-name config _]
-  (gem-run! config "install" "facter")
-  (gem-run! config "install" "hiera")
+  (gem-run! config "install" "facter" "--no-document")
+  (gem-run! config "install" "hiera" "--no-document")
 
   ;; Install the puppetserver vendored gems listed inside its jar; this is where
   ;; ezbake gets them
   (let [gem-list (string/split (slurp (io/resource (str "ext/build-scripts/" gem-list-name))) #"\n")]
     (doseq [[gem version] (map #(string/split % #"\s") gem-list)]
-      (gem-run! config "install" gem "--version" version))))
+      (gem-run! config "install" gem "--no-document" "--version" version))))
 
 (defn -main
   [& args]
