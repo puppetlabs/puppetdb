66855297fe6696d2d0e842e3b1631f482b436f2b
Zachary Kent <zachary.kent@puppet.com>
2020-07-08 11:21:23 -0700

    (maint) Bump ezbake version to 2.1.7

--- project.clj
+++ project.clj
@@ -296,7 +296,7 @@
                                                ;; in the final package.
                                                [puppetlabs/puppetdb ~pdb-version]]
                       :name "puppetdb"
-                      :plugins [[puppetlabs/lein-ezbake "2.1.6"]]}
+                      :plugins [[puppetlabs/lein-ezbake "2.1.7"]]}
              :testutils {:source-paths ^:replace ["test"]
                          :resource-paths ^:replace []
                          ;; Something else may need adjustment, but
