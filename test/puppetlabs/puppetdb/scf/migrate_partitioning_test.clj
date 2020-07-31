0b117000f99e641050ca1458ec8ca74f975d1390
Austin Blatt <austin.blatt@puppet.com>
2020-07-31 13:25:22 -0700

    (PDB-4832) Add schema-diff check for migration 76
    Add a schema diff check for migration 76 when the index has not been
    applied by migration 74 (which will be the case for anyone running a
    version of pdb between 6.10.0 and 6.11.2.

--- test/puppetlabs/puppetdb/scf/migrate_partitioning_test.clj
+++ test/puppetlabs/puppetdb/scf/migrate_partitioning_test.clj
@@ -1277,3 +1277,40 @@
                                           :same nil}]))
                                     dates))}
            (diff-schema-maps before-migration (schema-info-map *db*))))))
+
+(deftest migration-76-schema-diff
+  (clear-db-for-testing!)
+  ;; don't add the idx_reports_id index when fast forwarding past migration 74
+  (binding [partitioning/add-report-id-idx? false]
+    (fast-forward-to-migration! 75))
+
+  (let [before-migration (schema-info-map *db*)
+        today (ZonedDateTime/now (ZoneId/of "UTC"))
+        days-range (range -4 4)
+        dates (map #(.plusDays today %) days-range)
+        part-names (map #(str/lower-case (partitioning/date-suffix %)) dates)]
+    (apply-migration-for-testing! 76)
+
+    (is (= {:index-diff (into
+                          []
+                          cat
+                          (map
+                            (fn [part-name]
+                              (let [table-name (str "reports_" part-name)]
+                                 [{:left-only nil
+                                  :right-only
+                                  {:schema "public"
+                                   :table table-name
+                                   :index (str "idx_reports_id_" part-name)
+                                   :index_keys ["id"]
+                                   :type "btree"
+                                   :unique? true
+                                   :functional? false
+                                   :is_partial false
+                                   :primary? false
+                                   :user "pdb_test"}
+                                  :same nil}]))
+                            part-names))
+            :table-diff nil
+            :constraint-diff nil}
+           (diff-schema-maps before-migration (schema-info-map *db*))))))
