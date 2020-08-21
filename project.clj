48f15fe50f8cc512f90a2a7643bb550b477020dd
Austin Blatt <austin.blatt@puppet.com>
2020-08-21 15:02:36 -0700

    (maint) Bump ezbake to 2.2.1
    Ensures we use the right OutOfMemory cli flag based on the java version

--- project.clj
+++ project.clj
@@ -297,7 +297,7 @@
                                                ;; in the final package.
                                                [puppetlabs/puppetdb ~pdb-version]]
                       :name "puppetdb"
-                      :plugins [[puppetlabs/lein-ezbake "2.2.0"]]}
+                      :plugins [[puppetlabs/lein-ezbake "2.2.1"]]}
              :testutils {:source-paths ^:replace ["test"]
                          :resource-paths ^:replace []
                          ;; Something else may need adjustment, but
