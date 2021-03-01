0c84bf095af4a7186090f7d8db47384db75d27c8
Rob Browning <rlb@puppet.com>
2021-03-01 11:29:05 -0600

    (PDB-5022) eastwood: ignore supspicious-expressions created by alt!!
    test/puppetlabs/puppetdb/command_test.clj:173:3:
    suspicious-expression: or called with 1 args.  (or x) always returns
    x.  Perhaps there are misplaced parentheses?

--- eastwood.clj
+++ eastwood.clj
@@ -6,3 +6,10 @@
     #"^#'puppetlabs\.trapperkeeper\.testutils\.logging/atom-appender$"
     #"^#'puppetlabs\.trapperkeeper\.testutils\.logging/atom-logger$"
     #"^#'puppetlabs\.trapperkeeper\.testutils\.logging/logs-matching$"}})
+
+(disable-warning
+ {:linter :suspicious-expression
+  :for-macro 'clojure.core/or
+  :if-inside-macroexpansion-of #{'clojure.core.async/alt!!}
+  :within-depth 6 ;; determined experimentally
+  :reason "alt!! creates one-armed or expressions"})
