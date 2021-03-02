f92c2bd73c8127d787192a1d2a59727d3b824c53
Rob Browning <rlb@puppet.com>
2021-03-02 14:09:21 -0600

    (PDB-5022) migrate: convert doall to doseqs for more idiomatic side-effects

--- src/puppetlabs/puppetdb/scf/migrate.clj
+++ src/puppetlabs/puppetdb/scf/migrate.clj
@@ -843,16 +843,14 @@
   (jdbc/call-with-query-rows
    ["select id from factsets where hash is null"]
    (fn [rows]
-     (doseq [batch (partition-all 500 rows)]
-       (doall
-        (map #(jdbc/do-prepared
-               "update factsets set hash=? where id=?"
-               [(-> (:id %)
-                    .toString
-                    kitchensink/utf8-string->sha1
-                    sutils/munge-hash-for-storage)
-                (:id %)])
-             batch)))))
+     (doseq [batch (partition-all 500 rows)
+             row batch]
+       (jdbc/do-prepared "update factsets set hash=? where id=?"
+                         [(-> (:id row)
+                              .toString
+                              kitchensink/utf8-string->sha1
+                              sutils/munge-hash-for-storage)
+                          (:id row)]))))
 
   (jdbc/do-commands
    "ALTER TABLE factsets ALTER COLUMN hash SET NOT NULL"))
