99c2d39a822d9b8f0fb5c3cf8ac8fe9d6e6de9c7
Zak-Kent <zak.kent@gmail.com>
2021-03-31 15:01:30 -0700

    (maint) Set crypto-policies to LEGACY in el8 acceptance tests
    This commit should be reverted once PDB 6.15.1 and PDB 7.2.1 are released.
    
    The upgrade_latest acceptance tests install a version of PDB on el8 which
    still needs the crypto-policies workaround until a version of PDB with
    an increased ephemeralDHKeySize is released.

--- acceptance/setup/pre_suite/15_setup_repos.rb
+++ acceptance/setup/pre_suite/15_setup_repos.rb
@@ -1,4 +1,9 @@
 unless (test_config[:skip_presuite_provisioning])
+
+  if is_el8 && ([:upgrade_latest].include? test_config[:install_mode])
+    on(hosts, 'update-crypto-policies --set LEGACY')
+  end
+
   step "Install Puppet Labs repositories" do
     hosts.each do |host|
       initialize_repo_on_host(host, test_config[:os_families][host.name], test_config[:nightly], puppet_repo_version)
