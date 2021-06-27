4971ed9b6e107743a75ee9acbaa026f764d57ac8
Rob Browning <rlb@puppet.com>
2021-06-27 12:47:17 -0500

    (PDB-5161) Add AST query field parser (e.g. foo.bar[3]...)
    The parser intentionally drops support for empty field components,
    e.g. foo..bar, and it's primarily just trying to codify/handle the
    existing syntax strictly, without making any substantial changes.
    
    The intent is to maintain backward compatibility where possible and/or
    important.

--- test/puppetlabs/puppetdb/query_eng/parse_test.clj
+++ test/puppetlabs/puppetdb/query_eng/parse_test.clj
@@ -1,7 +1,102 @@
 (ns puppetlabs.puppetdb.query-eng.parse-test
   (:require
    [clojure.test :refer :all]
-   [puppetlabs.puppetdb.query-eng.parse :as t]))
+   [puppetlabs.puppetdb.query-eng.parse :as parse])
+  (:import
+   (clojure.lang ExceptionInfo)))
+
+;; FIXME: finish tests
+
+(defn parse-field-ex-info [field]
+  (try
+    (parse/parse-field field)
+    (catch ExceptionInfo ex
+      {:msg (.getMessage ex)
+       :data (ex-data ex)})))
+
+(deftest field-parsing
+
+  (is (= {:kind ::parse/invalid-field :field ""}
+         (-> "" parse-field-ex-info :data)))
+
+  (is (= {:kind ::parse/invalid-field-component :field "nope!" :offset 0}
+         (-> "nope!" parse-field-ex-info :data)))
+
+  (is (= {:kind ::parse/invalid-field-component :field "nope!.nope" :offset 0}
+         (-> "nope!.nope" parse-field-ex-info :data)))
+
+  (is (= {:kind ::parse/invalid-field-component :field "nope." :offset 5}
+         (-> "nope." parse-field-ex-info :data)))
+
+  (is (= {:kind ::parse/invalid-field-component :field "nope..nope" :offset 5}
+         (-> "nope..nope" parse-field-ex-info :data)))
+
+  (is (= {:kind ::parse/invalid-field-component :field "nope.\"\".nope" :offset 5}
+         (-> "nope.\"\".nope" parse-field-ex-info :data)))
+
+  (is (= [{:kind ::parse/named-field-part :name "foo"}]
+         (parse/parse-field "foo")))
+  (is (= [{:kind ::parse/named-field-part :name "foo?"}]
+         (parse/parse-field "foo?")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar"}]
+         (parse/parse-field "foo.bar")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar\nbaz"}]
+         (parse/parse-field "foo.bar\nbaz")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar"}
+          {:kind ::parse/named-field-part :name "baz"}]
+         (parse/parse-field "foo.bar.baz")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/indexed-field-part :name "bar" :index 3}]
+         (parse/parse-field "foo.bar[3]")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/indexed-field-part :name "bar baz" :index 3}]
+         (parse/parse-field "foo.\"bar baz[3]\"")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/match-field-part :pattern "bar"}]
+         (parse/parse-field "foo.match(\"bar\")")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar baz"}]
+         (parse/parse-field "foo.\"bar baz\"")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar.baz"}]
+         (parse/parse-field "foo.\"bar.baz\"")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar.baz.bax"}
+          {:kind ::parse/named-field-part :name "xyz"}]
+         (parse/parse-field "foo.\"bar.baz.bax\".xyz")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar.baz"}
+          {:kind ::parse/named-field-part :name "bar.bax"}]
+         (parse/parse-field "foo.\"bar.baz\".\"bar.bax\"")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar..baz"}]
+         (parse/parse-field "foo.\"bar..baz\"")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar\"baz"}
+          {:kind ::parse/named-field-part :name "bax"}]
+         (parse/parse-field "foo.bar\"baz.bax")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "ba\\\".r"}]
+         (parse/parse-field "foo.\"ba\\\".r\"")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar.baz\"bax"}
+          {:kind ::parse/named-field-part :name "xyz"}]
+         (parse/parse-field "foo.\"bar.baz\"bax\".xyz")))
+  (is (= [{:kind ::parse/named-field-part :name "v"}
+          {:kind ::parse/named-field-part :name "w x"}
+          {:kind ::parse/indexed-field-part :name "y" :index 3}
+          {:kind ::parse/match-field-part :pattern "z"}]
+         (parse/parse-field "v.\"w x\".y[3].match(\"z\")")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "\""}
+          {:kind ::parse/named-field-part :name "bar"}]
+         (parse/parse-field "foo.\".bar")))
+  (is (= [{:kind ::parse/named-field-part :name "foo"}
+          {:kind ::parse/named-field-part :name "bar\""}]
+         (parse/parse-field "foo.bar\""))))
 
 (deftest dotted-query-to-path
   (testing "vanilla dotted path"
