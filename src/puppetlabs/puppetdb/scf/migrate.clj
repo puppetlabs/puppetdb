1586c4d1b1e6bdb0e34d9c44e09ea4b811395f78
Rob Browning <rlb@puppet.com>
2022-03-02 11:11:27 -0600

    (PT-1027) Add storage for "desired policies"
    Add storage for the "desired policies" component of the experimental
    policies feature.

--- src/puppetlabs/puppetdb/scf/migrate.clj
+++ src/puppetlabs/puppetdb/scf/migrate.clj
@@ -2059,6 +2059,19 @@
     "  PRIMARY KEY (workspace_uuid, certname),"
     "  FOREIGN KEY (workspace_uuid) REFERENCES workspaces(uuid) ON DELETE CASCADE)"]))
 
+(defn add-desired-policies
+  []
+  (jdbc/do-commands
+   ["create table desired_policies ("
+    "  changed timestamp with time zone not null,"
+    "  certname text primary key,"
+    "  version text,"
+    "  policies jsonb,"
+    "  foreign key (certname) references certnames(certname) on delete cascade"
+    ")"]
+   "create index idx_desired_policies_changed on desired_policies using btree (changed)"
+   "create index idx_desired_policies_policies on desired_policies using gin(policies jsonb_path_ops)"))
+
 (def migration-spec
   (->> [{:id 00 :fn require-schema-migrations-table}
         {:id 28 :fn init-through-2-3-8}
@@ -2121,7 +2134,9 @@
         {:id 77 :fn add-catalog-inputs-pkey}
         {:id 78 :fn add-catalog-inputs-hash}
         {:id 79 :fn add-report-partition-indexes-on-certname-end-time}
-        {:id 80 :fn add-workspaces-tables}]
+        {:id 80 :fn add-workspaces-tables}
+        (when (conf/experimental-features "policies")
+          {:id 81 :fn add-desired-policies :strict-order? true})]
        ;; Make sure that if you change the structure of reports or
        ;; resource events, you also update the delete-reports cli
        ;; command.
