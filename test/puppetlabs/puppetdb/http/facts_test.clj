puppetlabs/puppetdb

commit a1136f29f7076c389342618c5c8884070a4051b4

(PDB-4731) Validate extract clause filters

src/puppetlabs/puppetdb/query_eng/engine.clj
@@ -2531,6 +2531,26 @@
               (if (empty? error-context) "" (str " " error-context))
               (formatter/comma-separated-keywords allowed-fields)))))
 
+(defn valid-operator?
+  [operator]
+  (or (contains? #{"from" "in" "extract" "subquery" "and"
+                   "or" "not" "function" "group_by" "null?"} operator)
+      (contains? binary-operators operator)
+      (contains? (ks/keyset user-name->query-rec-name) operator)))
+
+(defn validate-extract-filters
+  [expr]
+  (let [message (vec (filter seq (for [clause expr]
+                                   (let [operator (first clause)]
+                                     (if (vector? clause)
+                                       (if (contains? #{"or" "and"} operator)
+                                         (validate-extract-filters clause)
+                                         (if (not (valid-operator? operator))
+                                           (tru "{0} is not a valid expression for \"extract\"" (pr-str clause)))))))))]
+    (if (empty? message)
+      nil
+      (first message))))
+
 (defn annotate-with-context
   "Add `context` as meta on each `node` that is a vector. This associates the
   the query context assocated to each query clause with it's associated context"
@@ -2617,16 +2637,17 @@
 
             ; This validation is only for top-level extract operator
             ; For in-extract operator validation, please see annotate-with-context function
-            [["extract" field & _]]
+            [["extract" field & expr]]
             (let [query-context (:query-context (meta node))
                   extractable-fields (projectable-fields query-context)
                   extractable-json-fields (projectable-json-fields query-context)
-                  column-validation-message (validate-query-operation-fields
-                                              field
-                                              extractable-fields
-                                              extractable-json-fields
-                                              (:alias query-context)
-                                              "Can't extract" "")]
+                  column-validation-message (or (validate-query-operation-fields
+                                                  field
+                                                  extractable-fields
+                                                  extractable-json-fields
+                                                  (:alias query-context)
+                                                  "Can't extract" "")
+                                                (validate-extract-filters expr))]
               (when column-validation-message
                 {:node node
                  :state (conj state column-validation-message)}))
@@ -2658,13 +2679,6 @@
                  :state (conj state column-validation-message)}))
 
             :else nil))
-
-(defn valid-operator?
-  [operator]
-  (or (contains? #{"from" "in" "extract" "subquery" "and"
-                   "or" "not" "function" "group_by" "null?"} operator)
-      (contains? binary-operators operator)
-      (contains? (ks/keyset user-name->query-rec-name) operator)))
 
 (defn ops-to-lower
   "Lower cases operators (such as and/or)."

test/puppetlabs/puppetdb/http/reports_test.clj
@@ -457,6 +457,35 @@
                  body))
     (is (= 400 status))))
 
+(deftest-http-app invalid-extract
+  [[version endpoint] endpoints
+   method [:get :post]]
+   (let [bad-queries [["extract"
+                       [["function" "count"] "certname"]
+                       ["null?" "type" false]
+                       ["groupy_by" "certname"]]
+                      ["extract"
+                       [["function" "count"] "certname"]
+                       ["null?" "type" false]
+                       ["some_invalid_string"]]]
+         good-queries [["extract" ["hash" "certname" "transaction_uuid"]
+                        ["=" "certname" "foo.com"]]
+                       ["extract" "transaction_uuid"
+                        ["=" "certname" "foo.com"]]
+                       ["extract" [["function" "count"] "status"]
+                        ["=" "certname" "foo.com"]
+                        ["group_by" "status"]]]]
+  (doseq [bad-query bad-queries]
+    (testing (str "Ensure bad query fails: " (pr-str bad-query))
+      (let [{:keys [body status]} (query-response method endpoint bad-query)]
+        (is (re-matches #".* is not a valid expression for \"extract\".*" body))
+        (is (= 400 status)))))
+
+  (doseq [good-query good-queries]
+    (testing (str "Ensure good query succeeds: " (pr-str good-query))
+      (let [{:keys [_ status]} (query-response method endpoint good-query)]
+        (is (= 200 status)))))))
+
 (deftest-http-app query-by-status
   [[version endpoint] endpoints
    method [:get :post]]

commit 30ed7c59443f987a4cd7b16b18528880d77bf0c0

(PE-33977) Fix AST validation regression when ~> inside extract
A regression was introduced with commit
a1136f29f7076c389342618c5c8884070a4051b4 that mistakenly marked valid
queries as invalid when the query used the ~> regex array match operator
inside an extract clause.

This patch fixes the valid-operator? function to stop marking the ~>
operator is invalid by including the ~> operator in the
simple-binary-operators set.

The binary operator node validation previously did not produce a helpful
error when ~> had more than two arguments. This is now fixed.

The function validate-extract-filters is rewritten
to improve clarity and efficiency. Now the function will short circuit
once a validation error has been found.

src/puppetlabs/puppetdb/query_eng/engine.clj
@@ -1873,9 +1873,6 @@
   (-> (get user-name->query-rec-name subquery)
       (assoc :subquery? true)))
 
-(def binary-operators
-  #{"=" ">" "<" ">=" "<=" "~"})
-
 (defn strip-function-calls
   [column-or-columns]
   (let [{functions true
@@ -2109,11 +2106,14 @@
 
             :else nil))
 
-(def binary-operator-checker
+(def simple-binary-operators
+  #{"=" ">" "<" ">=" "<=" "~" "~>"})
+
+(def simple-binary-operator-checker
   "A function that will return nil if the query snippet successfully validates, otherwise
   will return a data structure with error information"
   (s/checker [(s/one
-               (apply s/enum binary-operators)
+               (apply s/enum simple-binary-operators)
                :operator)
               (s/one (s/cond-pre s/Str
                                  [(s/one s/Str :nested-field)
@@ -2195,10 +2195,12 @@
               (when (not= 1 (count clauses))
                 (throw (IllegalArgumentException. (tru "''not'' takes exactly one argument, but {0} were supplied" (count clauses)))))
 
-              [[op & _]]
-              (when (and (contains? binary-operators op)
-                         (binary-operator-checker node))
-                (throw (IllegalArgumentException. (tru "{0} requires exactly two arguments" op))))
+              [[op & clauses]]
+              (when (simple-binary-operators op)
+                (when (not= 2 (count clauses))
+                  (throw (IllegalArgumentException. (tru "{0} requires exactly two arguments" op))))
+                (when (simple-binary-operator-checker node)
+                  (throw (IllegalArgumentException. (tru "Invalid {0} clause" op)))))
 
               :else nil)))
 
@@ -2729,21 +2731,20 @@
   [operator]
   (or (contains? #{"from" "in" "extract" "subquery" "and"
                    "or" "not" "function" "group_by" "null?"} operator)
-      (contains? binary-operators operator)
+      (contains? simple-binary-operators operator)
       (contains? (ks/keyset user-name->query-rec-name) operator)))
 
 (defn validate-extract-filters
+  "Validates the operators inside an extract clause. Short-circuits once an
+  invalid operator is found"
   [expr]
-  (let [message (vec (filter seq (for [clause expr]
-                                   (let [operator (first clause)]
-                                     (when (vector? clause)
-                                       (if (contains? #{"or" "and"} operator)
-                                         (validate-extract-filters clause)
-                                         (when-not (valid-operator? operator)
-                                           (tru "{0} is not a valid expression for \"extract\"" (pr-str clause)))))))))]
-    (if (empty? message)
-      nil
-      (first message))))
+  (some (fn check-extract-clause [[operator :as clause]]
+          (when (vector? clause)
+            (if (#{"or" "and"} operator)
+              (validate-extract-filters clause)
+              (when-not (valid-operator? operator)
+                (tru "{0} is not a valid expression for \"extract\"" (pr-str clause))))))
+        expr))
 
 (defn annotate-with-context
   "Add `context` as meta on each `node` that is a vector. This associates the

test/puppetlabs/puppetdb/http/facts_test.clj
@@ -28,7 +28,8 @@
                      is-query-result
                      query-response
                      query-result
-                     vector-param]]
+                     vector-param
+                     with-http-app]]
             [puppetlabs.puppetdb.utils :as utils]
             [puppetlabs.puppetdb.testutils.services :as svc-utils
              :refer [call-with-puppetdb-instance]]
@@ -252,7 +253,18 @@
                 #"Can't extract unknown 'facts' field 'nothing'.*Acceptable fields are.*"
 
                 ["extract" ["certname" "nothing" "nothing2"] ["~" "certname" ".*"]]
-                #"Can't extract unknown 'facts' fields 'nothing' and 'nothing2'.*Acceptable fields are.*")))
+                #"Can't extract unknown 'facts' fields 'nothing' and 'nothing2'.*Acceptable fields are.*"
+
+                ["~>" "test" "test" "test"] #"~> requires exactly two arguments"
+                ["~>" "test"] #"~> requires exactly two arguments"
+                [">" "test" 50 100] #"> requires exactly two arguments"
+                [">" "test"] #"> requires exactly two arguments"
+                [">=" "test" 50 100] #">= requires exactly two arguments"
+                [">=" "test"] #">= requires exactly two arguments"
+                ["<" "test" 50 100] #"< requires exactly two arguments"
+                ["<" "test"] #"< requires exactly two arguments"
+                ["<=" "test" 50 100] #"<= requires exactly two arguments"
+                ["<=" "test"] #"<= requires exactly two arguments")))
 
 (deftest-http-app invalid-projections
   [[_version endpoint] facts-endpoints
@@ -263,6 +275,16 @@
         (is (re-find msg body))
         (is (= HttpURLConnection/HTTP_BAD_REQUEST status))
         (are-error-response-headers headers)))))
+
+(deftest valid-projections
+  (testing "Projection works with ~> (regexp array match) operator"
+    (with-test-db
+      (with-http-app
+        (let [query ["from" "fact_contents"
+                     ["extract" ["certname" "value"]
+                      ["~>" "path" ["virtual"]]] ["order_by" ["certname"]]]
+              {:keys [status]} (query-response :post "/v4" query)]
+          (is (= java.net.HttpURLConnection/HTTP_OK status)))))))
 
 (def pg-versioned-invalid-regexps
   (omap/ordered-map