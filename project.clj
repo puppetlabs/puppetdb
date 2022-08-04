63dc7d001b5045f091903d7110ae54cc2ac2baf8
Austin Blatt <austin.blatt@puppet.com>
2022-08-04 14:38:26 -0700

    (PDB-5519) Add honeysql v2 to project.clj
    honeysql v1 and v2 can be present in a project simultaneously

--- project.clj
+++ project.clj
@@ -205,6 +205,7 @@
                  ;; Database connectivity
                  [com.zaxxer/HikariCP]
                  [honeysql]
+                 [com.github.seancorfield/honeysql]
 
                  ;; WebAPI support libraries.
                  [bidi]
