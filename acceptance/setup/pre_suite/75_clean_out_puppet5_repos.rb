af58ae0cce4ce82429cf1d695ca01e00ec113938
Austin Blatt <austinblatt@gmail.com>
2023-04-18 19:06:33 -0700

    (PDB-5630) redhat: select java 11 manually
    On RedHat, java 8 has a higher priority than java 11, so for servers
    upgraded from PuppetDB 7 to PuppetDB 8 we need to manually select java
    11 as the system java.

--- acceptance/setup/pre_suite/75_clean_out_puppet5_repos.rb
+++ acceptance/setup/pre_suite/75_clean_out_puppet5_repos.rb
@@ -9,6 +9,17 @@ if (test_config[:install_mode] == :upgrade_oldest) \
         # need to remove puppet6 repo to avoid conflicts when upgrading
         uninstall_package(database, "puppet7-release")
 
+        databases.each do |host|
+          os = test_config[:os_families][host.name]
+
+          # On RedHat systems, java 8 is still higher priority than java 8
+          # To avoid the upgrade failing, we "pre-upgrade" the server to java 11
+          if os == :redhat
+            install_package(host, 'java-11-openjdk-headless')
+            on(database, "alternatives --set java /usr/lib/jvm/java-11-openjdk-*/bin/java")
+          end
+        end
+
         # This is an upgrade oldest test, but we are upgrading now and want the
         # latest platform so specify the :install type to get the proper puppet repo
         #
