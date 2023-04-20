d52f3f3da2f8018f7204f97af31c16c3b43b2eb5
Austin Blatt <austinblatt@gmail.com>
2023-04-20 08:59:26 -0700

    (maint) bionic: upgrade ssl-cert package
    Installing PuppetDB via the module was failing with the following error.
    It closely matched
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=990228 which was fixed
    in ssl-cert 1.1.1. Even though the problem is described as affecting
    OpenSSL 3.0.0, it fixes our issue as well.
    
    ```
    Setting up ssl-cert (1.0.39) ...
        Could not create certificate. Openssl output was:
        .+........+......+....+...+...+.....+......++++<continues>
        -----
        Cannot write random bytes:
        C0D0CC97387F0000:error:1200007A:random number generator:RAND_write_file:Not a regular file:crypto/rand/randfile.c:190:Filename=/dev/urandom
        dpkg: error processing package ssl-cert (--configure):
         installed ssl-cert package post-installation script subprocess returned error exit status 1
    ```

--- acceptance/setup/pre_suite/40_install_deps.rb
+++ acceptance/setup/pre_suite/40_install_deps.rb
@@ -11,6 +11,21 @@ unless (test_config[:skip_presuite_provisioning])
     end
   end
 
+  # Unfortunately we need jammy to bring a workable version of ssl-cert into bionic
+  if is_bionic
+    step 'Update Ubuntu 18 package repo' do
+      # Install jammy repos so we can pull in its ssl-cert
+      on master, "echo 'deb http://archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse' > /etc/apt/sources.list.d/jammy.list"
+      on master, "echo 'deb-src http://archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse' >> /etc/apt/sources.list.d/jammy.list"
+      on master, 'apt-get update'
+      on master, 'apt-get install -y -t jammy ssl-cert'
+
+      # Once we have jammy's ssl-cert get rid of jammy packages to avoid unintentially pulling in other packages
+      on master, 'rm /etc/apt/sources.list.d/jammy.list'
+      on master, 'apt-get update'
+    end
+  end
+
   if is_el8
     # work around for testing on rhel8 and the repos on the image not finding the pg packages it needs
     step "Install PostgreSQL manually" do
