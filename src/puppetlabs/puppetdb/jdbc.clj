0d224850df170d0734987f1044b2a1c52ceea288
Rob Browning <rlb@puppet.com>
2023-07-26 11:34:27 -0500

    (PDB-5645) body-stream: summarize streaming query terminations
    Since the body-stream is off in its own thread on the other end of the
    piped-input-string that's connected to the jetty thread pushing the
    data to the client, exceptions won't make it to
    wrap-with-exception-handling, so log the summary directly in
    body-stream instead.

--- src/puppetlabs/puppetdb/jdbc.clj
+++ src/puppetlabs/puppetdb/jdbc.clj
@@ -11,6 +11,7 @@
             [schema.core :as s]
             [puppetlabs.i18n.core :refer [trs]])
   (:import
+   (clojure.lang ExceptionInfo)
    (com.zaxxer.hikari HikariDataSource HikariConfig)
    (java.sql Connection SQLException SQLTransientConnectionException)
    (org.postgresql.util PGobject)))
@@ -31,6 +32,13 @@
        kw-name)
       (throw (IllegalArgumentException.
               (trs "Requested unknown SQL state")))))
+
+(defn clj-jdbc-termination-ex? [ex]
+  (and (instance? ExceptionInfo ex)
+       (let [{:keys [rollback handling]} (ex-data ex)]
+         (and rollback
+              (= (sql-state :admin-shutdown)
+                 (some-> handling .getSQLState))))))
 
 (defmacro with-db-connection [spec & body]
   `(sql/with-db-connection [db# ~spec]
