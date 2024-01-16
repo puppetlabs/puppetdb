72088f08ae654e7dfdb721f814c5b91c28f3e409
Austin Blatt <austinblatt@gmail.com>
2024-01-15 16:52:24 -0800

    (PE-37466) monitor-test/connection-reuse: include a non-streaming query
    Add a test that includes a non-streaming query since they weren't
    being properly forgotten before, which caused a race between the query
    monitor and jetty which could cause the monitor to consume an
    unrelated query's data when jetty re-used the connection.
    
    [rlb@puppet.com: add commit message]

--- test/puppetlabs/puppetdb/query/monitor_test.clj
+++ test/puppetlabs/puppetdb/query/monitor_test.clj
@@ -277,30 +277,52 @@
                         summary)))))))
 
 (deftest connection-reuse
-  ;; Test multiple queries over the same connection.  Run the test
-  ;; multiple times because the first problem we encountered could
-  ;; only occur during a narrow window in the monitor loop if a new
-  ;; request came in between select invocations, after the key had
-  ;; been cancelled.
-  ;; https://github.com/puppetlabs/puppetdb/issues/3866
-  (with-puppetdb nil
-    (jdbc/with-db-transaction [] (add-certnames certnames))
-    ;; Just use curl, since it's trivally easy to get it to do what we
-    ;; need, and we already require it for test setup (via ext/).  (It
-    ;; looks like both clj-http and the JDK HttpClient have more
-    ;; indirect control over reuse.)
-    (let [nodes (-> (assoc *base-url* :prefix "/pdb/query/v4/nodes")
-                    base-url->str-with-prefix)
-          cmd ["curl" "--no-progress-meter" "--show-error" "--fail-early"
-               "--fail" nodes "-o" "/dev/null"
-               "--next" "--fail" nodes "-o" "/dev/null"
-               "--next" "--fail" nodes "-o" "/dev/null"]]
-      (loop [i 0]
-        (let [{:keys [exit out err]} (apply sh cmd)]
-          (when (< i 10)
-            (if (is (= 0 exit))
-              (recur (inc i))
-              (do
-                (apply println "Failed:" cmd)
-                (print out)
-                (print err)))))))))
+  (testing "connection reuse with simple queries"
+    ;; Test multiple queries over the same connection.  Run the test
+    ;; multiple times because the first problem we encountered could
+    ;; only occur during a narrow window in the monitor loop if a new
+    ;; request came in between select invocations, after the key had
+    ;; been cancelled.
+    ;; https://github.com/puppetlabs/puppetdb/issues/3866
+    (with-puppetdb nil
+      (jdbc/with-db-transaction [] (add-certnames certnames))
+      ;; Just use curl, since it's trivally easy to get it to do what we
+      ;; need, and we already require it for test setup (via ext/).  (It
+      ;; looks like both clj-http and the JDK HttpClient have more
+      ;; indirect control over reuse.)
+      (let [nodes (-> (assoc *base-url* :prefix "/pdb/query/v4/nodes")
+                      base-url->str-with-prefix)
+            cmd ["curl" "--no-progress-meter" "--show-error" "--fail-early"
+                 "--fail" nodes "-o" "/dev/null"
+                 "--next" "--fail" nodes "-o" "/dev/null"
+                 "--next" "--fail" nodes "-o" "/dev/null"]]
+        (loop [i 0]
+          (let [{:keys [exit out err]} (apply sh cmd)]
+            (when (< i 10)
+              (if (is (= 0 exit))
+                (recur (inc i))
+                (do
+                  (apply println "Failed:" cmd)
+                  (print out)
+                  (print err)))))))))
+
+  (testing "connection reuse with non-streaming queries"
+    (with-puppetdb nil
+      (jdbc/with-db-transaction [] (add-certnames certnames))
+      (let [q (-> (assoc *base-url* :prefix "/pdb/query/v4")
+                  base-url->str-with-prefix)
+            query (fn [q] (str "{\"query\":\"" q "\"}"))
+            ast-q (fn [q] (str "{\"query\":\"" q "\", \"ast_only\":true}"))
+            cmd ["curl" "-X" "POST" "--no-progress-meter" "--show-error" "--fail-early"
+                 "--fail" "--max-time" "1" q "-H" "Content-Type:application/json"
+                 "-d" (ast-q "nodes{}") "-o" "/dev/null"
+                 "--next"
+                 "--fail" "--max-time" "1" q "-H" "Content-Type:application/json"
+                 "-d" (query "nodes{}") "-o" "/dev/null"]
+            {:keys [exit out err]} (apply sh cmd)]
+        (if (is (= 0 exit))
+          nil
+          (do
+            (apply println "Failed:" cmd)
+            (print out)
+            (print err)))))))
