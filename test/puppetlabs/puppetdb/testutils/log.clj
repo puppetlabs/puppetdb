0054486474a5d0b09f682772650536b72e002562
Rob Browning <rlb@puppet.com>
2024-05-13 15:16:34 -0500

    (PDB-5739) notable-pdb-event?: don't suppress config errors
    No longer needed.

--- test/puppetlabs/puppetdb/testutils/log.clj
+++ test/puppetlabs/puppetdb/testutils/log.clj
@@ -5,9 +5,5 @@
 ;;The below functions are useful with
 ;;puppetlabs.trapperkeeper.testutils.logging/with-log-suppressed-unless-notable
 
-(defn notable-pdb-event?
-  [event]
-  (and (.isGreaterOrEqual (.getLevel event) Level/ERROR)
-       ;; FIXME don't filter out these errors, configure the tests properly
-       (not (re-find #"^The read-database user is not configured properly"
-                     (.getFormattedMessage event)))))
+(defn notable-pdb-event? [event]
+  (.isGreaterOrEqual (.getLevel event) Level/ERROR))
