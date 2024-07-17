puppetlabs/puppetdb

commit 53b2a370acee09d889ea57e6d35e79b9a196d069

(PDB-5745) check-schema-version: no stack traces for expected errors
Don't produce stack traces pointing to this function for schema
mismatches, missing schema_migrations, etc.

Explicitly check for schema_migrations instead of letting it crash
there, and narrow the try/catch scope to the relevant code.

src/puppetlabs/puppetdb/cli/services.clj
@@ -810,35 +810,37 @@
   (when emit-cmd-events?
     (async/>!! cmd-event-ch (queue/make-cmd-event cmdref kind))))
 
-(defn check-schema-version
+(defn stop-unless-valid-schema-version
   [desired-version db _service request-shutdown]
   {:pre [(integer? desired-version)]}
-  (let [schema-version (-> (jdbc/with-transacted-connection db
-                             (jdbc/query "select max(version) from schema_migrations"))
-                           first
-                           :max)
-        stop (fn [msg status]
+  (let [stop (fn stop-after-schema-check-failure [msg status]
                (log/error msg)
-               (request-shutdown {::tk/exit
-                                  {:status status
-                                   :messages [[msg *err*]]}}))]
-    (when-not (= schema-version desired-version)
-      (cond
-        (> schema-version desired-version)
-        (stop (str
-               (trs "Please upgrade PuppetDB: ")
-               (trs "your database contains schema migration {0} which is too new for this version of PuppetDB."
-                    schema-version))
-              (int \M))
-
-        (< schema-version desired-version)
-        (stop (str
-               (trs "Please run PuppetDB with the migrate option set to true to upgrade your database. ")
-               (trs "The detected migration level {0} is out of date." schema-version))
-              (int \m))
-
-        :else
-        (throw (Exception. "Unknown state when checking schema versions"))))))
+               (request-shutdown {::tk/exit {:status status
+                                             :messages [[msg *err*]]}}))
+        res (try
+              (jdbc/with-transacted-connection db
+                (when (jdbc/table-exists? "schema_migrations")
+                  (jdbc/query "select max(version) from schema_migrations")))
+              (catch SQLException ex
+                ex))]
+    (if (instance? SQLException res)
+      (stop (trs "Unable to read schema version: {0}" (.getMessage res)) 2)
+      (let [ver (-> res first :max)]
+        (cond
+          (= ver desired-version) true
+          (nil? res)
+          (stop (trs "Cannot find the PuppetDB schema migration table") 2)
+          (> ver desired-version)
+          (stop (str
+                 (trs "Please upgrade PuppetDB: ")
+                 (trs "your database contains schema migration {0} which is too new for this version of PuppetDB."
+                      ver))
+                (int \M))
+          (< ver desired-version)
+          (stop (str
+                 (trs "Please run PuppetDB with the migrate option set to true to upgrade your database. ")
+                 (trs "The detected migration level {0} is out of date." ver))
+                (int \m)))))))
 
 (defn init-queue [config send-event! cmd-event-ch shutdown-for-ex]
   (let [stockdir (conf/stockpile-dir config)
@@ -916,8 +918,8 @@
                               (str allocate-at-startup-at-least-mb)))
                (vec (repeatedly allocate-at-startup-at-least-mb
                                 #(long-array (* 1024 128))))) ;; ~1mb
-             (check-schema-version (desired-schema-version)
-                                   db service request-shutdown)
+             (stop-unless-valid-schema-version (desired-schema-version)
+                                               db service request-shutdown)
              (catch InterruptedException _
                (log/info (trs "Schema checker interrupted")))))))
      0 schema-check-interval)))

src/puppetlabs/puppetdb/jdbc.clj
@@ -33,6 +33,10 @@
       (throw (IllegalArgumentException.
               (trs "Requested unknown SQL state")))))
 
+(defn table-exists? [name]
+  (some-> (-> *db* sql/db-connection .getMetaData (.getTables nil nil name nil))
+          .next))
+
 (defn clj-jdbc-termination-ex? [ex]
   (and (instance? ExceptionInfo ex)
        (let [{:keys [rollback handling]} (ex-data ex)]

commit f80bb35e116522d47678c7854af3996ab2478b9a

(PDB-5745) stop-unless-valid-schema-version: don't stop for missing db
Don't shut down when postgres is unavailable.  This was just a mistake
introduced by 53b2a370acee09d889ea57e6d35e79b9a196d069.

src/puppetlabs/puppetdb/cli/services.clj
@@ -826,7 +826,10 @@
               (catch SQLException ex
                 ex))]
     (if (instance? SQLException res)
-      (stop (trs "Unable to read schema version: {0}" (.getMessage res)) 2)
+      (if (= (jdbc/sql-state :sqlclient-unable-to-establish-sqlconnection)
+             (.getSQLState res))
+        (log/info (trs "Unable to read schema version: {0}" (.getMessage res)))
+        (log/error (trs "Unable to read schema version: {0}" (.getMessage res))))
       (let [ver (-> res first :max)]
         (cond
           (= ver desired-version) true

src/puppetlabs/puppetdb/jdbc.clj
@@ -44,6 +44,7 @@
    :serialization-failure "40001"
    ;; check constraint violation
    :check-violation "23514"
+   :sqlclient-unable-to-establish-sqlconnection "08001"
    :undefined-table "42P01"})
 
 (def sql-state-name