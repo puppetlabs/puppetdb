eac8382b3278c84c2ac910d3f51f26cdf23334e3
Saurabh Pandit <saurabh.pandit@perforce.com>
2024-09-04 08:34:59 +0530

    (PDB-5782): Updated ezbake build version to support debian-12-x86_64 build for puppetdb

--- project.clj
+++ project.clj
@@ -281,7 +281,7 @@
                                                ;; in the final package.
                                                [puppetlabs/puppetdb ~pdb-version]]
                       :name "puppetdb"
-                      :plugins [[puppetlabs/lein-ezbake "2.5.5"]]}
+                      :plugins [[puppetlabs/lein-ezbake "2.6.2"]]}
              :testutils {:source-paths ^:replace ["test"]
                          :resource-paths ^:replace []
                          ;; Something else may need adjustment, but
